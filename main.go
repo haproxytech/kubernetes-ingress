27fc3d69170b3eefd98ff1a40af63def9a51c9e9
Zlatko Bratkovic <zbratkovic@haproxy.com>
2018-09-28 13:25:12 +0200

    MINOR: doc: added info and picture for project

--- main.go
+++ main.go
@@ -17,11 +17,26 @@ limitations under the License.
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 )
 
 func main() {
+
+	fmt.Println(`
+ _   _    _    ____                                                               
+| | | |  / \  |  _ \ _ __ _____  ___   _                                          
+| |_| | / _ \ | |_) | '__/ _ \ \/ / | | |                                         
+|  _  |/ ___ \|  __/| | | (_) >  <| |_| |                                         
+|_| |_/_/   \_\_|   |_|  \___/_/\_\\__, |                                         
+ ___                               |___/__            _             _ _           
+|_ _|_ __   __ _ _ __ ___  ___ ___   / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __ 
+ | || '_ \ / _` + "`" + ` | '__/ _ \/ __/ __| | |   / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+ | || | | | (_| | | |  __/\__ \__ \ | |__| (_) | | | | |_| | | (_) | | |  __/ |   
+|___|_| |_|\__, |_|  \___||___/___/  \____\___/|_| |_|\__|_|  \___/|_|_|\___|_|   
+           |___/ 
+`)
 	//TODO currently using default log, switch to something more convenient
 	//log.SetFlags(log.LstdFlags | log.Llongfile)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
