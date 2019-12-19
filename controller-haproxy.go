haproxytech/kubernetes-ingress

commit d333725998bb858f0fcdbc005d468d0ef9d89498

BUG/MAJOR: Fix ingress path tracking issue
This fixes an issue to track ingress paths that can be reproduced for
example by updating an existing ingress rule which makes the controller
loose track of a number of related paths.
This is fixed by not relying on path indexes any more for use_backend
rules, but rather map use_backend rules to paths via [namespace.Name, ingress.Name,
 rule.Host, path.Path]

controller-haproxy.go
@@ -16,8 +16,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -64,48 +64,24 @@ func (c *HAProxyController) updateHAProxy() error {
 			continue
 		}
 		for _, ingress := range namespace.Ingresses {
-			pathIndex := 0
 			annClass, _ := GetValueFromAnnotations("ingress.class", ingress.Annotations) // default is ""
 			if annClass.Value != "" && annClass.Value != c.osArgs.IngressClass {
 				ingress.Status = DELETED
 			}
-			//no need for switch/case for now
-			sortedList := make([]string, len(ingress.Rules))
-			index := 0
-			for name := range ingress.Rules {
-				sortedList[index] = name
-				index++
-			}
-			sort.Strings(sortedList)
-			for _, ruleName := range sortedList {
-				rule := ingress.Rules[ruleName]
-				indexedPaths := make([]*IngressPath, len(rule.Paths))
+			for _, rule := range ingress.Rules {
 				for _, path := range rule.Paths {
-					if path.Status != DELETED && ingress.Status != DELETED {
-						indexedPaths[path.PathIndex] = path
-					} else {
-						for key, rule := range c.cfg.UseBackendRules {
-							if rule.Host == ruleName && rule.Namespace == namespace.Name && rule.Path == path.Path {
-								delete(c.cfg.UseBackendRules, key)
-								break
-							}
-						}
+					if path.Status == DELETED {
+						delete(c.cfg.UseBackendRules, fmt.Sprintf("R%s%s%s%0006d", namespace.Name, ingress.Name, rule.Host, path.Path))
 						c.cfg.UseBackendRulesStatus = MODIFIED
+					} else {
+						reload, err = c.handlePath(namespace, ingress, rule, path)
+						needsReload = needsReload || reload
+						LogErr(err)
 					}
-				}
-				for i := range indexedPaths {
-					path := indexedPaths[i]
-					if path == nil {
-						continue
-					}
-					reload, err = c.handlePath(pathIndex, namespace, ingress, rule, path)
-					needsReload = needsReload || reload
-					LogErr(err)
-					pathIndex++
 				}
 			}
-			ingressSecrets := map[string]struct{}{}
 			//handle certs
+			ingressSecrets := map[string]struct{}{}
 			for _, tls := range ingress.TLS {
 				if _, ok := ingressSecrets[tls.SecretName.Value]; !ok {
 					ingressSecrets[tls.SecretName.Value] = struct{}{}
@@ -208,5 +184,5 @@ func (c *HAProxyController) handleDefaultService() (needsReload bool, err error)
 		ServiceName: dsvc[1],
 		PathIndex:   -1,
 	}
-	return c.handlePath(0, namespace, ingress, nil, path)
+	return c.handlePath(namespace, ingress, nil, path)
 }

controller-annotations.go
@@ -169,7 +169,7 @@ func (c *HAProxyController) handleServerAnnotations(ingress *Ingress, service *S
 	return annnotationsActive
 }
 
-func (c *HAProxyController) handleRateLimitingAnnotations(ingress *Ingress, service *Service, path *IngressPath, index int) {
+func (c *HAProxyController) handleRateLimitingAnnotations(ingress *Ingress, service *Service, path *IngressPath) {
 	//Annotations with default values don't need error checking.
 	annWhitelist, _ := GetValueFromAnnotations("whitelist", service.Annotations, ingress.Annotations, c.cfg.ConfigMap.Annotations)
 	annWhitelistRL, _ := GetValueFromAnnotations("whitelist-with-rate-limit", service.Annotations, ingress.Annotations, c.cfg.ConfigMap.Annotations)
@@ -177,7 +177,7 @@ func (c *HAProxyController) handleRateLimitingAnnotations(ingress *Ingress, serv
 	status := annWhitelist.Status
 	if status == EMPTY {
 		if annWhitelistRL.Status != EMPTY {
-			data, ok := c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", index)]
+			data, ok := c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", path.Path)]
 			if ok && len(data) > 0 {
 				status = MODIFIED
 			}
@@ -203,20 +203,20 @@ func (c *HAProxyController) handleRateLimitingAnnotations(ingress *Ingress, serv
 				CondTest: fmt.Sprintf("{ path_beg %s }", path.Path),
 			}
 			if allowRateLimiting {
-				c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", index)] = []models.HTTPRequestRule{
+				c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", path.Path)] = []models.HTTPRequestRule{
 					*httpRequest1,
 				}
 			} else {
-				c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", index)] = []models.HTTPRequestRule{
+				c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", path.Path)] = []models.HTTPRequestRule{
 					*httpRequest2, //reverse order
 					*httpRequest1,
 				}
 			}
 		} else {
-			c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", index)] = []models.HTTPRequestRule{}
+			c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", path.Path)] = []models.HTTPRequestRule{}
 		}
 		c.cfg.HTTPRequestsStatus = MODIFIED
 	case DELETED:
-		c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", index)] = []models.HTTPRequestRule{}
+		c.cfg.HTTPRequests[fmt.Sprintf("WHT-%0006d", path.Path)] = []models.HTTPRequestRule{}
 	}
 }

controller.go
@@ -186,7 +186,7 @@ func (c *HAProxyController) HAProxyReload() error {
 	return err
 }
 
-func (c *HAProxyController) handlePath(index int, namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath) (needReload bool, err error) {
+func (c *HAProxyController) handlePath(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath) (needReload bool, err error) {
 	needReload = false
 	service, ok := namespace.Services[path.ServiceName]
 	if !ok {
@@ -194,7 +194,7 @@ func (c *HAProxyController) handlePath(index int, namespace *Namespace, ingress 
 		return needReload, fmt.Errorf("service %s does not exists", path.ServiceName)
 	}
 
-	backendName, newBackend, reload, err := c.handleService(index, namespace, ingress, rule, path, service)
+	backendName, newBackend, reload, err := c.handleService(namespace, ingress, rule, path, service)
 	needReload = needReload || reload
 	if err != nil {
 		return needReload, err
@@ -214,7 +214,7 @@ func (c *HAProxyController) handlePath(index int, namespace *Namespace, ingress 
 	return needReload, nil
 }
 
-// handleEndpointIP processes the IngressPath related endpoints and makes corresponding HAProxy configuation
+// handleEndpointIP processes the IngressPath related endpoints and makes corresponding backend servers configuration in HAProxy
 func (c *HAProxyController) handleEndpointIP(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service, backendName string, newBackend bool, endpoints *Endpoints, ip *EndpointIP) (needReload bool) {
 	needReload = false
 	weight := int64(128)
@@ -273,11 +273,11 @@ func (c *HAProxyController) handleEndpointIP(namespace *Namespace, ingress *Ingr
 	return needReload
 }
 
-// handleService processes the IngressPath related service and makes corresponding HAProxy configuation
-func (c *HAProxyController) handleService(index int, namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service) (backendName string, newBackend bool, needReload bool, err error) {
+// handleService processes the IngressPath related service and makes corresponding backend configuration in HAProxy
+func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service) (backendName string, newBackend bool, needReload bool, err error) {
 	needReload = false
 
-	c.handleRateLimitingAnnotations(ingress, service, path, index)
+	c.handleRateLimitingAnnotations(ingress, service, path)
 
 	// Set TargetPort
 	if path.ServicePortInt == 0 {
@@ -348,10 +348,11 @@ func (c *HAProxyController) handleService(index int, namespace *Namespace, ingre
 		}
 		// Update usebackend rule
 		if rule != nil {
-			key := fmt.Sprintf("R%s%s%s%0006d", namespace.Name, ingress.Name, rule.Host, index)
+			key := fmt.Sprintf("R%s%s%s%0006d", namespace.Name, ingress.Name, rule.Host, path.Path)
 			old, ok := c.cfg.UseBackendRules[key]
 			if ok {
-				if old.Backend != backendName || old.Host != rule.Host || old.Path != path.Path {
+				// Check if the existing use_backend rule refers to the right backend
+				if old.Backend != backendName {
 					c.cfg.UseBackendRules[key] = BackendSwitchingRule{
 						Host:      rule.Host,
 						Path:      path.Path,

commit 973a498ded23d4537968484bf3df3c4a4f66a94c

BUG/MEDIUM: Ignore ingress objects with inappropirate ingress class
This is a regression in the commit d3337259 ("BUG/MAJOR: Fix ingress
path tracking issuei"). Now instead of setting an ingress with
irrelevant ingress class to DELETE status and process it, we simply
ignore it.

controller-haproxy.go
@@ -66,7 +66,7 @@ func (c *HAProxyController) updateHAProxy() error {
 		for _, ingress := range namespace.Ingresses {
 			annClass, _ := GetValueFromAnnotations("ingress.class", ingress.Annotations) // default is ""
 			if annClass.Value != "" && annClass.Value != c.osArgs.IngressClass {
-				ingress.Status = DELETED
+				continue
 			}
 			for _, rule := range ingress.Rules {
 				for _, path := range rule.Paths {