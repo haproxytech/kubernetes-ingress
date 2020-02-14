83917e656dd8fcb478acad16179fd904fb594aa2
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2020-02-14 16:13:47 +0100

    MINOR: Handle MODIFIED status of TCP services

--- controller-tcp.go
+++ controller-tcp.go
@@ -13,27 +13,45 @@ func (c *HAProxyController) handleTCPServices() (needsReload bool, err error) {
 		return false, nil
 	}
 	for port, svc := range c.cfg.ConfigMapTCPServices.Annotations {
+		// Get TCP service from ConfigMap
 		parts := strings.Split(svc.Value, ":")
 		portDest := parts[1]
 		parts = strings.Split(parts[0], "/")
 		namespace := parts[0]
 		service := parts[1]
 
+		if svc.Status == EMPTY {
+			continue
+		}
+
+		// Set frontend and backend names
 		backendName := strings.ReplaceAll(strings.ReplaceAll(svc.Value, "/", "-"), ":", "-")
-		frontendName := fmt.Sprintf("tcp-frontend-%s-%s", backendName, port)
-		//log.Println(port, svc, frontendName)
+		frontendName := fmt.Sprintf("tcp-%s", port)
+
+		// Handle Frontend
 		var frontend models.Frontend
-		frontend, err = c.frontendGet(frontendName)
 		portInt64 := int64(-1)
 		if prt, errParse := strconv.ParseInt(portDest, 10, 64); errParse == nil {
 			portInt64 = prt
 		}
 		switch svc.Status {
-		case ADDED:
-			if err == nil {
-				LogErr(err)
+		case DELETED:
+			err = c.frontendDelete(frontendName)
+			PanicErr(err)
+			needsReload = true
+			c.cfg.BackendSwitchingStatus["tcp-services"] = struct{}{}
+			continue
+		case MODIFIED:
+			if frontend, err = c.frontendGet(frontendName); err != nil {
+				PanicErr(err)
 				continue
 			}
+			frontend.DefaultBackend = backendName
+			if err = c.frontendEdit(frontend); err != nil {
+				PanicErr(err)
+				continue
+			}
+		case ADDED:
 			frontend = models.Frontend{
 				Name:           frontendName,
 				Mode:           "tcp",
@@ -41,41 +59,37 @@ func (c *HAProxyController) handleTCPServices() (needsReload bool, err error) {
 				DefaultBackend: backendName,
 			}
 			err = c.frontendCreate(frontend)
-			LogErr(err)
+			PanicErr(err)
 			err = c.frontendBindCreate(frontendName, models.Bind{
 				Address: "0.0.0.0:" + port,
 				Name:    "bind_1",
 			})
-			LogErr(err)
+			PanicErr(err)
 			err = c.frontendBindCreate(frontendName, models.Bind{
 				Address: ":::" + port,
 				Name:    "bind_2",
 				V4v6:    true,
 			})
-			LogErr(err)
-			ingress := &Ingress{
-				Namespace:   namespace,
-				Annotations: MapStringW{},
-				Rules:       map[string]*IngressRule{},
-			}
-			path := &IngressPath{
-				ServiceName:    service,
-				ServicePortInt: portInt64,
-				IsTCPService:   true,
-			}
-			nsmmp := c.cfg.GetNamespace(namespace)
-			_, err = c.handlePath(nsmmp, ingress, nil, path)
-			LogErr(err)
-			needsReload = true
-		case MODIFIED:
-			LogErr(err)
-			//TODO delete old, create new
-		case DELETED:
-			LogErr(err)
-			err = c.frontendDelete(frontendName)
-			LogErr(err)
+			PanicErr(err)
 			needsReload = true
 		}
+
+		// Handle Backend
+		ingress := &Ingress{
+			Namespace:   namespace,
+			Annotations: MapStringW{},
+			Rules:       map[string]*IngressRule{},
+		}
+		path := &IngressPath{
+			ServiceName:    service,
+			ServicePortInt: portInt64,
+			IsTCPService:   true,
+			Status:         svc.Status,
+		}
+		nsmmp := c.cfg.GetNamespace(namespace)
+		_, err = c.handlePath(nsmmp, ingress, nil, path)
+		PanicErr(err)
+		needsReload = true
 	}
 	return needsReload, err
 }
