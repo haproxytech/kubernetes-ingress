58bcd32aaece4fcf95f75be1197af894fc6040f9
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2020-04-12 15:47:15 +0200

    REORG: Put service related code in a seperate file

--- controller/controller.go
+++ controller/controller.go
@@ -28,7 +28,6 @@ import (
 	"github.com/haproxytech/client-native/runtime"
 	parser "github.com/haproxytech/config-parser/v2"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-	"github.com/haproxytech/models"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
@@ -44,12 +43,20 @@ type HAProxyController struct {
 	serverlessPods              map[string]int
 }
 
+// Return Parser of current configuration (for config-parser usage)
+func (c *HAProxyController) ActiveConfiguration() (*parser.Parser, error) {
+	if c.ActiveTransaction == "" {
+		return nil, fmt.Errorf("no active transaction")
+	}
+	return c.NativeAPI.Configuration.GetParser(c.ActiveTransaction)
+}
+
 // Start initialize and run HAProxyController
 func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 
 	c.osArgs = osArgs
 
-	c.HAProxyInitialize()
+	c.haproxyInitialize()
 
 	var k8s *K8s
 	var err error
@@ -81,8 +88,117 @@ func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 	<-ctx.Done()
 }
 
+// Sync HAProxy configuration
+func (c *HAProxyController) updateHAProxy() error {
+	reload := false
+
+	err := c.apiStartTransaction()
+	if err != nil {
+		utils.LogErr(err)
+		return err
+	}
+	defer func() {
+		c.apiDisposeTransaction()
+	}()
+
+	reload, restart := c.handleGlobalAnnotations()
+
+	r, err := c.handleDefaultService()
+	utils.LogErr(err)
+	reload = reload || r
+
+	usedCerts := map[string]struct{}{}
+
+	for _, namespace := range c.cfg.Namespace {
+		if !namespace.Relevant {
+			continue
+		}
+		for _, ingress := range namespace.Ingresses {
+			if c.cfg.PublishService != nil && ingress.Status != DELETED {
+				utils.LogErr(c.k8s.UpdateIngressStatus(ingress, c.cfg.PublishService))
+			}
+			// handle Default Backend
+			if ingress.DefaultBackend != nil {
+				r, err = c.handlePath(namespace, ingress, &IngressRule{}, ingress.DefaultBackend)
+				utils.LogErr(err)
+				reload = reload || r
+			}
+			// handle Ingress rules
+			for _, rule := range ingress.Rules {
+				for _, path := range rule.Paths {
+					r, err = c.handlePath(namespace, ingress, rule, path)
+					reload = reload || r
+					utils.LogErr(err)
+				}
+			}
+			//handle certs
+			ingressSecrets := map[string]struct{}{}
+			for _, tls := range ingress.TLS {
+				if _, ok := ingressSecrets[tls.SecretName.Value]; !ok {
+					ingressSecrets[tls.SecretName.Value] = struct{}{}
+					r = c.handleTLSSecret(*ingress, *tls, usedCerts)
+					reload = reload || r
+				}
+			}
+
+			utils.LogErr(c.handleRateLimiting(ingress))
+			utils.LogErr(c.handleRequestCapture(ingress))
+			utils.LogErr(c.handleRequestSetHdr(ingress))
+			utils.LogErr(c.handleBlacklisting(ingress))
+			utils.LogErr(c.handleWhitelisting(ingress))
+			utils.LogErr(c.handleHTTPRedirect(ingress))
+		}
+	}
+
+	utils.LogErr(c.handleProxyProtocol())
+
+	r = c.handleDefaultCertificate(usedCerts)
+	reload = reload || r
+
+	r = c.handleHTTPS(usedCerts)
+	reload = reload || r
+
+	reload = c.RequestsHTTPRefresh() || reload
+
+	reload = c.RequestsTCPRefresh() || reload
+
+	r, err = c.cfg.MapFiles.Refresh()
+	utils.LogErr(err)
+	reload = reload || r
+
+	r, err = c.handleTCPServices()
+	utils.LogErr(err)
+	reload = reload || r
+
+	r = c.refreshBackendSwitching()
+	reload = reload || r
+
+	err = c.apiCommitTransaction()
+	if err != nil {
+		utils.LogErr(err)
+		return err
+	}
+	c.cfg.Clean()
+	if restart {
+		if err := c.haproxyService("restart"); err != nil {
+			utils.LogErr(err)
+		} else {
+			log.Println("HAProxy restarted")
+		}
+		return nil
+	}
+	if reload {
+		if err := c.haproxyService("reload"); err != nil {
+			utils.LogErr(err)
+		} else {
+			log.Println("HAProxy reloaded")
+		}
+	}
+	return nil
+}
+
 //HAProxyInitialize runs HAProxy for the first time so native client can have access to it
-func (c *HAProxyController) HAProxyInitialize() {
+func (c *HAProxyController) haproxyInitialize() {
 	//cmd := exec.Command("haproxy", "-f", HAProxyCFG)
 	err := os.MkdirAll(HAProxyCertDir, 0755)
 	if err != nil {
@@ -106,7 +222,7 @@ func (c *HAProxyController) HAProxyInitialize() {
 	}
 
 	log.Println("Starting HAProxy with", HAProxyCFG)
-	utils.PanicErr(c.HAProxyService("start"))
+	utils.PanicErr(c.haproxyService("start"))
 
 	hostname, err := os.Hostname()
 	utils.LogErr(err)
@@ -139,13 +255,27 @@ func (c *HAProxyController) HAProxyInitialize() {
 
 }
 
-func (c *HAProxyController) ActiveConfiguration() (*parser.Parser, error) {
-	if c.ActiveTransaction == "" {
-		return nil, fmt.Errorf("no active transaction")
+// Handle HAProxy system service
+func (c *HAProxyController) haproxyService(action string) (err error) {
+	if c.osArgs.Test {
+		log.Println("HAProxy would be reload" + action + "ed now")
+		return nil
 	}
-	return c.NativeAPI.Configuration.GetParser(c.ActiveTransaction)
+	switch action {
+	case "reload", "restart":
+		utils.LogErr(c.saveServerState())
+	case "start":
+	default:
+		return fmt.Errorf("unkown command '%s'", action)
+	}
+	cmd := exec.Command("service", "haproxy", action)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	return err
 }
 
+// Saves HAProxy servers state so it is retrieved after reload.
 func (c *HAProxyController) saveServerState() error {
 	result, err := c.NativeAPI.Runtime.ExecuteRaw("show servers state")
 	if err != nil {
@@ -170,245 +300,4 @@ func (c *HAProxyController) saveServerState() error {
 		return err
 	}
 	return nil
-}
-
-func (c *HAProxyController) HAProxyService(action string) (err error) {
-	if c.osArgs.Test {
-		log.Println("HAProxy would be reload" + action + "ed now")
-		return nil
-	}
-	switch action {
-	case "reload", "restart":
-		utils.LogErr(c.saveServerState())
-	case "start":
-	default:
-		return fmt.Errorf("unkown command '%s'", action)
-	}
-	cmd := exec.Command("service", "haproxy", action)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Start()
-	return err
-}
-
-func (c *HAProxyController) handlePath(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath) (reload bool, err error) {
-	reload = false
-	service, ok := namespace.Services[path.ServiceName]
-	if !ok {
-		log.Printf("service '%s' does not exist", path.ServiceName)
-		return reload, fmt.Errorf("service '%s' does not exist", path.ServiceName)
-	}
-
-	backendName, newBackend, r, err := c.handleService(namespace, ingress, rule, path, service)
-	reload = reload || r
-	if err != nil {
-		return reload, err
-	}
-
-	endpoints, ok := namespace.Endpoints[service.Name]
-	if !ok {
-		log.Printf("No Endpoints found for service '%s'", service.Name)
-		return reload, nil // not an end of world scenario, just log this
-	}
-	endpoints.BackendName = backendName
-	if err := c.setTargetPort(path, service, endpoints); err != nil {
-		return reload, err
-	}
-
-	for _, ip := range *endpoints.Addresses {
-		r := c.handleEndpointIP(namespace, ingress, rule, path, service, backendName, newBackend, endpoints, ip)
-		reload = reload || r
-	}
-	return reload, nil
-}
-
-// handleEndpointIP processes the IngressPath related endpoints and makes corresponding backend servers configuration in HAProxy
-func (c *HAProxyController) handleEndpointIP(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service, backendName string, newBackend bool, endpoints *Endpoints, ip *EndpointIP) (reload bool) {
-	reload = false
-	server := models.Server{
-		Name:    ip.HAProxyName,
-		Address: ip.IP,
-		Port:    &path.TargetPort,
-		Weight:  utils.PtrInt64(128),
-	}
-	if ip.Disabled {
-		server.Maintenance = "enabled"
-	}
-	annotationsActive := c.handleServerAnnotations(ingress, service, &server)
-	status := ip.Status
-	if status == EMPTY {
-		if newBackend {
-			status = ADDED
-		} else if annotationsActive {
-			status = MODIFIED
-		}
-	}
-	switch status {
-	case ADDED:
-		err := c.backendServerCreate(backendName, server)
-		if err != nil {
-			if !strings.Contains(err.Error(), "already exists") {
-				utils.LogErr(err)
-				reload = true
-			}
-		} else {
-			reload = true
-		}
-	case MODIFIED:
-		err := c.backendServerEdit(backendName, server)
-		if err != nil {
-			if strings.Contains(err.Error(), "does not exist") {
-				err1 := c.backendServerCreate(backendName, server)
-				utils.LogErr(err1)
-				reload = true
-			} else {
-				utils.LogErr(err)
-			}
-		}
-		status := "ready"
-		if ip.Disabled {
-			status = "maint"
-		}
-		log.Printf("Modified: %s - %s - %v\n", backendName, ip.HAProxyName, status)
-	case DELETED:
-		err := c.backendServerDelete(backendName, server.Name)
-		if err != nil && !strings.Contains(err.Error(), "does not exist") {
-			utils.LogErr(err)
-		}
-		return true
-	}
-	return reload
-}
-
-// handleService processes the service related to the IngressPath and makes corresponding backend configuration in HAProxy
-func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service) (backendName string, newBackend bool, reload bool, err error) {
-
-	// Get Backend status
-	status := service.Status
-	if status == EMPTY {
-		status = path.Status
-	}
-
-	// If status DELETED
-	// remove use_backend rule and leave.
-	// Backend will be deleted when no more use_backend
-	// rules are left for the backend in question.
-	// This is done via c.refreshBackendSwitching
-	if status == DELETED {
-		key := fmt.Sprintf("R%s%s%s%s", namespace.Name, ingress.Name, rule.Host, path.Path)
-		switch {
-		case path.IsSSLPassthrough:
-			c.deleteUseBackendRule(key, FrontendSSL)
-		case path.IsDefaultBackend:
-			log.Printf("Removing default_backend %s from ingress \n", service.Name)
-			utils.LogErr(c.setDefaultBackend(""))
-			reload = true
-		default:
-			c.deleteUseBackendRule(key, FrontendHTTP, FrontendHTTPS)
-		}
-		return "", false, reload, nil
-	}
-
-	// Set backendName
-	if path.ServicePortInt == 0 {
-		backendName = fmt.Sprintf("%s-%s-%s", namespace.Name, service.Name, path.ServicePortString)
-	} else {
-		backendName = fmt.Sprintf("%s-%s-%d", namespace.Name, service.Name, path.ServicePortInt)
-	}
-
-	// Get/Create Backend
-	newBackend = false
-	reload = false
-	var backend models.Backend
-	if backend, err = c.backendGet(backendName); err != nil {
-		mode := "http"
-		backend = models.Backend{
-			Name: backendName,
-			Mode: mode,
-		}
-		if path.IsTCPService || path.IsSSLPassthrough {
-			backend.Mode = string(ModeTCP)
-		}
-		if err = c.backendCreate(backend); err != nil {
-			return "", true, reload, err
-		}
-		newBackend = true
-		reload = true
-	}
-
-	// handle Annotations
-	activeSSLPassthrough := c.handleSSLPassthrough(ingress, service, path, &backend, newBackend)
-	activeBackendAnn := c.handleBackendAnnotations(ingress, service, &backend, newBackend)
-	if activeBackendAnn || activeSSLPassthrough {
-		if err = c.backendEdit(backend); err != nil {
-			return backendName, newBackend, reload, err
-		}
-		reload = true
-	}
-
-	// No need to update BackendSwitching
-	if (status == EMPTY && !activeSSLPassthrough) || path.IsTCPService {
-		return backendName, newBackend, reload, nil
-	}
-
-	// Update backendSwitching
-	key := fmt.Sprintf("R%s%s%s%s", namespace.Name, ingress.Name, rule.Host, path.Path)
-	useBackendRule := UseBackendRule{
-		Host:      rule.Host,
-		Path:      path.Path,
-		Backend:   backendName,
-		Namespace: namespace.Name,
-	}
-	switch {
-	case path.IsDefaultBackend:
-		log.Printf("Confiugring default_backend %s from ingress %s\n", service.Name, ingress.Name)
-		utils.LogErr(c.setDefaultBackend(backendName))
-		reload = true
-	case path.IsSSLPassthrough:
-		c.addUseBackendRule(key, useBackendRule, FrontendSSL)
-		if activeSSLPassthrough {
-			c.deleteUseBackendRule(key, FrontendHTTP, FrontendHTTPS)
-		}
-	default:
-		c.addUseBackendRule(key, useBackendRule, FrontendHTTP, FrontendHTTPS)
-		if activeSSLPassthrough {
-			c.deleteUseBackendRule(key, FrontendSSL)
-		}
-	}
-
-	if err != nil {
-		return "", newBackend, reload, err
-	}
-
-	return backendName, newBackend, reload, nil
-}
-
-// Looks for the targetPort (Endpoint port) corresponding to the servicePort of the IngressPath
-func (c *HAProxyController) setTargetPort(path *IngressPath, service *Service, endpoints *Endpoints) error {
-	for _, sp := range service.Ports {
-		// Find corresponding servicePort
-		if sp.Name == path.ServicePortString || sp.Port == path.ServicePortInt {
-			// Find the corresponding targetPort in Endpoints ports
-			if endpoints != nil {
-				for _, epPort := range *endpoints.Ports {
-					if epPort.Name == sp.Name {
-						// Dinamically update backend port
-						if path.TargetPort != epPort.Port && path.TargetPort != 0 {
-							for _, EndpointIP := range *endpoints.Addresses {
-								if err := c.NativeAPI.Runtime.SetServerAddr(endpoints.BackendName, EndpointIP.HAProxyName, EndpointIP.IP, int(epPort.Port)); err != nil {
-									log.Println(err)
-								}
-								log.Printf("TargetPort for backend %s changed to %d", endpoints.BackendName, epPort.Port)
-							}
-						}
-						path.TargetPort = epPort.Port
-						return nil
-					}
-				}
-				log.Printf("Could not find Targetport of '%s' for service %s", sp.Name, service.Name)
-			} // Return nil even if corresponding target port was not found.
-			return nil
-		}
-	}
-	return fmt.Errorf("servicePort(Str: %s, Int: %d) for serviceName '%s' not found", path.ServicePortString, path.ServicePortInt, service.Name)
 }
---
+++ controller/service.go
@@ -0,0 +1,280 @@
+// Copyright 2019 HAProxy Technologies LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
+	"github.com/haproxytech/models"
+)
+
+// handle defaultBackned configured via cli param "default-backend-service"
+func (c *HAProxyController) handleDefaultService() (reload bool, err error) {
+	reload = false
+	dsvcData, _ := GetValueFromAnnotations("default-backend-service")
+	dsvc := strings.Split(dsvcData.Value, "/")
+
+	if len(dsvc) != 2 {
+		return reload, fmt.Errorf("default service invalid data")
+	}
+	if dsvc[0] == "" || dsvc[1] == "" {
+		return reload, nil
+	}
+	namespace, ok := c.cfg.Namespace[dsvc[0]]
+	if !ok {
+		return reload, fmt.Errorf("default service invalid namespace " + dsvc[0])
+	}
+	service, ok := namespace.Services[dsvc[1]]
+	if !ok {
+		return reload, fmt.Errorf("service '" + dsvc[1] + "' does not exist")
+	}
+	ingress := &Ingress{
+		Namespace:   namespace.Name,
+		Name:        "DefaultService",
+		Annotations: MapStringW{},
+		Rules:       map[string]*IngressRule{},
+	}
+	path := &IngressPath{
+		ServiceName:      service.Name,
+		ServicePortInt:   service.Ports[0].Port,
+		IsDefaultBackend: true,
+	}
+	return c.handlePath(namespace, ingress, &IngressRule{}, path)
+}
+
+// handle the IngressPath related endpoints and make corresponding backend servers configuration in HAProxy
+func (c *HAProxyController) handleEndpointIP(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service, backendName string, newBackend bool, endpoints *Endpoints, ip *EndpointIP) (reload bool) {
+	reload = false
+	server := models.Server{
+		Name:    ip.HAProxyName,
+		Address: ip.IP,
+		Port:    &path.TargetPort,
+		Weight:  utils.PtrInt64(128),
+	}
+	if ip.Disabled {
+		server.Maintenance = "enabled"
+	}
+	annotationsActive := c.handleServerAnnotations(ingress, service, &server)
+	status := ip.Status
+	if status == EMPTY {
+		if newBackend {
+			status = ADDED
+		} else if annotationsActive {
+			status = MODIFIED
+		}
+	}
+	switch status {
+	case ADDED:
+		err := c.backendServerCreate(backendName, server)
+		if err != nil {
+			if !strings.Contains(err.Error(), "already exists") {
+				utils.LogErr(err)
+				reload = true
+			}
+		} else {
+			reload = true
+		}
+	case MODIFIED:
+		err := c.backendServerEdit(backendName, server)
+		if err != nil {
+			if strings.Contains(err.Error(), "does not exist") {
+				err1 := c.backendServerCreate(backendName, server)
+				utils.LogErr(err1)
+				reload = true
+			} else {
+				utils.LogErr(err)
+			}
+		}
+		status := "ready"
+		if ip.Disabled {
+			status = "maint"
+		}
+		log.Printf("Modified: %s - %s - %v\n", backendName, ip.HAProxyName, status)
+	case DELETED:
+		err := c.backendServerDelete(backendName, server.Name)
+		if err != nil && !strings.Contains(err.Error(), "does not exist") {
+			utils.LogErr(err)
+		}
+		return true
+	}
+	return reload
+}
+
+// handle service of an IngressPath and make corresponding backend configuration in HAProxy
+func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath, service *Service) (backendName string, newBackend bool, reload bool, err error) {
+
+	// Get Backend status
+	status := service.Status
+	if status == EMPTY {
+		status = path.Status
+	}
+
+	// If status DELETED
+	// remove use_backend rule and leave.
+	// Backend will be deleted when no more use_backend
+	// rules are left for the backend in question.
+	// This is done via c.refreshBackendSwitching
+	if status == DELETED {
+		key := fmt.Sprintf("R%s%s%s%s", namespace.Name, ingress.Name, rule.Host, path.Path)
+		switch {
+		case path.IsSSLPassthrough:
+			c.deleteUseBackendRule(key, FrontendSSL)
+		case path.IsDefaultBackend:
+			log.Printf("Removing default_backend %s from ingress \n", service.Name)
+			err = c.setDefaultBackend("")
+			reload = true
+		default:
+			c.deleteUseBackendRule(key, FrontendHTTP, FrontendHTTPS)
+		}
+		return "", false, reload, err
+	}
+
+	// Set backendName
+	if path.ServicePortInt == 0 {
+		backendName = fmt.Sprintf("%s-%s-%s", namespace.Name, service.Name, path.ServicePortString)
+	} else {
+		backendName = fmt.Sprintf("%s-%s-%d", namespace.Name, service.Name, path.ServicePortInt)
+	}
+
+	// Get/Create Backend
+	newBackend = false
+	reload = false
+	var backend models.Backend
+	if backend, err = c.backendGet(backendName); err != nil {
+		mode := "http"
+		backend = models.Backend{
+			Name: backendName,
+			Mode: mode,
+		}
+		if path.IsTCPService || path.IsSSLPassthrough {
+			backend.Mode = string(ModeTCP)
+		}
+		if err = c.backendCreate(backend); err != nil {
+			return "", true, reload, err
+		}
+		newBackend = true
+		reload = true
+	}
+
+	// handle Annotations
+	activeSSLPassthrough := c.handleSSLPassthrough(ingress, service, path, &backend, newBackend)
+	activeBackendAnn := c.handleBackendAnnotations(ingress, service, &backend, newBackend)
+	if activeBackendAnn || activeSSLPassthrough {
+		if err = c.backendEdit(backend); err != nil {
+			return backendName, newBackend, reload, err
+		}
+		reload = true
+	}
+
+	// No need to update BackendSwitching
+	if (status == EMPTY && !activeSSLPassthrough) || path.IsTCPService {
+		return backendName, newBackend, reload, nil
+	}
+
+	// Update backendSwitching
+	key := fmt.Sprintf("R%s%s%s%s", namespace.Name, ingress.Name, rule.Host, path.Path)
+	useBackendRule := UseBackendRule{
+		Host:      rule.Host,
+		Path:      path.Path,
+		Backend:   backendName,
+		Namespace: namespace.Name,
+	}
+	switch {
+	case path.IsDefaultBackend:
+		log.Printf("Confiugring default_backend %s from ingress %s\n", service.Name, ingress.Name)
+		err = c.setDefaultBackend(backendName)
+		reload = true
+	case path.IsSSLPassthrough:
+		c.addUseBackendRule(key, useBackendRule, FrontendSSL)
+		if activeSSLPassthrough {
+			c.deleteUseBackendRule(key, FrontendHTTP, FrontendHTTPS)
+		}
+	default:
+		c.addUseBackendRule(key, useBackendRule, FrontendHTTP, FrontendHTTPS)
+		if activeSSLPassthrough {
+			c.deleteUseBackendRule(key, FrontendSSL)
+		}
+	}
+
+	if err != nil {
+		return "", newBackend, reload, err
+	}
+
+	return backendName, newBackend, reload, nil
+}
+
+// handle IngressPath and make corresponding HAProxy configuration
+func (c *HAProxyController) handlePath(namespace *Namespace, ingress *Ingress, rule *IngressRule, path *IngressPath) (reload bool, err error) {
+	reload = false
+	service, ok := namespace.Services[path.ServiceName]
+	if !ok {
+		return reload, fmt.Errorf("service '%s' does not exist", path.ServiceName)
+	}
+
+	backendName, newBackend, r, err := c.handleService(namespace, ingress, rule, path, service)
+	reload = reload || r
+	if err != nil {
+		return reload, err
+	}
+
+	endpoints, ok := namespace.Endpoints[service.Name]
+	if !ok {
+		log.Printf("No Endpoints found for service '%s'", service.Name)
+		return reload, nil // not an end of world scenario, just log this
+	}
+	endpoints.BackendName = backendName
+	if err := c.setTargetPort(path, service, endpoints); err != nil {
+		return reload, err
+	}
+
+	for _, ip := range *endpoints.Addresses {
+		r := c.handleEndpointIP(namespace, ingress, rule, path, service, backendName, newBackend, endpoints, ip)
+		reload = reload || r
+	}
+	return reload, nil
+}
+
+// Look for the targetPort (Endpoint port) corresponding to the servicePort of the IngressPath
+func (c *HAProxyController) setTargetPort(path *IngressPath, service *Service, endpoints *Endpoints) error {
+	for _, sp := range service.Ports {
+		// Find corresponding servicePort
+		if sp.Name == path.ServicePortString || sp.Port == path.ServicePortInt {
+			// Find the corresponding targetPort in Endpoints ports
+			if endpoints != nil {
+				for _, epPort := range *endpoints.Ports {
+					if epPort.Name == sp.Name {
+						// Dinamically update backend port
+						if path.TargetPort != epPort.Port && path.TargetPort != 0 {
+							for _, EndpointIP := range *endpoints.Addresses {
+								if err := c.NativeAPI.Runtime.SetServerAddr(endpoints.BackendName, EndpointIP.HAProxyName, EndpointIP.IP, int(epPort.Port)); err != nil {
+									log.Println(err)
+								}
+								log.Printf("TargetPort for backend %s changed to %d", endpoints.BackendName, epPort.Port)
+							}
+						}
+						path.TargetPort = epPort.Port
+						return nil
+					}
+				}
+				log.Printf("Could not find Targetport of '%s' for service %s", sp.Name, service.Name)
+			} // Return nil even if corresponding target port was not found.
+			return nil
+		}
+	}
+	return fmt.Errorf("servicePort(Str: %s, Int: %d) for serviceName '%s' not found", path.ServicePortString, path.ServicePortInt, service.Name)
+}
