haproxytech/kubernetes-ingress

commit 4c5f2197d96a6ddfda258b973cd49ad3795fb860

MEDIUM: Add logging messages for debug

controller/service.go
@@ -73,33 +73,28 @@ func (c *HAProxyController) handleEndpoint(namespace *Namespace, ingress *Ingres
 		ip.Status = MODIFIED
 	}
 
+	var errAPI error
 	switch ip.Status {
 	case ADDED, MODIFIED:
-		errEdit := c.Client.BackendServerEdit(backendName, server)
-		if errEdit != nil {
-			if strings.Contains(errEdit.Error(), "does not exist") {
-				if errCreate := c.Client.BackendServerCreate(backendName, server); errCreate != nil {
-					c.Logger.Err(errCreate)
-					return false
-				}
-				return true
+		errAPI = c.Client.BackendServerEdit(backendName, server)
+		if errAPI != nil {
+			if strings.Contains(errAPI.Error(), "does not exist") {
+				c.Logger.Debugf("Creating server '%s/%s'", backendName, server.Name)
+				errAPI = c.Client.BackendServerCreate(backendName, server)
+				break
 			}
-			c.Logger.Error(errEdit)
-			return false
+			c.Logger.Debugf("Updating server '%s/%s'", backendName, server.Name)
 		}
-		status := "ready"
-		if ip.Disabled {
-			status = "maint"
-		}
-		c.Logger.Debugf("Modified: %s - %s - %v", backendName, ip.HAProxyName, status)
+
 	case DELETED:
-		err := c.Client.BackendServerDelete(backendName, server.Name)
-		if err != nil && !strings.Contains(err.Error(), "does not exist") {
-			c.Logger.Error(err)
-		}
-		return true
+		c.Logger.Debugf("Deleting server '%s/%s'", backendName, server.Name)
+		errAPI = c.Client.BackendServerDelete(backendName, server.Name)
 	}
-	return reload
+	if errAPI != nil {
+		c.Logger.Err(errAPI)
+		return false
+	}
+	return false
 }
 
 // handle service of an IngressPath and make corresponding backend configuration in HAProxy
@@ -122,7 +117,7 @@ func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress
 		case path.IsSSLPassthrough:
 			c.deleteUseBackendRule(key, FrontendSSL)
 		case path.IsDefaultBackend:
-			c.Logger.Debugf("Removing default_backend %s from ingress", service.Name)
+			c.Logger.Debugf("Removing default backend '%s/%s'", namespace.Name, service.Name)
 			err = c.setDefaultBackend("")
 			reload = true
 		default:
@@ -151,6 +146,7 @@ func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress
 		if path.IsTCPService || path.IsSSLPassthrough {
 			backend.Mode = string(TCP)
 		}
+		c.Logger.Debugf("Ingress '%s/%s': Creating new backend '%s'", namespace.Name, ingress.Name, backendName)
 		if err = c.Client.BackendCreate(backend); err != nil {
 			return "", true, reload, err
 		}
@@ -162,6 +158,7 @@ func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress
 	activeSSLPassthrough := c.handleSSLPassthrough(ingress, service, path, &backend, newBackend)
 	activeBackendAnn := c.handleBackendAnnotations(ingress, service, &backend, newBackend)
 	if activeBackendAnn || activeSSLPassthrough {
+		c.Logger.Debugf("Ingress '%s/%s': Applying annotations changes to backend '%s'", namespace.Name, ingress.Name, backendName)
 		if err = c.Client.BackendEdit(backend); err != nil {
 			return backendName, newBackend, reload, err
 		}
@@ -183,7 +180,7 @@ func (c *HAProxyController) handleService(namespace *Namespace, ingress *Ingress
 	}
 	switch {
 	case path.IsDefaultBackend:
-		c.Logger.Debugf("Configuring default_backend %s from ingress %s", service.Name, ingress.Name)
+		c.Logger.Debugf("Using service '%s/%s' as default backend", namespace.Name, service.Name)
 		err = c.setDefaultBackend(backendName)
 		reload = true
 	case path.IsSSLPassthrough:

controller/backend-annotations.go
@@ -80,6 +80,7 @@ func (c *HAProxyController) handleBackendAnnotations(ingress *Ingress, service *
 			continue
 		}
 		if v.Status != EMPTY || newBackend {
+			c.Logger.Debugf("Backend '%s': Configuring '%s' annotation", backend.Name, k)
 			switch k {
 			case "abortonclose":
 				if err := backend.UpdateAbortOnClose(v.Value); err != nil {
@@ -198,6 +199,7 @@ func (c *HAProxyController) handleServerAnnotations(ingress *Ingress, service *S
 			continue
 		}
 		if v.Status != EMPTY || serverStatus != EMPTY {
+			c.Logger.Tracef("Server '%s': Configuring '%s' annotation", server.Name, k)
 			switch k {
 			case "cookie-persistence":
 				if v.Status == DELETED {

controller/backend-switching.go
@@ -50,6 +50,7 @@ func (c *HAProxyController) refreshBackendSwitching() (reload bool) {
 	if len(c.cfg.BackendSwitchingStatus) == 0 {
 		return false
 	}
+	c.Logger.Debug("Updating Backend Switching rules")
 	frontends, err := c.Client.FrontendsGet()
 	if err != nil {
 		c.Logger.Panic(err)
@@ -130,6 +131,7 @@ func (c *HAProxyController) clearBackends(activeBackends map[string]struct{}) (r
 	}
 	for _, backend := range allBackends {
 		if _, ok := activeBackends[backend.Name]; !ok {
+			c.Logger.Debugf("Deleting backend '%s'", backend.Name)
 			if err := c.Client.BackendDelete(backend.Name); err != nil {
 				c.Logger.Panic(err)
 			}

controller/controller.go
@@ -75,6 +75,7 @@ func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 
 	if osArgs.OutOfCluster {
 		kubeconfig := filepath.Join(utils.HomeDir(), ".kube", "config")
+		c.Logger.Info("Running Controller out of K8s cluster")
 		if osArgs.KubeConfig != "" {
 			kubeconfig = osArgs.KubeConfig
 		}
@@ -102,6 +103,7 @@ func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 
 // Sync HAProxy configuration
 func (c *HAProxyController) updateHAProxy() error {
+	c.Logger.Trace("HAProxy config sync started")
 	reload := false
 
 	err := c.Client.APIStartTransaction()
@@ -211,6 +213,8 @@ func (c *HAProxyController) updateHAProxy() error {
 			c.Logger.Info("HAProxy reloaded")
 		}
 	}
+
+	c.Logger.Trace("HAProxy config sync terminated")
 	return nil
 }
 

controller/events.go
@@ -372,6 +372,7 @@ func (c *HAProxyController) processEndpointIPs(data *Endpoints) (updateRequired 
 				if ip.Disabled {
 					status = "maint"
 				}
+				c.Logger.Debugf("server '%s/%s' changed status to %v", data.BackendName, ip.HAProxyName, status)
 				err = c.Client.SetServerState(data.BackendName, ip.HAProxyName, status)
 				if err != nil {
 					c.Logger.Error(err)

controller/frontend-annotations.go
@@ -59,8 +59,10 @@ func (c *HAProxyController) handleBlacklisting(ingress *Ingress) error {
 		c.cfg.FrontendRulesStatus[HTTP] = MODIFIED
 		c.cfg.FrontendRulesStatus[TCP] = MODIFIED
 		if status == DELETED {
+			c.Logger.Debugf("Ingress %s/%s: Deleting blacklist configuration", ingress.Namespace, ingress.Name)
 			return nil
 		}
+		c.Logger.Debugf("Ingress %s/%s: Configuring blacklist annotation", ingress.Namespace, ingress.Name)
 	}
 	for hostname := range ingress.Rules {
 		mapFiles.AppendHost(key, hostname)
@@ -179,8 +181,10 @@ func (c *HAProxyController) handleProxyProtocol() error {
 	if status != EMPTY {
 		c.cfg.FrontendRulesStatus[TCP] = MODIFIED
 		if status == DELETED {
+			c.Logger.Debugf("Deleting ProxyProtcol configuration")
 			return nil
 		}
+		c.Logger.Debugf("Configuring ProxyProtcol annotation")
 	}
 
 	tcpRule := models.TCPRequestRule{
@@ -230,9 +234,11 @@ func (c *HAProxyController) handleRateLimiting(ingress *Ingress) error {
 		mapFiles.Modified(trackKey)
 		c.cfg.FrontendRulesStatus[HTTP] = MODIFIED
 		if status == DELETED {
+			c.Logger.Debugf("Ingress %s/%s: Deleting rate-limit-requests configuration", ingress.Namespace, ingress.Name)
 			delete(rateLimitTables, tableName)
 			return nil
 		}
+		c.Logger.Debugf("Ingress %s/%s: Configuring rate-limit-requests annotation", ingress.Namespace, ingress.Name)
 	}
 	for hostname := range ingress.Rules {
 		mapFiles.AppendHost(reqsKey, hostname)
@@ -298,8 +304,10 @@ func (c *HAProxyController) handleRequestCapture(ingress *Ingress) error {
 			c.cfg.FrontendRulesStatus[HTTP] = MODIFIED
 			c.cfg.FrontendRulesStatus[TCP] = MODIFIED
 			if status == DELETED {
+				c.Logger.Debugf("Ingress %s/%s: Deleting configuration for '%s' request capture ", ingress.Namespace, ingress.Name, sample)
 				break
 			}
+			c.Logger.Debugf("Ingress %s/%s: Configuring request capture for '%s'", ingress.Namespace, ingress.Name, sample)
 		}
 		for hostname := range ingress.Rules {
 			mapFiles.AppendHost(key, hostname)
@@ -351,8 +359,10 @@ func (c *HAProxyController) handleRequestSetHdr(ingress *Ingress) error {
 			mapFiles.Modified(key)
 			c.cfg.FrontendRulesStatus[HTTP] = MODIFIED
 			if status == DELETED {
+				c.Logger.Debugf("Ingress %s/%s: Deleting configuration for request set '%s' header ", ingress.Namespace, ingress.Name, param)
 				break
 			}
+			c.Logger.Debugf("Ingress %s/%s: Configuring request set '%s' header ", ingress.Namespace, ingress.Name, param)
 		}
 		for hostname := range ingress.Rules {
 			mapFiles.AppendHost(key, hostname)
@@ -440,8 +450,10 @@ func (c *HAProxyController) handleWhitelisting(ingress *Ingress) error {
 		c.cfg.FrontendRulesStatus[HTTP] = MODIFIED
 		c.cfg.FrontendRulesStatus[TCP] = MODIFIED
 		if status == DELETED {
+			c.Logger.Debugf("Ingress %s/%s: Deleting whitelist configuration", ingress.Namespace, ingress.Name)
 			return nil
 		}
+		c.Logger.Debugf("Ingress %s/%s: Configuring whitelist configuration", ingress.Namespace, ingress.Name)
 	}
 	for hostname := range ingress.Rules {
 		mapFiles.AppendHost(key, hostname)

controller/global-annotations.go
@@ -42,6 +42,7 @@ func (c *HAProxyController) handleNbthread() bool {
 			errParser = c.Client.SetNbthread(nil)
 			reload = true
 		} else if annNbthread.Status != EMPTY {
+			c.Logger.Infof("Set NbThread to: '%d'", numThreads)
 			errParser = c.Client.SetNbthread(&numThreads)
 		}
 		c.Logger.Error(errParser)
@@ -109,6 +110,7 @@ func (c *HAProxyController) handleSyslog() (restart, reload bool) {
 					continue
 				}
 			}
+			c.Logger.Infof("Adding log target: '%s'", logData)
 			errParser = c.Client.SetLogTarget(logData, index)
 			if errParser == nil {
 				reload = true
@@ -118,11 +120,13 @@ func (c *HAProxyController) handleSyslog() (restart, reload bool) {
 	}
 	if stdoutLog {
 		if daemonMode {
+			c.Logger.Info("Disabling Daemon mode")
 			errParser = c.Client.SetDaemonMode(nil)
 			restart = true
 		}
 	} else if !daemonMode {
 		enabled := true
+		c.Logger.Info("Enabling Daemon mode")
 		errParser = c.Client.SetDaemonMode(&enabled)
 		restart = true
 	}
@@ -146,7 +150,7 @@ func (c *HAProxyController) handleDefaultOption(option string) bool {
 	}
 	var err error
 	if annOption.Status == DELETED {
-		c.Logger.Printf(fmt.Sprintf("Removing '%s' option", option))
+		c.Logger.Infof("Removing '%s' option", option)
 		err = c.Client.SetDefaulOption(option, nil)
 		if err != nil {
 			c.Logger.Error(err)
@@ -163,7 +167,7 @@ func (c *HAProxyController) handleDefaultOption(option string) bool {
 	if !enabled {
 		action = "Disabling"
 	}
-	c.Logger.Printf(fmt.Sprintf("%s %s", action, option))
+	c.Logger.Infof("%s %s", action, option)
 	err = c.Client.SetDefaulOption(option, &enabled)
 	if err != nil {
 		c.Logger.Err(err)
@@ -197,10 +201,10 @@ func (c *HAProxyController) handleDefaultTimeout(timeout string) bool {
 	if annTimeout.Status != "" {
 		var err error
 		if annTimeout.Status == DELETED {
-			c.Logger.Debugf("Removing default timeout-%s ", timeout)
+			c.Logger.Infof("Removing default timeout-%s ", timeout)
 			err = c.Client.SetDefaulTimeout(timeout, nil)
 		} else {
-			c.Logger.Debugf("Setting default timeout-%s to %s", timeout, annTimeout.Value)
+			c.Logger.Infof("Setting default timeout-%s to %s", timeout, annTimeout.Value)
 			err = c.Client.SetDefaulTimeout(timeout, &annTimeout.Value)
 		}
 		if err != nil {
@@ -232,14 +236,14 @@ func (c *HAProxyController) handleDefaultMaxconn() bool {
 			c.Logger.Error(err)
 			return false
 		}
-		c.Logger.Debug("Removing default maxconn")
+		c.Logger.Info("Removing default maxconn")
 	default:
 		err = c.Client.SetDefaulMaxconn(&value)
 		if err != nil {
 			c.Logger.Error(err)
 			return false
 		}
-		c.Logger.Debugf("Setting default maxconn to %d", value)
+		c.Logger.Infof("Setting default maxconn to %d", value)
 	}
 	return true
 }
@@ -249,6 +253,7 @@ func (c *HAProxyController) handleDefaultLogFormat() bool {
 	if annLogFormat.Status == EMPTY {
 		return false
 	}
+	c.Logger.Infof("Changing default log format to '%s'", annLogFormat.Value)
 	err := c.Client.SetDefaulLogFormat(&annLogFormat.Value)
 	if err != nil {
 		c.Logger.Error(err)

controller/https.go
@@ -91,6 +91,7 @@ func (c *HAProxyController) handleSecret(ingress Ingress, secret Secret, writeSe
 					c.Logger.Error(err)
 					return false
 				}
+				c.Logger.Debugf("Using certtificate from secret '%s/%s'", secret.Namespace, secret.Name)
 				reload = true
 			}
 			certs[filename] = struct{}{}
@@ -161,11 +162,13 @@ func (c *HAProxyController) handleHTTPS(usedCerts map[string]struct{}) (reload b
 	// ssl-passthrough
 	if len(c.cfg.BackendSwitchingRules[FrontendSSL]) > 0 {
 		if !c.cfg.SSLPassthrough {
+			c.Logger.Info("Enabling ssl-passthrough")
 			c.Logger.Panic(c.enableSSLPassthrough())
 			c.cfg.SSLPassthrough = true
 			reload = true
 		}
 	} else if c.cfg.SSLPassthrough {
+		c.Logger.Info("Disabling ssl-passthrough")
 		c.Logger.Panic(c.disableSSLPassthrough())
 		c.cfg.SSLPassthrough = false
 		reload = true
@@ -178,6 +181,7 @@ func (c *HAProxyController) handleHTTPS(usedCerts map[string]struct{}) (reload b
 			reload = true
 		}
 	} else if c.cfg.HTTPS {
+		c.Logger.Info("Disabling ssl offload")
 		c.Logger.Panic(c.disableSSLOffload(FrontendHTTPS))
 		c.cfg.HTTPS = false
 		reload = true

commit fede729a05aef0f3d550274da383059695d55d05

BUG/MEDIUM: Controller won't reload HAProxy as it should be when application endpoints are updated
Regression introduced in 4c5f2197

controller/service.go
@@ -89,12 +89,14 @@ func (c *HAProxyController) handleEndpoint(namespace *Namespace, ingress *Ingres
 	case DELETED:
 		c.Logger.Debugf("Deleting server '%s/%s'", backendName, server.Name)
 		errAPI = c.Client.BackendServerDelete(backendName, server.Name)
+	default:
+		return false
 	}
 	if errAPI != nil {
 		c.Logger.Err(errAPI)
 		return false
 	}
-	return false
+	return true
 }
 
 // handle service of an IngressPath and make corresponding backend configuration in HAProxy