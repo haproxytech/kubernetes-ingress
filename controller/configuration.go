haproxytech/kubernetes-ingress

commit db51a5dbb6a66ee4959730a96187b3fb141b5906

MEDIUM: frontend: custom bind port, and frontend and IP protocol toggling

controller/configuration.go
@@ -36,7 +36,7 @@ type Configuration struct {
 }
 
 //Init initialize configuration
-func (c *Configuration) Init(mapDir string) {
+func (c *Configuration) Init(mapDir string, httpsEnabled bool) {
 
 	c.FrontendHTTPReqRules = make(map[Rule]FrontendHTTPReqs)
 	for _, rule := range []Rule{BLACKLIST, SSL_REDIRECT, RATE_LIMIT, REQUEST_CAPTURE, REQUEST_SET_HEADER, WHITELIST} {
@@ -65,6 +65,8 @@ func (c *Configuration) Init(mapDir string) {
 		c.BackendSwitchingRules[frontend] = UseBackendRules{}
 	}
 	c.BackendHTTPRules = make(map[string]BackendHTTPReqs)
+
+	c.HTTPS = httpsEnabled
 }
 
 //Clean cleans all the statuses of various data that was changed

controller/controller.go
@@ -25,10 +25,12 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/haproxytech/models/v2"
+	"k8s.io/apimachinery/pkg/watch"
+
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-	"k8s.io/apimachinery/pkg/watch"
 )
 
 const (
@@ -41,11 +43,11 @@ const (
 	FrontendHTTPS = "https"
 	FrontendSSL   = "ssl"
 	//Status
-	ADDED    store.Status = store.ADDED
-	DELETED  store.Status = store.DELETED
-	ERROR    store.Status = store.ERROR
-	EMPTY    store.Status = store.EMPTY
-	MODIFIED store.Status = store.MODIFIED
+	ADDED    = store.ADDED
+	DELETED  = store.DELETED
+	ERROR    = store.ERROR
+	EMPTY    = store.EMPTY
+	MODIFIED = store.MODIFIED
 )
 
 var (
@@ -95,6 +97,20 @@ func (c *HAProxyController) HAProxyProcess() (*os.Process, error) {
 	return process, err
 }
 
+// Wrapping a Native-Client transaction and commit it.
+// Returning an error to let panic or log it upon the scenario.
+func (c *HAProxyController) clientClosure(fn func()) (err error) {
+	if err = c.Client.APIStartTransaction(); err != nil {
+		return
+	}
+	fn()
+	if err = c.Client.APICommitTransaction(); err != nil {
+		return
+	}
+	c.Client.APIDisposeTransaction()
+	return
+}
+
 // Start initialize and run HAProxyController
 func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 
@@ -102,13 +118,29 @@ func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 
 	logger.SetLevel(osArgs.LogLevel.LogLevel)
 	c.haproxyInitialize()
+	logger.Panic(c.clientClosure(func() {
+		var http, https bool
+		var err error
+
+		if !c.osArgs.DisableHTTP {
+			http, err = c.handleBind("http", c.osArgs.HTTPBindPort)
+		}
+		if !c.osArgs.DisableHTTPS {
+			https, err = c.handleBind("https", c.osArgs.HTTPSBindPort)
+		}
+		if http || https {
+			err = c.haproxyService("restart")
+		}
+
+		logger.Panic(err)
+	}))
 	c.initHandlers()
 
 	if c.osArgs.PprofEnabled {
-		logger.Error(c.Client.APIStartTransaction())
-		logger.Error(c.handlePprof())
-		c.refreshBackendSwitching()
-		logger.Error(c.Client.APICommitTransaction())
+		logger.Error(c.clientClosure(func() {
+			logger.Error(c.handlePprof())
+			c.refreshBackendSwitching()
+		}))
 	}
 
 	parts := strings.Split(osArgs.PublishService, "/")
@@ -297,7 +329,32 @@ func (c *HAProxyController) haproxyInitialize() {
 		logger.Panic(err)
 	}
 
-	c.cfg.Init(HAProxyMapDir)
+	c.cfg.Init(HAProxyMapDir, !c.osArgs.DisableHTTPS)
+}
+
+func (c *HAProxyController) handleBind(protocol string, port int64) (bool, error) {
+	var binds []models.Bind
+	if !c.osArgs.DisableIPV4 {
+		binds = append(binds, models.Bind{
+			Name:    "bind_1",
+			Address: c.osArgs.IPV4BindAddr,
+			Port:    utils.PtrInt64(port),
+		})
+	}
+	if !c.osArgs.DisableIPV6 {
+		binds = append(binds, models.Bind{
+			Name:    "bind_2",
+			Address: c.osArgs.IPV6BindAddr,
+			Port:    utils.PtrInt64(port),
+			V4v6:    true,
+		})
+	}
+	for _, b := range binds {
+		if err := c.Client.FrontendBindCreate(protocol, b); err != nil {
+			return false, fmt.Errorf("cannot create bind %s for protocol %s: %s", b.Name, protocol, err.Error())
+		}
+	}
+	return len(binds) > 0, nil
 }
 
 // Handle HAProxy daemon via Master process

controller/handler-https.go
@@ -15,21 +15,49 @@
 package controller
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 
+	"github.com/haproxytech/models/v2"
+
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-	"github.com/haproxytech/models/v2"
 )
 
 type HTTPS struct {
+	ipv4    bool
+	ipv6    bool
+	port    int64
 	certDir string
 }
 
+func (h HTTPS) bindList() (binds []models.Bind) {
+	if h.ipv4 {
+		binds = append(binds, models.Bind{
+			Address: "0.0.0.0",
+			Port:    utils.PtrInt64(h.port),
+			Name:    "bind_1",
+		})
+	}
+	if h.ipv6 {
+		binds = append(binds, models.Bind{
+			Address: "::",
+			Port:    utils.PtrInt64(h.port),
+			Name:    "bind_2",
+			V4v6:    true,
+		})
+	}
+	return
+}
+
 func (h HTTPS) Update(k store.K8s, cfg Configuration, api api.HAProxyClient) (reload bool, err error) {
+	if !cfg.HTTPS {
+		logger.Debugf("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
+		return false, nil
+	}
 	// ssl-passthrough
 	if len(cfg.BackendSwitchingRules[FrontendSSL]) > 0 {
 		if !cfg.SSLPassthrough {
@@ -76,20 +104,10 @@ func (h HTTPS) enableSSLPassthrough(cfg Configuration, api api.HAProxyClient) (e
 	if err != nil {
 		return err
 	}
-	err = api.FrontendBindCreate(FrontendSSL, models.Bind{
-		Address: "0.0.0.0:443",
-		Name:    "bind_1",
-	})
-	if err != nil {
-		return err
-	}
-	err = api.FrontendBindCreate(FrontendSSL, models.Bind{
-		Address: ":::443",
-		Name:    "bind_2",
-		V4v6:    true,
-	})
-	if err != nil {
-		return err
+	for _, b := range h.bindList() {
+		if err = api.FrontendBindCreate(FrontendSSL, b); err != nil {
+			return fmt.Errorf("cannot create bind for SSL Passthrough: %s", err.Error())
+		}
 	}
 	err = api.FrontendTCPRequestRuleCreate(FrontendSSL, models.TCPRequestRule{
 		Index:    utils.PtrInt64(0),
@@ -165,29 +183,10 @@ func (h HTTPS) disableSSLPassthrough(cfg Configuration, api api.HAProxyClient) (
 }
 
 func (h HTTPS) bindSSLPassthrough(enabled bool, api api.HAProxyClient) (err error) {
-	binds, err := api.FrontendBindsGet(FrontendHTTPS)
-	if err != nil {
-		return err
-	}
-	var ipv4, ipv6 string
-	if enabled {
-		ipv4 = "127.0.0.1:8443"
-		ipv6 = "127.0.0.1:8443"
-	} else {
-		ipv4 = "0.0.0.0:443"
-		ipv6 = ":::443"
-	}
-	for _, bind := range binds {
-		if bind.Name == "bind_1" {
-			bind.Address = ipv4
-			bind.Port = nil
-			bind.AcceptProxy = enabled
-		} else if bind.Name == "bind_2" {
-			bind.Address = ipv6
-			bind.Port = nil
-			bind.AcceptProxy = enabled
-		}
-		if err = api.FrontendBindEdit(FrontendHTTPS, *bind); err != nil {
+	for _, bind := range h.bindList() {
+		bind.Port = utils.PtrInt64(8443)
+		bind.AcceptProxy = enabled
+		if err = api.FrontendBindEdit(FrontendHTTPS, bind); err != nil {
 			return err
 		}
 	}

controller/handler.go
@@ -28,7 +28,12 @@ func (c *HAProxyController) initHandlers() {
 		ProxyProtocol{},
 		DefaultCertificate{},
 		ErrorFile{},
-		HTTPS{certDir: HAProxyCertDir},
+		HTTPS{
+			certDir: HAProxyCertDir,
+			ipv4:    !c.osArgs.DisableIPV4,
+			ipv6:    !c.osArgs.DisableIPV6,
+			port:    c.osArgs.HTTPSBindPort,
+		},
 		TCPHandler{handlePath: c.handlePath},
 	}
 }

controller/utils/flags.go
@@ -77,4 +77,12 @@ type OSArgs struct {
 	SyncPeriod            time.Duration  `long:"sync-period" default:"5s" description:"Sets the period at which the controller syncs HAProxy configuration file"`
 	CacheResyncPeriod     time.Duration  `long:"cache-resync-period" default:"10m" description:"Sets the underlying Shared Informer resync period: resyncing controller with informers cache"`
 	LogLevel              LogLevelValue  `long:"log" default:"info" description:"level of log messages you can see"`
+	DisableIPV4           bool           `long:"disable-ipv4" description:"toggle to disable the IPv4 protocol from all frontends"`
+	IPV4BindAddr          string         `long:"ipv4-bind-address" default:"0.0.0.0" description:"IPv4 address the Ingress Controller listens on (if enabled)"`
+	DisableIPV6           bool           `long:"disable-ipv6" description:"toggle to disable the IPv6 protocol from all frontends"`
+	IPV6BindAddr          string         `long:"ipv6-bind-address" default:"::" description:"IPv6 address the Ingress Controller listens on (if enabled)"`
+	HTTPBindPort          int64          `long:"http-bind-port" default:"80" description:"port to listen on for HTTP traffic"`
+	DisableHTTP           bool           `long:"disable-http" description:"toggle to disable the HTTP frontend"`
+	HTTPSBindPort         int64          `long:"https-bind-port" default:"443" description:"port to listen on for HTTPS traffic"`
+	DisableHTTPS          bool           `long:"disable-https" description:"toggle to disable the HTTPs frontend"`
 }

fs/etc/haproxy/haproxy.cfg
@@ -36,17 +36,15 @@ defaults
 
 frontend https
   mode http
-  bind 0.0.0.0:443 name bind_1
-  bind :::443 v4v6 name bind_2
+  bind /tmp/haproxy/https.sock
   http-request set-var(txn.host) req.hdr(Host),field(1,:),lower
   http-request set-var(txn.path) path,lower
   http-request set-var(txn.base) base
   http-request set-header X-Forwarded-Proto https if { ssl_fc }
   default_backend default_backend
 
-frontend http   
-  bind 0.0.0.0:80 name bind_1
-  bind :::80 v4v6 name bind_2
+frontend http
+  bind /tmp/haproxy/http.sock
   http-request set-var(txn.host) req.hdr(Host),field(1,:),lower
   http-request set-var(txn.path) path,lower
   http-request set-var(txn.base) base

main.go
@@ -87,6 +87,16 @@ func main() {
 	logger.Printf("Default ssl certificate: %s", defaultCertificate)
 	logger.Printf("Controller sync period: %s", osArgs.SyncPeriod.String())
 	logger.Debugf("Kubernetes Informers resync period: %s", osArgs.CacheResyncPeriod.String())
+	if !osArgs.DisableHTTP {
+		logger.Printf("Frontend HTTP listening on: %d", osArgs.HTTPBindPort)
+	}
+	if !osArgs.DisableHTTPS {
+		logger.Printf("Frontend HTTPS listening on: %d", osArgs.HTTPSBindPort)
+	}
+	logger.Printf("Disabling HTTP frontend: %t", osArgs.DisableHTTP)
+	logger.Printf("Disabling HTTPS frontend: %t", osArgs.DisableHTTPS)
+	logger.Printf("Disabling IPv4 support: %t", osArgs.DisableIPV4)
+	logger.Printf("Disabling IPv6 support: %t", osArgs.DisableIPV6)
 
 	if osArgs.ConfigMapTCPServices.Name != "" {
 		logger.Printf("TCP Services defined in %s/%s", osArgs.ConfigMapTCPServices.Namespace, osArgs.ConfigMapTCPServices.Name)

commit 91bccbe40224933ab427d5c18e6f6ee4a8a125a6

BUG/MAJOR: fixing ssl offloading regression introduced in db51a5dbb6
commit db51a5dbb6 introduced c.osArgs.DisableHTTPS flag which conflicts with
the internal HTTPS state in "Configuration.HTTPS" and prevents SSL
offloading when DisableHTTPS equal false.

controller/configuration.go
@@ -35,7 +35,7 @@ type Configuration struct {
 }
 
 //Init initialize configuration
-func (c *Configuration) Init(mapDir string, httpsEnabled bool) {
+func (c *Configuration) Init(mapDir string) {
 
 	c.FrontendHTTPReqRules = make(map[Rule]FrontendHTTPReqs)
 	for _, rule := range []Rule{BLACKLIST, SSL_REDIRECT, RATE_LIMIT, REQUEST_CAPTURE, REQUEST_SET_HEADER, REQUEST_SET_HOST, REQUEST_PATH_REWRITE, WHITELIST} {
@@ -63,8 +63,6 @@ func (c *Configuration) Init(mapDir string, httpsEnabled bool) {
 	for _, frontend := range []string{FrontendHTTP, FrontendHTTPS, FrontendSSL} {
 		c.BackendSwitchingRules[frontend] = UseBackendRules{}
 	}
-
-	c.HTTPS = httpsEnabled
 }
 
 //Clean cleans all the statuses of various data that was changed

controller/controller.go
@@ -343,7 +343,7 @@ func (c *HAProxyController) haproxyInitialize() {
 		logger.Panic(err)
 	}
 
-	c.cfg.Init(HAProxyMapDir, !c.osArgs.DisableHTTPS)
+	c.cfg.Init(HAProxyMapDir)
 }
 
 func (c *HAProxyController) handleBind(p *parser.Parser, protocol string, port int64) (reload bool, err error) {

controller/handler-https.go
@@ -28,6 +28,7 @@ import (
 )
 
 type HTTPS struct {
+	enabled  bool
 	addrIpv4 string
 	ipv4     bool
 	addrIpv6 string
@@ -68,7 +69,7 @@ func (h HTTPS) bindList(passhthrough bool) (binds []models.Bind) {
 }
 
 func (h HTTPS) Update(k store.K8s, cfg Configuration, api api.HAProxyClient) (reload bool, err error) {
-	if !cfg.HTTPS {
+	if h.enabled {
 		logger.Debugf("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
 		return false, nil
 	}

controller/handler.go
@@ -29,6 +29,7 @@ func (c *HAProxyController) initHandlers() {
 		DefaultCertificate{},
 		ErrorFile{},
 		HTTPS{
+			enabled:  !c.osArgs.DisableHTTPS,
 			certDir:  HAProxyCertDir,
 			ipv4:     !c.osArgs.DisableIPV4,
 			addrIpv4: c.osArgs.IPV4BindAddr,