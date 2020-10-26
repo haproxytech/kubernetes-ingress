haproxytech/kubernetes-ingress

commit 91bccbe40224933ab427d5c18e6f6ee4a8a125a6

BUG/MAJOR: fixing ssl offloading regression introduced in db51a5dbb6
commit db51a5dbb6 introduced c.osArgs.DisableHTTPS flag which conflicts with
the internal HTTPS state in "Configuration.HTTPS" and prevents SSL
offloading when DisableHTTPS equal false.

controller/handler-https.go
@@ -28,6 +28,7 @@ import (
 )
 
 type HTTPS struct {
+	enabled  bool
 	addrIpv4 string
 	ipv4     bool
 	addrIpv6 string
@@ -68,7 +69,7 @@ func (h HTTPS) bindList(passhthrough bool) (binds []models.Bind) {
 }
 
 func (h HTTPS) Update(k store.K8s, cfg Configuration, api api.HAProxyClient) (reload bool, err error) {
-	if !cfg.HTTPS {
+	if h.enabled {
 		logger.Debugf("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
 		return false, nil
 	}

controller/configuration.go
@@ -35,7 +35,7 @@ type Configuration struct {
 }
 
 //Init initialize configuration
-func (c *Configuration) Init(mapDir string, httpsEnabled bool) {
+func (c *Configuration) Init(mapDir string) {
 
 	c.FrontendHTTPReqRules = make(map[Rule]FrontendHTTPReqs)
 	for _, rule := range []Rule{BLACKLIST, SSL_REDIRECT, RATE_LIMIT, REQUEST_CAPTURE, REQUEST_SET_HEADER, REQUEST_SET_HOST, REQUEST_PATH_REWRITE, WHITELIST} {
@@ -63,8 +63,6 @@ func (c *Configuration) Init(mapDir string, httpsEnabled bool) {
 	for _, frontend := range []string{FrontendHTTP, FrontendHTTPS, FrontendSSL} {
 		c.BackendSwitchingRules[frontend] = UseBackendRules{}
 	}
-
-	c.HTTPS = httpsEnabled
 }
 
 //Clean cleans all the statuses of various data that was changed

controller/controller.go
@@ -343,7 +343,7 @@ func (c *HAProxyController) haproxyInitialize() {
 		logger.Panic(err)
 	}
 
-	c.cfg.Init(HAProxyMapDir, !c.osArgs.DisableHTTPS)
+	c.cfg.Init(HAProxyMapDir)
 }
 
 func (c *HAProxyController) handleBind(p *parser.Parser, protocol string, port int64) (reload bool, err error) {

controller/handler.go
@@ -29,6 +29,7 @@ func (c *HAProxyController) initHandlers() {
 		DefaultCertificate{},
 		ErrorFile{},
 		HTTPS{
+			enabled:  !c.osArgs.DisableHTTPS,
 			certDir:  HAProxyCertDir,
 			ipv4:     !c.osArgs.DisableIPV4,
 			addrIpv4: c.osArgs.IPV4BindAddr,

commit 224c65922b88c5ac7fa7ff8d41bd928c382d555d

BUG: ssl offloading, Fix typo in offloading  condition introduced in 91bccbe

controller/handler-https.go
@@ -69,7 +69,7 @@ func (h HTTPS) bindList(passhthrough bool) (binds []models.Bind) {
 }
 
 func (h HTTPS) Update(k store.K8s, cfg Configuration, api api.HAProxyClient) (reload bool, err error) {
-	if h.enabled {
+	if !h.enabled {
 		logger.Debugf("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
 		return false, nil
 	}