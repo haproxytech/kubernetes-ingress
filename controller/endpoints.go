170d6b525313fcad3f79bda3484096e3ecaf611d
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2020-10-26 10:07:04 +0100

    BUILD: Fix linting: capitalized error string

--- controller/endpoints.go
+++ controller/endpoints.go
@@ -203,7 +203,7 @@ func (c *HAProxyController) setTargetPort(path *store.IngressPath, service *stor
 					return update, nil
 				}
 			}
-			return update, fmt.Errorf("Could not find '%s' Targetport for service '%s'", sp.Name, service.Name)
+			return update, fmt.Errorf("could not find '%s' Targetport for service '%s'", sp.Name, service.Name)
 		}
 	}
 	return update, fmt.Errorf("ingress servicePort(Str: %s, Int: %d) not found for backend '%s'", path.ServicePortString, path.ServicePortInt, endpoints.BackendName)
