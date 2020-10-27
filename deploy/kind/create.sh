b7e80fb38ffb43ae7d5f6d979686ef28379f6dc0
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2020-10-27 01:33:10 +0100

    TEST/MINOR: kind: run create.sh from any directory

--- deploy/kind/create.sh
+++ deploy/kind/create.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+command -v kind >/dev/null 2>&1 || { echo >&2 "Kind not installed.  Aborting."; exit 1; }
+command -v kubectl >/dev/null 2>&1 || { echo >&2 "Kubectl not installed.  Aborting."; exit 1; }
+DIR=$(dirname "$0")
+
 kind delete cluster --name dev
-kind create cluster --name dev --config kind-config.yaml
+kind create cluster --name dev --config $DIR/kind-config.yaml
 
 kubectl cluster-info --context kind-dev
 
 docker build -t go-web-simple:1.1 https://github.com/oktalz/go-web-simple.git#v1.1.5:.
 kind --name="dev" load docker-image  go-web-simple:1.1
 
-sed -e 's/#GROUP#/zagreb/g' -e  's/1 #NUMBER#/4/g' web/web-rc.yml | kubectl apply -f -
-sed 's/#GROUP#/zagreb/g' web/web-svc.yml | kubectl apply -f -
+sed -e 's/#GROUP#/zagreb/g' -e  's/1 #NUMBER#/4/g' $DIR/web/web-rc.yml | kubectl apply -f -
+sed 's/#GROUP#/zagreb/g' $DIR/web/web-svc.yml | kubectl apply -f -
 
-sed -e 's/#GROUP#/paris/g' -e  's/1 #NUMBER#/2/g' web/web-rc.yml | kubectl apply -f -
-sed 's/#GROUP#/paris/g' web/web-svc.yml | kubectl apply -f -
+sed -e 's/#GROUP#/paris/g' -e  's/1 #NUMBER#/2/g' $DIR/web/web-rc.yml | kubectl apply -f -
+sed 's/#GROUP#/paris/g' $DIR/web/web-svc.yml | kubectl apply -f -
 
-sed -e 's/#GROUP#/waltham/g' -e  's/1 #NUMBER#/2/g' web/web-rc.yml | kubectl apply -f -
-sed 's/#GROUP#/waltham/g' web/web-svc.yml | kubectl apply -f -
+sed -e 's/#GROUP#/waltham/g' -e  's/1 #NUMBER#/2/g' $DIR/web/web-rc.yml | kubectl apply -f -
+sed 's/#GROUP#/waltham/g' $DIR/web/web-svc.yml | kubectl apply -f -
 
-kubectl apply -f config/0.namespace.yaml
-kubectl apply -f config/1.ingress.yaml
-kubectl apply -f config/2.default.yaml
-kubectl apply -f config/3.rbac.yaml
-kubectl apply -f config/4.configmap.yaml
-kubectl apply -f config/4.configmap.tcp.yaml
+kubectl apply -f $DIR/config/0.namespace.yaml
+kubectl apply -f $DIR/config/1.ingress.yaml
+kubectl apply -f $DIR/config/2.default.yaml
+kubectl apply -f $DIR/config/3.rbac.yaml
+kubectl apply -f $DIR/config/4.configmap.yaml
+kubectl apply -f $DIR/config/4.configmap.tcp.yaml
 docker pull haproxytech/kubernetes-ingress:latest
 kind --name=dev load docker-image haproxytech/kubernetes-ingress:latest
-sed 's|TAG/IMAGE|haproxytech/kubernetes-ingress:latest|g' config/5.ingress-controller.yaml | kubectl apply -f -
+sed 's|TAG/IMAGE|haproxytech/kubernetes-ingress:latest|g' $DIR/config/5.ingress-controller.yaml | kubectl apply -f -
 
 echo "waiting for pods to be up ..."
 kubectl wait --for=condition=ready --timeout=120s pod -l name=web-zagreb
