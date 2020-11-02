haproxytech/kubernetes-ingress

commit e763b6079512f70083b21c1e96058d81d1d03c60

MINOR: ingress: using GetValueFromAnnotations for ingress class name retrieval

controller/store/events.go
@@ -36,7 +36,10 @@ func (k K8s) EventNamespace(ns *Namespace, data *Namespace) (updateRequired bool
 }
 
 func (k K8s) EventIngress(ns *Namespace, data *Ingress, controllerClass string) (updateRequired bool) {
-	ingressClass := data.Class
+	var ingressClass string
+	ic, _ := k.GetValueFromAnnotations("ingress.class", data.Annotations)
+	ingressClass = ic.Value
+
 	updateRequired = false
 	switch data.Status {
 	case MODIFIED:

controller/store/convert.go
@@ -46,28 +46,11 @@ type ingressNetworkingV1Beta1Strategy struct {
 	obj *networkingv1beta1.Ingress
 }
 
-func getIngressClass(annotations map[string]string, specValue *string) string {
-	// Giving priority to annotation due to backward compatibility as suggested
-	// by Kuberentes documentation.
-	if v, ok := annotations["kubernetes.io/ingress.class"]; ok {
-		return v
-	}
-	// HAProxy Tech Ingress Controller allows also non prefixed annotations
-	if v, ok := annotations["ingress.class"]; ok {
-		return v
-	}
-	if specValue != nil {
-		return *specValue
-	}
-	return ""
-}
-
 func (n ingressNetworkingV1Beta1Strategy) Convert() *Ingress {
 	return &Ingress{
 		APIVersion:  "networking.k8s.io/v1beta1",
 		Namespace:   n.obj.GetNamespace(),
 		Name:        n.obj.GetName(),
-		Class:       getIngressClass(n.obj.GetAnnotations(), n.obj.Spec.IngressClassName),
 		Annotations: ConvertToMapStringW(n.obj.GetAnnotations()),
 		Rules: func(ingressRules []networkingv1beta1.IngressRule) map[string]*IngressRule {
 			rules := make(map[string]*IngressRule)
@@ -149,7 +132,6 @@ func (e ingressExtensionsStrategy) Convert() *Ingress {
 		APIVersion:  "extensions/v1beta1",
 		Namespace:   e.obj.GetNamespace(),
 		Name:        e.obj.GetName(),
-		Class:       getIngressClass(e.obj.GetAnnotations(), e.obj.Spec.IngressClassName),
 		Annotations: ConvertToMapStringW(e.obj.GetAnnotations()),
 		Rules: func(ingressRules []extensionsv1beta1.IngressRule) map[string]*IngressRule {
 			rules := make(map[string]*IngressRule)
@@ -231,7 +213,6 @@ func (n ingressNetworkingV1Strategy) Convert() *Ingress {
 		APIVersion:  "networking.k8s.io/v1",
 		Namespace:   n.obj.GetNamespace(),
 		Name:        n.obj.GetName(),
-		Class:       getIngressClass(n.obj.GetAnnotations(), n.obj.Spec.IngressClassName),
 		Annotations: ConvertToMapStringW(n.obj.GetAnnotations()),
 		Rules: func(ingressRules []networkingv1.IngressRule) map[string]*IngressRule {
 			rules := make(map[string]*IngressRule)

controller/store/types-equal.go
@@ -86,9 +86,6 @@ func (a *Ingress) Equal(b *Ingress) bool {
 	if a.Name != b.Name {
 		return false
 	}
-	if a.Class != b.Class {
-		return false
-	}
 	if len(a.Rules) != len(b.Rules) {
 		return false
 	}

controller/store/types.go
@@ -90,7 +90,6 @@ type Ingress struct {
 	APIVersion     string
 	Namespace      string
 	Name           string
-	Class          string
 	Annotations    MapStringW
 	Rules          map[string]*IngressRule
 	DefaultBackend *IngressPath

commit 108c99f3dcfebec298c2ed482c036f6a9360bfd2

BUG/MEDIUM: crash if no controller class provided.
Regression introduced in e763b607, "ingress.class" annotation does not
have a default value so the annotation should be non nil if it is going
to be used.

controller/store/events.go
@@ -37,8 +37,9 @@ func (k K8s) EventNamespace(ns *Namespace, data *Namespace) (updateRequired bool
 
 func (k K8s) EventIngress(ns *Namespace, data *Ingress, controllerClass string) (updateRequired bool) {
 	var ingressClass string
-	ic, _ := k.GetValueFromAnnotations("ingress.class", data.Annotations)
-	ingressClass = ic.Value
+	if ic, _ := k.GetValueFromAnnotations("ingress.class", data.Annotations); ic != nil {
+		ingressClass = ic.Value
+	}
 
 	updateRequired = false
 	switch data.Status {