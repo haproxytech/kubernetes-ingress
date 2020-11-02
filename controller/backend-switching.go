haproxytech/kubernetes-ingress

commit f69b22a9eeed4e45fb044215017fdd3d6b2017ce

MAJOR: Refactor HAProxy Rules and move them to a seperate package.

controller/configuration.go
@@ -16,17 +16,14 @@ package controller
 
 import (
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
-	"github.com/haproxytech/models/v2"
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/rules"
 )
 
 //Configuration represents k8s state
 
 type Configuration struct {
 	MapFiles                 haproxy.Maps
-	FrontendHTTPReqRules     map[Rule]FrontendHTTPReqs
-	FrontendHTTPRspRules     map[Rule]FrontendHTTPRsps
-	FrontendTCPRules         map[Rule]FrontendTCPReqs
-	FrontendRulesModified    map[Mode]bool
+	HAProxyRules             haproxy.Rules
 	BackendSwitchingRules    map[string]UseBackendRules
 	BackendSwitchingModified map[string]struct{}
 	HTTPS                    bool
@@ -37,25 +34,9 @@ type Configuration struct {
 //Init initialize configuration
 func (c *Configuration) Init(mapDir string) {
 
-	c.FrontendHTTPReqRules = make(map[Rule]FrontendHTTPReqs)
-	for _, rule := range []Rule{BLACKLIST, SSL_REDIRECT, RATE_LIMIT, REQUEST_CAPTURE, REQUEST_SET_HEADER, REQUEST_SET_HOST, REQUEST_PATH_REWRITE, WHITELIST} {
-		c.FrontendHTTPReqRules[rule] = make(map[haproxy.MapID]models.HTTPRequestRule)
-	}
-	c.FrontendHTTPRspRules = make(map[Rule]FrontendHTTPRsps)
-	for _, rule := range []Rule{RESPONSE_SET_HEADER} {
-		c.FrontendHTTPRspRules[rule] = make(map[haproxy.MapID]models.HTTPResponseRule)
-	}
-	c.FrontendTCPRules = make(map[Rule]FrontendTCPReqs)
-	for _, rule := range []Rule{BLACKLIST, REQUEST_CAPTURE, PROXY_PROTOCOL, WHITELIST} {
-		c.FrontendTCPRules[rule] = make(map[haproxy.MapID]models.TCPRequestRule)
-	}
-	c.FrontendRulesModified = map[Mode]bool{
-		HTTP: false,
-		TCP:  false,
-	}
 	c.MapFiles = haproxy.NewMapFiles(mapDir)
+	c.HAProxyRulesInit()
 
-	sslRedirectEnabled = make(map[string]struct{})
 	rateLimitTables = make(map[string]rateLimitTable)
 
 	c.BackendSwitchingRules = make(map[string]UseBackendRules)
@@ -69,15 +50,28 @@ func (c *Configuration) Init(mapDir string) {
 //deletes them completely or just resets them if needed
 func (c *Configuration) Clean() {
 	c.MapFiles.Clean()
-	for rule := range c.FrontendHTTPReqRules {
-		c.FrontendHTTPReqRules[rule] = make(map[haproxy.MapID]models.HTTPRequestRule)
-	}
-	for rule := range c.FrontendHTTPRspRules {
-		c.FrontendHTTPRspRules[rule] = make(map[haproxy.MapID]models.HTTPResponseRule)
-	}
-	for rule := range c.FrontendTCPRules {
-		c.FrontendTCPRules[rule] = make(map[haproxy.MapID]models.TCPRequestRule)
-	}
-	c.FrontendRulesModified[HTTP] = false
-	c.FrontendRulesModified[TCP] = false
+	c.HAProxyRulesInit()
+}
+
+func (c *Configuration) HAProxyRulesInit() {
+	c.HAProxyRules = haproxy.NewRules()
+	_ = c.HAProxyRules.AddRule(rules.SetHdr{
+		ForwardedProto: true,
+	}, 0, FrontendHTTPS)
+	_ = c.HAProxyRules.AddRule(rules.ReqSetVar{
+		Name:       "base",
+		Scope:      "txn",
+		Expression: "base",
+	}, 1, FrontendHTTP, FrontendHTTPS)
+	_ = c.HAProxyRules.AddRule(rules.ReqSetVar{
+		Name:       "path",
+		Scope:      "txn",
+		Expression: "path,lower",
+	}, 2, FrontendHTTP, FrontendHTTPS)
+	_ = c.HAProxyRules.AddRule(rules.ReqSetVar{
+		Name:       "host",
+		Scope:      "txn",
+		Expression: "req.hdr(Host),field(1,:),lower",
+	}, 3, FrontendHTTP, FrontendHTTPS)
+	//TODO: handle stacking error
 }

controller/haproxy/rules.go
@@ -1,0 +1,107 @@
+package haproxy
+
+import (
+	"fmt"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
+	"github.com/haproxytech/models/v2"
+)
+
+type Rule interface {
+	GetType() RuleType
+	Create(client api.HAProxyClient, frontend *models.Frontend) error
+}
+
+// Order matters !
+// Rules will be evaluated by HAPorxy in the defined order.
+type RuleType int
+
+//nolint
+const (
+	REQ_ACCEPT_CONTENT RuleType = iota
+	REQ_INSPECT_DELAY
+	REQ_PROXY_PROTOCOL
+	REQ_DENY
+	REQ_TRACK
+	REQ_RATELIMIT
+	REQ_SET_VAR
+	REQ_CAPTURE
+	REQ_SSL_REDIRECT
+	REQ_FORWARDED_PROTO
+	REQ_SET_HEADER
+	REQ_SET_HOST
+	REQ_PATH_REWRITE
+	RES_SET_HEADER
+)
+
+type Rules map[string]*frontendRules
+
+// This structure may evolve with additional fields
+type frontendRules struct {
+	rules map[RuleType]map[MapID]Rule
+}
+
+var logger = utils.GetLogger()
+
+func NewRules() Rules {
+	return make(map[string]*frontendRules)
+}
+
+func (r Rules) AddRule(rule Rule, id MapID, frontends ...string) error {
+	if rule == nil || len(frontends) == 0 {
+		return fmt.Errorf("invalid params")
+	}
+	for _, frontend := range frontends {
+		ftRules, ok := r[frontend]
+		if !ok {
+			ftRules = &frontendRules{
+				rules: make(map[RuleType]map[MapID]Rule),
+			}
+			r[frontend] = ftRules
+		}
+		ruleType := rule.GetType()
+		if _, ok := ftRules.rules[ruleType]; !ok {
+			ftRules.rules[ruleType] = make(map[MapID]Rule)
+		}
+		ftRules.rules[ruleType][id] = rule
+	}
+	return nil
+}
+
+func (r Rules) EnableSSLPassThrough(passThroughFtd, offloadFtd string) {
+	if _, ok := r[offloadFtd]; !ok {
+		return
+	}
+	if _, ok := r[passThroughFtd]; !ok {
+		r[passThroughFtd] = &frontendRules{
+			rules: make(map[RuleType]map[MapID]Rule),
+		}
+	}
+	for _, ruleType := range []RuleType{REQ_PROXY_PROTOCOL, REQ_DENY} {
+		r[passThroughFtd].rules[ruleType] = r[offloadFtd].rules[ruleType]
+		delete(r[offloadFtd].rules, ruleType)
+	}
+}
+
+func (r Rules) Refresh(client api.HAProxyClient) (reload bool) {
+	for feName, feRules := range r {
+		fe, err := client.FrontendGet(feName)
+		if err != nil {
+			logger.Error(fe)
+			continue
+		}
+		client.FrontendRuleDeleteAll(feName)
+		// All rules are created with Index 0,
+		// so first rule inserted will be last in the list
+		for ruleType := RES_SET_HEADER; ruleType >= REQ_ACCEPT_CONTENT; ruleType-- {
+			for _, rule := range feRules.rules[ruleType] {
+				// TODO: handle stacking errors
+				if err := rule.Create(client, &fe); err == nil {
+					reload = true
+				}
+			}
+		}
+	}
+	return reload
+}

controller/haproxy/rules/reqTrack.go
@@ -1,0 +1,37 @@
+package rules
+
+import (
+	"fmt"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
+	"github.com/haproxytech/models/v2"
+)
+
+type ReqTrack struct {
+	Ingress   haproxy.MapID
+	TableName string
+	TrackKey  string
+}
+
+func (r ReqTrack) GetType() haproxy.RuleType {
+	return haproxy.REQ_TRACK
+}
+
+func (r ReqTrack) Create(client api.HAProxyClient, frontend *models.Frontend) error {
+	if frontend.Mode == "tcp" {
+		//TODO: tcp request tracking
+		return fmt.Errorf("request Track cannot be configured in TCP mode")
+	}
+	ingressMapFile := r.Ingress.Path()
+	httpRule := models.HTTPRequestRule{
+		Index:         utils.PtrInt64(0),
+		Type:          "track-sc0",
+		TrackSc0Key:   r.TrackKey,
+		TrackSc0Table: r.TableName,
+		Cond:          "if",
+		CondTest:      makeACL("", ingressMapFile),
+	}
+	return client.FrontendHTTPRequestRuleCreate(frontend.Name, httpRule)
+}

controller/controller.go
@@ -239,19 +239,12 @@ func (c *HAProxyController) updateHAProxy() error {
 				}
 			}
 
+			// Ingress Annotations
 			if len(ingress.Rules) == 0 {
 				logger.Debugf("Ingress %s/%s: no rules defined", ingress.Namespace, ingress.Name)
 				continue
 			}
-			logger.Error(c.handleRateLimiting(ingress))
-			logger.Error(c.handleRequestCapture(ingress))
-			logger.Error(c.handleRequestPathRewrite(ingress))
-			logger.Error(c.handleRequestSetHost(ingress))
-			logger.Error(c.handleRequestSetHdr(ingress))
-			logger.Error(c.handleResponseSetHdr(ingress))
-			logger.Error(c.handleBlacklisting(ingress))
-			logger.Error(c.handleWhitelisting(ingress))
-			logger.Error(c.handleHTTPRedirect(ingress))
+			c.handleIngressAnnotations(ingress)
 		}
 	}
 
@@ -462,9 +455,7 @@ func (c *HAProxyController) clean() {
 }
 
 func (c *HAProxyController) Refresh() (reload bool) {
-	reload = c.FrontendHTTPReqsRefresh() || reload
-	reload = c.FrontendHTTPRspsRefresh() || reload
-	reload = c.FrontendTCPreqsRefresh() || reload
+	reload = c.cfg.HAProxyRules.Refresh(c.Client) || reload
 	reload = c.refreshBackendSwitching() || reload
 	r, err := c.cfg.MapFiles.Refresh()
 	reload = reload || r

controller/global-annotations.go
@@ -55,11 +55,6 @@ func (c *HAProxyController) handleGlobalAnnotations() (restart bool, reload bool
 		} else if r == 2 {
 			restart = true
 		}
-		// TODO: study if we can handle this inside annotation's Update function
-		// In which case, Update should also receive controller's Config
-		if name == "timeout-client" && c.cfg.SSLPassthrough {
-			c.cfg.FrontendRulesModified[TCP] = true
-		}
 	}
 	return restart, reload
 }

controller/handler-https.go
@@ -23,6 +23,7 @@ import (
 	"github.com/haproxytech/models/v2"
 
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/rules"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
@@ -96,6 +97,7 @@ func (h HTTPS) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (r
 			cfg.SSLPassthrough = true
 			reload = true
 		}
+		logger.Error(h.sslPassthroughRules(k, cfg))
 	} else if cfg.SSLPassthrough {
 		logger.Info("Disabling ssl-passthrough")
 		logger.Panic(h.disableSSLPassthrough(cfg, api))
@@ -126,50 +128,20 @@ func (h HTTPS) enableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (
 			return fmt.Errorf("cannot create bind for SSL Passthrough: %s", err.Error())
 		}
 	}
-	err = api.FrontendTCPRequestRuleCreate(FrontendSSL, models.TCPRequestRule{
-		Index:    utils.PtrInt64(0),
-		Action:   "accept",
-		Type:     "content",
-		Cond:     "if",
-		CondTest: "{ req_ssl_hello_type 1 }",
-	})
-	if err != nil {
-		return err
-	}
-	err = api.FrontendTCPRequestRuleCreate(FrontendSSL, models.TCPRequestRule{
-		Index:    utils.PtrInt64(0),
-		Action:   "set-var",
-		VarName:  "sni",
-		VarScope: "sess",
-		Expr:     "req_ssl_sni",
-		Type:     "content",
-	})
-	if err != nil {
-		return err
-	}
-	err = api.FrontendTCPRequestRuleCreate(FrontendSSL, models.TCPRequestRule{
-		Type:    "inspect-delay",
-		Index:   utils.PtrInt64(0),
-		Timeout: utils.PtrInt64(5000),
-	})
-	if err != nil {
-		return err
-	}
 	// Create backend for proxy chaining (chaining
 	// ssl-passthrough frontend to ssl-offload backend)
 	err = api.BackendCreate(models.Backend{
 		Name: backendHTTPS,
 		Mode: "tcp",
 	})
-	if err != nil {
-		return err
-	}
-	err = api.BackendServerCreate(backendHTTPS, models.Server{
+	_ = api.BackendServerCreate(backendHTTPS, models.Server{
 		Name:        FrontendHTTPS,
 		Address:     "127.0.0.1",
 		Port:        utils.PtrInt64(h.port),
 		SendProxyV2: "enabled",
 	})
+	_ = h.toggleSSLPassthrough(true, cfg.HTTPS, api)
+	//TODO: stack errors
 	if err != nil {
 		return err
 	}
@@ -177,8 +149,6 @@ func (h HTTPS) enableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (
 	if err = h.toggleSSLPassthrough(true, cfg.HTTPS, api); err != nil {
 		return err
 	}
-	// Some TCP rules depend on ssl-passthrough
-	cfg.FrontendRulesModified[TCP] = true
 	return nil
 }
 
@@ -195,8 +165,6 @@ func (h HTTPS) disableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) 
 	if err = h.toggleSSLPassthrough(false, cfg.HTTPS, api); err != nil {
 		return err
 	}
-	// Some TCP rules depend on ssl-passthrough
-	cfg.FrontendRulesModified[TCP] = true
 	return nil
 }
 
@@ -229,4 +197,29 @@ func (h HTTPS) CleanCertDir(usedCerts map[string]struct{}) error {
 		}
 	}
 	return nil
+}
+
+func (h HTTPS) sslPassthroughRules(k store.K8s, cfg *Configuration) error {
+	inspectTimeout := utils.PtrInt64(5000)
+	annTimeout, _ := k.GetValueFromAnnotations("timeout-client", k.ConfigMaps[Main].Annotations)
+	if annTimeout != nil {
+		if value, errParse := utils.ParseTime(annTimeout.Value); errParse == nil {
+			inspectTimeout = value
+		} else {
+			logger.Error(errParse)
+		}
+	}
+
+	cfg.HAProxyRules.EnableSSLPassThrough(FrontendSSL, FrontendHTTPS)
+	err := cfg.HAProxyRules.AddRule(rules.ReqAcceptContent{}, 0, FrontendSSL)
+	_ = cfg.HAProxyRules.AddRule(rules.ReqSetVar{
+		Name:       "sni",
+		Scope:      "sess",
+		Expression: "req_ssl_sni",
+	}, 0, FrontendSSL)
+	_ = cfg.HAProxyRules.AddRule(rules.ReqInspectDelay{
+		Timeout: inspectTimeout,
+	}, 0, FrontendSSL)
+	//TODO: handle stacking error
+	return err
 }

controller/handler-proxy-protocol.go
@@ -15,56 +15,40 @@
 package controller
 
 import (
-	"fmt"
 	"net"
 	"strings"
 
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/rules"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
-	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-	"github.com/haproxytech/models/v2"
 )
 
 type ProxyProtocol struct{}
 
 func (p ProxyProtocol) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (reload bool, err error) {
-	//  Get and validate annotations
+	//  Get annotation status
 	annProxyProtocol, _ := k.GetValueFromAnnotations("proxy-protocol", k.ConfigMaps[Main].Annotations)
 	if annProxyProtocol == nil {
 		return false, nil
 	}
-	value := strings.Replace(annProxyProtocol.Value, ",", " ", -1)
-	for _, address := range strings.Fields(value) {
+	if annProxyProtocol.Status == DELETED {
+		logger.Debugf("Deleting ProxyProtcol configuration")
+		return false, nil
+	}
+	// Validate annotation
+	ips, _ := haproxy.NewMapID(annProxyProtocol.Value)
+	for _, address := range strings.Split(annProxyProtocol.Value, ",") {
 		if ip := net.ParseIP(address); ip == nil {
-			if _, _, err := net.ParseCIDR(address); err != nil {
-				return false, fmt.Errorf("incorrect value for proxy-protocol annotation ")
+			if _, _, err = net.ParseCIDR(address); err != nil {
+				logger.Errorf("incorrect address '%s' in proxy-protocol annotation", address)
+				continue
 			}
+			cfg.MapFiles.AppendRow(ips, address)
 		}
 	}
-
-	// Get Rules status
-	status := annProxyProtocol.Status
-
-	// Update rules
-	// Since this is a Configmap Annotation ONLY, no need to
-	// track ingress hosts in Map file
-	if status != EMPTY {
-		cfg.FrontendRulesModified[TCP] = true
-		if status == DELETED {
-			logger.Debugf("Deleting ProxyProtcol configuration")
-			return false, nil
-		}
-		logger.Debugf("Configuring ProxyProtcol annotation")
-	}
-
-	tcpRule := models.TCPRequestRule{
-		Index:    utils.PtrInt64(0),
-		Type:     "connection",
-		Action:   "expect-proxy layer4",
-		Cond:     "if",
-		CondTest: fmt.Sprintf("{ src %s }", value),
-	}
-	cfg.FrontendTCPRules[PROXY_PROTOCOL][0] = tcpRule
-
-	return false, nil
+	// Configure Annotation
+	logger.Debugf("Configuring ProxyProtcol annotation")
+	err = cfg.HAProxyRules.AddRule(rules.ReqProxyProtocol{SrcIPs: ips}, 0, FrontendHTTP, FrontendHTTPS)
+	return false, err
 }

commit b807940fda19e263242acfd34c5ef869f5d59dbe

BUG/MEDIUM: rate-limite: tracking tables not created
Regression introduced in f69b22a9, when enabling rate limiting, tracking tables are
not created making HAProxy configuration fail.

controller/backend-switching.go
@@ -60,7 +60,7 @@ func (c *HAProxyController) refreshBackendSwitching() (reload bool) {
 	}
 	// Active backend will hold backends in use
 	activeBackends := make(map[string]struct{})
-	for rateLimitTable := range rateLimitTables {
+	for _, rateLimitTable := range rateLimitTables {
 		activeBackends[rateLimitTable] = struct{}{}
 	}
 	for _, frontend := range frontends {

controller/configuration.go
@@ -38,8 +38,6 @@ func (c *Configuration) Init(mapDir string) {
 	c.MapFiles = haproxy.NewMapFiles(mapDir)
 	logger.Panic(c.HAProxyRulesInit())
 
-	rateLimitTables = make(map[string]rateLimitTable)
-
 	c.BackendSwitchingRules = make(map[string]UseBackendRules)
 	c.BackendSwitchingModified = make(map[string]struct{})
 	for _, frontend := range []string{FrontendHTTP, FrontendHTTPS, FrontendSSL} {
@@ -52,6 +50,7 @@ func (c *Configuration) Init(mapDir string) {
 func (c *Configuration) Clean() {
 	c.MapFiles.Clean()
 	logger.Panic(c.HAProxyRulesInit())
+	rateLimitTables = []string{}
 }
 
 func (c *Configuration) HAProxyRulesInit() error {

controller/frontend-annotations.go
@@ -27,12 +27,7 @@ import (
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
-type rateLimitTable struct {
-	size   *int64
-	period *int64
-}
-
-var rateLimitTables map[string]rateLimitTable
+var rateLimitTables []string
 
 func (c *HAProxyController) handleIngressAnnotations(ingress *store.Ingress) {
 	logger.Error(c.handleRateLimiting(ingress))
@@ -161,20 +156,20 @@ func (c *HAProxyController) handleRateLimiting(ingress *store.Ingress) error {
 			}
 		}
 	}
-	rateLimitTables[tableName] = rateLimitTable{
-		size:   rateLimitSize,
-		period: rateLimitPeriod,
-	}
+	rateLimitTables = append(rateLimitTables, tableName)
 	reqTrack := rules.ReqTrack{
-		Ingress:   trackMatch,
-		TableName: tableName,
-		TrackKey:  "src",
+		Ingress:     trackMatch,
+		TableName:   tableName,
+		TableSize:   rateLimitSize,
+		TablePeriod: rateLimitPeriod,
+		TrackKey:    "src",
 	}
 	err = c.cfg.HAProxyRules.AddRule(reqTrack, trackMatch, FrontendHTTP, FrontendHTTPS)
 	if err != nil {
 		return err
 	}
 	reqRateLimit := rules.ReqRateLimit{
+		TableName:      tableName,
 		Ingress:        reqsMatch,
 		ReqsLimit:      reqsLimit,
 		DenyStatusCode: rateLimitCode,

controller/haproxy/rules.go
@@ -88,7 +88,7 @@ func (r Rules) Refresh(client api.HAProxyClient) (reload bool) {
 	for feName, feRules := range r {
 		fe, err := client.FrontendGet(feName)
 		if err != nil {
-			logger.Error(fe)
+			logger.Error(err)
 			continue
 		}
 		client.FrontendRuleDeleteAll(feName)

controller/haproxy/rules/reqTrack.go
@@ -10,9 +10,11 @@ import (
 )
 
 type ReqTrack struct {
-	Ingress   haproxy.MapID
-	TableName string
-	TrackKey  string
+	Ingress     haproxy.MapID
+	TableName   string
+	TablePeriod *int64
+	TableSize   *int64
+	TrackKey    string
 }
 
 func (r ReqTrack) GetType() haproxy.RuleType {
@@ -24,6 +26,21 @@ func (r ReqTrack) Create(client api.HAProxyClient, frontend *models.Frontend) er
 		//TODO: tcp request tracking
 		return fmt.Errorf("request Track cannot be configured in TCP mode")
 	}
+	// Create tracking table.
+	if _, err := client.BackendGet(r.TableName); err != nil {
+		err = client.BackendCreate(models.Backend{
+			Name: r.TableName,
+			StickTable: &models.BackendStickTable{
+				Type:  "ip",
+				Size:  r.TableSize,
+				Store: fmt.Sprintf("http_req_rate(%d)", *r.TablePeriod),
+			},
+		})
+		if err != nil {
+			return err
+		}
+	}
+	// Create rule
 	ingressMapFile := r.Ingress.Path()
 	httpRule := models.HTTPRequestRule{
 		Index:         utils.PtrInt64(0),