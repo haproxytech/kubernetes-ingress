haproxytech/kubernetes-ingress

commit 7a1f4bb3b32911d4fc19d43c1ef800c27568f9a1

MAJOR: refactor and  move Ingress routes related task to a different package.

controller/handler-https.go
@@ -90,15 +90,16 @@ func (h HTTPS) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (r
 		reload = true
 	}
 	// ssl-passthrough
-	if len(cfg.BackendSwitchingRules[FrontendSSL]) > 0 {
-		if !cfg.SSLPassthrough {
+	_, errFtSSL := api.FrontendGet(FrontendSSL)
+	if cfg.SSLPassthrough {
+		if errFtSSL != nil {
 			logger.Info("Enabling ssl-passthrough")
 			logger.Panic(h.enableSSLPassthrough(cfg, api))
 			cfg.SSLPassthrough = true
 			reload = true
 		}
 		logger.Error(h.sslPassthroughRules(k, cfg))
-	} else if cfg.SSLPassthrough {
+	} else if errFtSSL == nil {
 		logger.Info("Disabling ssl-passthrough")
 		logger.Panic(h.disableSSLPassthrough(cfg, api))
 		cfg.SSLPassthrough = false
@@ -112,12 +113,11 @@ func (h HTTPS) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (r
 
 func (h HTTPS) enableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (err error) {
 	// Create TCP frontend for ssl-passthrough
-	backendHTTPS := "https"
 	frontend := models.Frontend{
 		Name:           FrontendSSL,
 		Mode:           "tcp",
 		LogFormat:      "'%ci:%cp [%t] %ft %b/%s %Tw/%Tc/%Tt %B %ts %ac/%fc/%bc/%sc/%rc %sq/%bq %hr %hs %[var(sess.sni)]'",
-		DefaultBackend: backendHTTPS,
+		DefaultBackend: SSLDefaultBaceknd,
 	}
 	err = api.FrontendCreate(frontend)
 	if err != nil {
@@ -133,10 +133,10 @@ func (h HTTPS) enableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (
 	var errors utils.Errors
 	errors.Add(
 		api.BackendCreate(models.Backend{
-			Name: backendHTTPS,
+			Name: SSLDefaultBaceknd,
 			Mode: "tcp",
 		}),
-		api.BackendServerCreate(backendHTTPS, models.Server{
+		api.BackendServerCreate(SSLDefaultBaceknd, models.Server{
 			Name:        FrontendHTTPS,
 			Address:     "127.0.0.1",
 			Port:        utils.PtrInt64(h.port),

controller/backend-annotations.go
@@ -1,319 +1,0 @@
-// Copyright 2019 HAProxy Technologies LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package controller
-
-import (
-	"strconv"
-	"strings"
-
-	"github.com/haproxytech/models/v2"
-
-	"github.com/haproxytech/kubernetes-ingress/controller/configsnippet"
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
-	"github.com/haproxytech/kubernetes-ingress/controller/store"
-	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-)
-
-func (c *HAProxyController) handleSSLPassthrough(route *IngressRoute, backend *models.Backend) (updateBackendSwitching bool) {
-	if route.Path.IsTCPService || route.Path.IsDefaultBackend {
-		return false
-	}
-	updateBackendSwitching = false
-	annSSLPassthrough, _ := c.Store.GetValueFromAnnotations("ssl-passthrough", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	status := annSSLPassthrough.Status
-	if status == EMPTY {
-		status = route.Path.Status
-	}
-	if status != EMPTY || route.NewBackend {
-		enabled, err := utils.GetBoolValue(annSSLPassthrough.Value, "ssl-passthrough")
-		if err != nil {
-			logger.Errorf("ssl-passthrough annotation: %s", err)
-			return updateBackendSwitching
-		}
-		if enabled {
-			if !route.Path.IsSSLPassthrough {
-				route.Path.IsSSLPassthrough = true
-				backend.Mode = "tcp"
-				updateBackendSwitching = true
-
-			}
-		} else if route.Path.IsSSLPassthrough {
-			route.Path.IsSSLPassthrough = false
-			backend.Mode = "http"
-			updateBackendSwitching = true
-		}
-	}
-	return updateBackendSwitching
-}
-
-// Update backend with annotations values.
-func (c *HAProxyController) handleBackendAnnotations(route *IngressRoute, backendModel *models.Backend) (activeAnnotations bool) {
-	activeAnnotations = false
-	backend := haproxy.Backend(*backendModel)
-	backendAnnotations := make(map[string]*store.StringW, 7)
-
-	backendAnnotations["abortonclose"], _ = c.Store.GetValueFromAnnotations("abortonclose", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	backendAnnotations["config-snippet"], _ = c.Store.GetValueFromAnnotations("config-snippet", route.Ingress.Annotations, route.Service.Annotations)
-	backendAnnotations["cookie-persistence"], _ = c.Store.GetValueFromAnnotations("cookie-persistence", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	backendAnnotations["load-balance"], _ = c.Store.GetValueFromAnnotations("load-balance", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	backendAnnotations["timeout-check"], _ = c.Store.GetValueFromAnnotations("timeout-check", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	if backend.Mode == "http" {
-		backendAnnotations["check-http"], _ = c.Store.GetValueFromAnnotations("check-http", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-		backendAnnotations["forwarded-for"], _ = c.Store.GetValueFromAnnotations("forwarded-for", route.Service.Annotations, route.Ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	}
-
-	var cs string
-	if a, ok := backendAnnotations["config-snippet"]; ok && a != nil {
-		cs = a.Value
-	}
-
-	// The DELETED status of an annotation is handled explicitly
-	// only when there is no default annotation value.
-	for k, v := range backendAnnotations {
-		if v == nil {
-			continue
-		}
-		if v.Status != EMPTY || route.NewBackend {
-			logger.Debugf("Backend '%s': Configuring '%s' annotation", backend.Name, k)
-			switch k {
-			case "abortonclose":
-				if err := configsnippet.NewGenericAttribute("option abort").Overridden(cs); err != nil {
-					logger.Warning(err.Error())
-					continue
-				}
-				if err := backend.UpdateAbortOnClose(v.Value); err != nil {
-					logger.Error(err)
-					continue
-				}
-				activeAnnotations = true
-			case "check-http":
-				if err := configsnippet.NewGenericAttribute("option httpchk").Overridden(cs); err != nil {
-					logger.Warning(err.Error())
-					continue
-				}
-				if v.Status == DELETED && !route.NewBackend {
-					backend.Httpchk = nil
-				} else if err := backend.UpdateHttpchk(v.Value); err != nil {
-					logger.Errorf("%s annotation: %s", k, err)
-					continue
-				}
-				activeAnnotations = true
-			case "config-snippet":
-				var err error
-				if v.Status == DELETED && !route.NewBackend {
-					err = c.Client.BackendCfgSnippetSet(backend.Name, nil)
-				} else {
-					value := strings.SplitN(strings.Trim(v.Value, "\n"), "\n", -1)
-					if len(value) == 0 {
-						continue
-					}
-					err = c.Client.BackendCfgSnippetSet(backend.Name, &value)
-				}
-				if err != nil {
-					logger.Errorf("%s annotation: %s", k, err)
-				} else {
-					activeAnnotations = true
-				}
-			case "cookie-persistence":
-				if err := configsnippet.NewGenericAttribute("cookie").Overridden(cs); err != nil {
-					logger.Warning(err.Error())
-					continue
-				}
-				if v.Status == DELETED && !route.NewBackend {
-					backend.Cookie = nil
-				} else {
-					cookie := c.handleCookieAnnotations(route.Ingress, route.Service)
-					if err := backend.UpdateCookie(&cookie); err != nil {
-						logger.Errorf("%s annotation: %s", k, err)
-						continue
-					}
-				}
-				activeAnnotations = true
-			case "forwarded-for":
-				if err := configsnippet.NewGenericAttribute("option forwardfor").Overridden(cs); err != nil {
-					logger.Warning(err.Error())
-					continue
-				}
-				if err := backend.UpdateForwardfor(v.Value); err != nil {
-					logger.Errorf("%s annotation: %s", k, err)
-					continue
-				}
-				activeAnnotations = true
-			case "load-balance":
-				if err := configsnippet.NewGenericAttribute("balance").Overridden(cs); err != nil {
-					logger.Warning(err.Error())
-					continue
-				}
-				if err := backend.UpdateBalance(v.Value); err != nil {
-					logger.Errorf("%s annotation: %s", k, err)
-					continue
-				}
-				activeAnnotations = true
-			}
-		}
-	}
-	*backendModel = models.Backend(backend)
-	return activeAnnotations
-
-}
-
-// Update server with annotations values.
-func (c *HAProxyController) handleServerAnnotations(serverModel *models.Server, annotations map[string]*store.StringW) {
-	server := haproxy.Server(*serverModel)
-
-	// The DELETED status of an annotation is handled explicitly
-	// only when there is no default annotation value.
-	for k, v := range annotations {
-		if v == nil {
-			continue
-		}
-		logger.Tracef("Server '%s': Configuring '%s' annotation", server.Name, k)
-		switch k {
-		case "cookie-persistence":
-			if v.Status == DELETED {
-				server.Cookie = ""
-			} else {
-				server.Cookie = server.Name
-			}
-		case "check":
-			if err := server.UpdateCheck(v.Value); err != nil {
-				logger.Errorf("%s annotation: %s", k, err)
-				continue
-			}
-		case "check-interval":
-			if v.Status == DELETED {
-				server.Inter = nil
-			} else if err := server.UpdateInter(v.Value); err != nil {
-				logger.Errorf("%s annotation: %s", k, err)
-				continue
-			}
-		case "pod-maxconn":
-			if v.Status == DELETED {
-				server.Maxconn = nil
-			} else if err := server.UpdateMaxconn(v.Value); err != nil {
-				logger.Errorf("%s annotation: %s", k, err)
-				continue
-			}
-		case "server-ssl":
-			if err := server.UpdateServerSsl(v.Value); err != nil {
-				logger.Errorf("%s annotation: %s", k, err)
-				continue
-			}
-		case "send-proxy-protocol":
-			if v.Status == DELETED || len(v.Value) == 0 {
-				server.ResetSendProxy()
-				continue
-			}
-			if err := server.UpdateSendProxy(v.Value); err != nil {
-				logger.Errorf("%s annotation: %s", k, err)
-				continue
-			}
-		}
-	}
-	*serverModel = models.Server(server)
-}
-
-func (c *HAProxyController) handleCookieAnnotations(ingress *store.Ingress, service *store.Service) models.Cookie {
-
-	cookieAnnotations := make(map[string]*store.StringW, 11)
-	cookieAnnotations["cookie-persistence"], _ = c.Store.GetValueFromAnnotations("cookie-persistence", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-domain"], _ = c.Store.GetValueFromAnnotations("cookie-domain", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-dynamic"], _ = c.Store.GetValueFromAnnotations("cookie-dynamic", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-httponly"], _ = c.Store.GetValueFromAnnotations("cookie-httponly", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-indirect"], _ = c.Store.GetValueFromAnnotations("cookie-indirect", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-maxidle"], _ = c.Store.GetValueFromAnnotations("cookie-maxidle", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-maxlife"], _ = c.Store.GetValueFromAnnotations("cookie-maxlife", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-nocache"], _ = c.Store.GetValueFromAnnotations("cookie-nocache", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-postonly"], _ = c.Store.GetValueFromAnnotations("cookie-postonly", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-preserve"], _ = c.Store.GetValueFromAnnotations("cookie-preserve", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-secure"], _ = c.Store.GetValueFromAnnotations("cookie-secure", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookieAnnotations["cookie-type"], _ = c.Store.GetValueFromAnnotations("cookie-type", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	cookie := models.Cookie{}
-	for k, v := range cookieAnnotations {
-		if v == nil {
-			continue
-		}
-		switch k {
-		case "cookie-domain":
-			var domains []*models.Domain
-			for _, domain := range strings.Fields(v.Value) {
-				domains = append(domains, &models.Domain{Value: domain})
-			}
-			cookie.Domains = domains
-		case "cookie-dynamic":
-			dynamic, err := utils.GetBoolValue(v.Value, "cookie-dynamic")
-			logger.Error(err)
-			cookie.Dynamic = dynamic
-		case "cookie-httponly":
-			httponly, err := utils.GetBoolValue(v.Value, "cookie-httponly")
-			logger.Error(err)
-			cookie.Httponly = httponly
-		case "cookie-indirect":
-			indirect, err := utils.GetBoolValue(v.Value, "cookie-indirect")
-			logger.Error(err)
-			cookie.Indirect = indirect
-		case "cookie-maxidle":
-			maxidle, err := strconv.ParseInt(v.Value, 10, 64)
-			logger.Error(err)
-			cookie.Maxidle = maxidle
-		case "cookie-maxlife":
-			maxlife, err := strconv.ParseInt(v.Value, 10, 64)
-			logger.Error(err)
-			cookie.Maxlife = maxlife
-		case "cookie-nocache":
-			nocache, err := utils.GetBoolValue(v.Value, "cookie-nocache")
-			logger.Error(err)
-			cookie.Nocache = nocache
-		case "cookie-persistence":
-			cookie.Name = utils.PtrString(v.Value)
-		case "cookie-postonly":
-			postonly, err := utils.GetBoolValue(v.Value, "cookie-postonly")
-			logger.Error(err)
-			cookie.Postonly = postonly
-		case "cookie-preserve":
-			preserve, err := utils.GetBoolValue(v.Value, "cookie-preserve")
-			logger.Error(err)
-			cookie.Preserve = preserve
-		case "cookie-secure":
-			secure, err := utils.GetBoolValue(v.Value, "cookie-secure")
-			logger.Error(err)
-			cookie.Secure = secure
-		case "cookie-type":
-			cookie.Type = v.Value
-		}
-	}
-	return cookie
-}
-
-func (c *HAProxyController) getServerAnnotations(ingress *store.Ingress, service *store.Service) (srvAnnotations map[string]*store.StringW, activeAnnotations bool) {
-	srvAnnotations = make(map[string]*store.StringW, 5)
-	srvAnnotations["cookie-persistence"], _ = c.Store.GetValueFromAnnotations("cookie-persistence", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	srvAnnotations["check"], _ = c.Store.GetValueFromAnnotations("check", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	srvAnnotations["check-interval"], _ = c.Store.GetValueFromAnnotations("check-interval", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	srvAnnotations["pod-maxconn"], _ = c.Store.GetValueFromAnnotations("pod-maxconn", service.Annotations)
-	srvAnnotations["server-ssl"], _ = c.Store.GetValueFromAnnotations("server-ssl", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
-	srvAnnotations["send-proxy-protocol"], _ = c.Store.GetValueFromAnnotations("send-proxy-protocol", service.Annotations)
-	for k, v := range srvAnnotations {
-		if v == nil {
-			delete(srvAnnotations, k)
-			continue
-		}
-		if v.Status != EMPTY {
-			activeAnnotations = true
-			break
-		}
-	}
-	return srvAnnotations, activeAnnotations
-}

controller/backend-switching.go
@@ -1,162 +1,0 @@
-// Copyright 2019 HAProxy Technologies LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package controller
-
-import (
-	"fmt"
-	"sort"
-
-	"github.com/haproxytech/models/v2"
-
-	"github.com/haproxytech/kubernetes-ingress/controller/utils"
-)
-
-type UseBackendRules map[string]UseBackendRule
-
-type UseBackendRule struct {
-	Host       string
-	Path       string
-	ExactMatch bool
-	Backend    string
-	Namespace  string
-}
-
-func (c *HAProxyController) addUseBackendRule(key string, rule UseBackendRule, frontends ...string) {
-	for _, frontendName := range frontends {
-		c.cfg.BackendSwitchingRules[frontendName][key] = rule
-		c.cfg.BackendSwitchingModified[frontendName] = struct{}{}
-	}
-}
-
-func (c *HAProxyController) deleteUseBackendRule(key string, frontends ...string) {
-	for _, frontendName := range frontends {
-		delete(c.cfg.BackendSwitchingRules[frontendName], key)
-		c.cfg.BackendSwitchingModified[frontendName] = struct{}{}
-	}
-}
-
-//  Recreate use_backend rules
-func (c *HAProxyController) refreshBackendSwitching() (reload bool) {
-	if len(c.cfg.BackendSwitchingModified) == 0 {
-		return false
-	}
-	logger.Debug("Updating Backend Switching rules")
-	frontends, err := c.Client.FrontendsGet()
-	if err != nil {
-		logger.Panic(err)
-		return false
-	}
-	// Active backend will hold backends in use
-	activeBackends := make(map[string]struct{})
-	for _, rateLimitTable := range rateLimitTables {
-		activeBackends[rateLimitTable] = struct{}{}
-	}
-	for _, frontend := range frontends {
-		activeBackends[frontend.DefaultBackend] = struct{}{}
-		useBackendRules, ok := c.cfg.BackendSwitchingRules[frontend.Name]
-		if !ok {
-			continue
-		}
-		sortedKeys := []string{}
-		for key, rule := range useBackendRules {
-			activeBackends[rule.Backend] = struct{}{}
-			sortedKeys = append(sortedKeys, key)
-		}
-		if _, ok := c.cfg.BackendSwitchingModified[frontend.Name]; !ok {
-			// No need to refresh rules if the use_backend rules
-			// of the frontend were not updated
-			continue
-		}
-		// host/path are part of use_backend keys, so sorting keys will
-		// result in sorted use_backend rules where the longest path will match first.
-		// Example:
-		// use_backend service-abc if { var(txn.host) example } { var(txn.path) -m beg /a/b/c }
-		// use_backend service-ab  if { var(txn.host) example } { var(txn.path) -m beg /a/b }
-		// use_backend service-a   if { var(txn.host) example } { var(txn.path) -m beg /a }
-		sort.Strings(sortedKeys)
-		c.Client.BackendSwitchingRuleDeleteAll(frontend.Name)
-		for _, key := range sortedKeys {
-			rule := useBackendRules[key]
-			var condTest string
-			switch frontend.Mode {
-			case "http":
-				if rule.Host != "" {
-					//TODO: provide option to do strict host matching
-					condTest = fmt.Sprintf("{ var(txn.host) %s } ", rule.Host)
-				}
-				if rule.Path != "" {
-					if rule.ExactMatch {
-						condTest = fmt.Sprintf("%s{ var(txn.path) %s }", condTest, rule.Path)
-					} else {
-						condTest = fmt.Sprintf("%s{ var(txn.path) -m beg %s }", condTest, rule.Path)
-					}
-				}
-				if condTest == "" {
-					logger.Infof("both Host and Path are empty for frontend %v with backend %v, SKIP", frontend, rule.Backend)
-					continue
-				}
-			case "tcp":
-				if rule.Host == "" {
-					logger.Infof("Empty SNI for backend %s, SKIP", rule.Backend)
-					continue
-				}
-				condTest = fmt.Sprintf("{ req_ssl_sni -i %s } ", rule.Host)
-			}
-			err := c.Client.BackendSwitchingRuleCreate(frontend.Name, models.BackendSwitchingRule{
-				Cond:     "if",
-				CondTest: condTest,
-				Name:     rule.Backend,
-				Index:    utils.PtrInt64(0),
-			})
-			logger.Panic(err)
-		}
-		reload = true
-		delete(c.cfg.BackendSwitchingModified, frontend.Name)
-	}
-	reload = c.clearBackends(activeBackends) || reload
-	return reload
-}
-
-// Remove unused backends
-func (c *HAProxyController) clearBackends(activeBackends map[string]struct{}) (reload bool) {
-	allBackends, err := c.Client.BackendsGet()
-	if err != nil {
-		return false
-	}
-	for _, backend := range allBackends {
-		if _, ok := activeBackends[backend.Name]; !ok {
-			logger.Debugf("Deleting backend '%s'", backend.Name)
-			if err := c.Client.BackendDelete(backend.Name); err != nil {
-				logger.Panic(err)
-			}
-			reload = true
-		}
-	}
-	return reload
-}
-
-func (c *HAProxyController) setDefaultBackend(backendName string) (err error) {
-	for _, frontendName := range []string{FrontendHTTP, FrontendHTTPS} {
-		frontend, e := c.Client.FrontendGet(frontendName)
-		if e == nil {
-			frontend.DefaultBackend = backendName
-			e = c.Client.FrontendEdit(frontend)
-		}
-		if e != nil {
-			err = e
-		}
-	}
-	return err
-}

controller/configuration.go
@@ -17,38 +17,34 @@ package controller
 import (
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/rules"
+	"github.com/haproxytech/kubernetes-ingress/controller/ingress"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 //Configuration represents k8s state
 
 type Configuration struct {
-	MapFiles                 haproxy.Maps
-	HAProxyRules             haproxy.Rules
-	BackendSwitchingRules    map[string]UseBackendRules
-	BackendSwitchingModified map[string]struct{}
-	HTTPS                    bool
-	SSLPassthrough           bool
-	UsedCerts                map[string]struct{}
+	MapFiles       haproxy.Maps
+	HAProxyRules   haproxy.Rules
+	IngressRoutes  ingress.Routes
+	HTTPS          bool
+	SSLPassthrough bool
+	UsedCerts      map[string]struct{}
 }
 
 //Init initialize configuration
 func (c *Configuration) Init(mapDir string) {
 
 	c.MapFiles = haproxy.NewMapFiles(mapDir)
+	c.IngressRoutes = ingress.NewRoutes()
 	logger.Panic(c.HAProxyRulesInit())
-
-	c.BackendSwitchingRules = make(map[string]UseBackendRules)
-	c.BackendSwitchingModified = make(map[string]struct{})
-	for _, frontend := range []string{FrontendHTTP, FrontendHTTPS, FrontendSSL} {
-		c.BackendSwitchingRules[frontend] = UseBackendRules{}
-	}
 }
 
 //Clean cleans all the statuses of various data that was changed
 //deletes them completely or just resets them if needed
 func (c *Configuration) Clean() {
 	c.MapFiles.Clean()
+	c.IngressRoutes = ingress.NewRoutes()
 	logger.Panic(c.HAProxyRulesInit())
 	rateLimitTables = []string{}
 }

controller/controller.go
@@ -30,6 +30,7 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	ingressRoute "github.com/haproxytech/kubernetes-ingress/controller/ingress"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
@@ -39,10 +40,12 @@ const (
 	Main        = "main"
 	TCPServices = "tcpservices"
 	Errorfiles  = "errorfiles"
-	//frontends
-	FrontendHTTP  = "http"
-	FrontendHTTPS = "https"
-	FrontendSSL   = "ssl"
+	//sections
+	FrontendHTTP       = "http"
+	FrontendHTTPS      = "https"
+	FrontendSSL        = "ssl"
+	HTTPDefaultBackend = "default_backend"
+	SSLDefaultBaceknd  = "ssl"
 	//Status
 	ADDED    = store.ADDED
 	DELETED  = store.DELETED
@@ -131,7 +134,6 @@ func (c *HAProxyController) Start(ctx context.Context, osArgs utils.OSArgs) {
 	if c.osArgs.PprofEnabled {
 		logger.Error(c.clientClosure(func() {
 			logger.Error(c.handlePprof())
-			c.refreshBackendSwitching()
 		}))
 	}
 
@@ -198,7 +200,7 @@ func (c *HAProxyController) updateHAProxy() error {
 
 	restart, reload := c.handleGlobalAnnotations()
 
-	reload = c.handleDefaultService() || reload
+	c.handleDefaultService()
 
 	usedCerts := map[string]struct{}{}
 	c.cfg.UsedCerts = usedCerts
@@ -213,21 +215,22 @@ func (c *HAProxyController) updateHAProxy() error {
 			}
 			// handle Default Backend
 			if ingress.DefaultBackend != nil {
-				reload = c.handleRoute(&IngressRoute{
+				c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
 					Namespace: namespace,
 					Ingress:   ingress,
 					Path:      ingress.DefaultBackend,
-				}) || reload
+				})
 			}
 			// handle Ingress rules
 			for _, rule := range ingress.Rules {
 				for _, path := range rule.Paths {
-					reload = c.handleRoute(&IngressRoute{
-						Namespace: namespace,
-						Ingress:   ingress,
-						Host:      rule.Host,
-						Path:      path,
-					}) || reload
+					c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
+						Namespace:      namespace,
+						Ingress:        ingress,
+						Host:           rule.Host,
+						Path:           path,
+						SSLPassthrough: c.sslPassthroughEnabled(namespace, ingress, path),
+					})
 				}
 			}
 			//handle certs
@@ -431,37 +434,37 @@ func (c *HAProxyController) handlePprof() (err error) {
 		return err
 	}
 	logger.Debug("pprof backend created")
-	useBackendRule := UseBackendRule{
-		Host:       "",
-		Path:       "/debug/pprof",
-		ExactMatch: false,
-		Backend:    pprofBackend,
-	}
-	c.addUseBackendRule("pprof", useBackendRule, FrontendHTTPS)
+	c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
+		Path: &store.IngressPath{
+			Path:           "/debug/pprof",
+			ExactPathMatch: false,
+		},
+		BackendName: pprofBackend,
+	})
 	return nil
 }
 
 // handleDefaultService configures HAProy default backend provided via cli param "default-backend-service"
-func (c *HAProxyController) handleDefaultService() (reload bool) {
+func (c *HAProxyController) handleDefaultService() {
 	dsvcData, _ := c.Store.GetValueFromAnnotations("default-backend-service")
 	dsvc := strings.Split(dsvcData.Value, "/")
 
 	if len(dsvc) != 2 {
 		logger.Errorf("default service invalid data")
-		return false
+		return
 	}
 	if dsvc[0] == "" || dsvc[1] == "" {
-		return false
+		return
 	}
 	namespace, ok := c.Store.Namespaces[dsvc[0]]
 	if !ok {
 		logger.Errorf("default service invalid namespace " + dsvc[0])
-		return false
+		return
 	}
 	service, ok := namespace.Services[dsvc[1]]
 	if !ok {
 		logger.Errorf("service '" + dsvc[1] + "' does not exist")
-		return false
+		return
 	}
 	ingress := &store.Ingress{
 		Namespace:   namespace.Name,
@@ -474,7 +477,7 @@ func (c *HAProxyController) handleDefaultService() (reload bool) {
 		ServicePortInt:   service.Ports[0].Port,
 		IsDefaultBackend: true,
 	}
-	return c.handleRoute(&IngressRoute{
+	c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
 		Namespace: namespace,
 		Ingress:   ingress,
 		Path:      path,
@@ -488,14 +491,65 @@ func (c *HAProxyController) clean() {
 	if c.PublishService != nil {
 		c.PublishService.Status = EMPTY
 	}
+	c.cfg.SSLPassthrough = false
 }
 
 // refresh controller state
 func (c *HAProxyController) refresh() (reload bool) {
+	reload, activeBackends := c.cfg.IngressRoutes.Refresh(c.Client, c.Store)
+	reload = c.clearBackends(activeBackends) || reload
 	reload = c.cfg.HAProxyRules.Refresh(c.Client) || reload
-	reload = c.refreshBackendSwitching() || reload
 	r, err := c.cfg.MapFiles.Refresh()
 	reload = reload || r
 	logger.Error(err)
 	return reload
+}
+
+// Remove unused backends
+func (c *HAProxyController) clearBackends(activeBackends map[string]struct{}) (reload bool) {
+	// HTTP default backend
+	activeBackends[HTTPDefaultBackend] = struct{}{}
+	// SSL default backend
+	activeBackends[SSLDefaultBaceknd] = struct{}{}
+	// Ratelimting backends
+	for _, rateLimitTable := range rateLimitTables {
+		activeBackends[rateLimitTable] = struct{}{}
+	}
+	allBackends, err := c.Client.BackendsGet()
+	if err != nil {
+		return false
+	}
+	for _, backend := range allBackends {
+		if _, ok := activeBackends[backend.Name]; !ok {
+			logger.Debugf("Deleting backend '%s'", backend.Name)
+			if err := c.Client.BackendDelete(backend.Name); err != nil {
+				logger.Panic(err)
+			}
+			reload = true
+		}
+	}
+	return reload
+}
+
+func (c *HAProxyController) sslPassthroughEnabled(namespace *store.Namespace, ingress *store.Ingress, path *store.IngressPath) bool {
+	var annSSLPassthrough *store.StringW
+	service, ok := namespace.Services[path.ServiceName]
+	if ok {
+		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", service.Annotations, ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
+	} else {
+		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", ingress.Annotations, c.Store.ConfigMaps[Main].Annotations)
+	}
+	enabled, err := utils.GetBoolValue(annSSLPassthrough.Value, "ssl-passthrough")
+	if err != nil {
+		logger.Errorf("ssl-passthrough annotation: %s", err)
+		return false
+	}
+	if annSSLPassthrough.Status == DELETED {
+		return false
+	}
+	if enabled {
+		c.cfg.SSLPassthrough = true
+		return true
+	}
+	return false
 }

commit aac108476d6f925be176dc388855349e3ebf094e

BUG/MINOR: ssl-passthroug: uses incorrend backend name.
Regression in 7a1f4bb3b after updating the name of the
ssl-passthroug backend (from "https" to "ssl"), disableSSLPassthrough is
still using the old name.

controller/handler-https.go
@@ -147,12 +147,11 @@ func (h HTTPS) enableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (
 }
 
 func (h HTTPS) disableSSLPassthrough(cfg *Configuration, api api.HAProxyClient) (err error) {
-	backendHTTPS := "https"
 	err = api.FrontendDelete(FrontendSSL)
 	if err != nil {
 		return err
 	}
-	err = api.BackendDelete(backendHTTPS)
+	err = api.BackendDelete(SSLDefaultBaceknd)
 	if err != nil {
 		return err
 	}