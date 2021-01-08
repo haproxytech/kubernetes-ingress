ca151ba88199aaa3f5e8b6b36a9b5f9c91f6fa48
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2021-01-08 21:16:19 +0100

    CLEANUP/MINOR: remove cfgsnippet code left from refactor on 80839ac88

--- controller/configsnippet/overriden.go
+++
@@ -1,7 +0,0 @@
-package configsnippet
-
-type Overridden interface {
-	// Utility for checking if the attribute name has been already declared inside the configuration snippet:
-	// this is going to be appended at the end of the section, overwriting the previous declared one.
-	Overridden(configSnippet string) error
-}
