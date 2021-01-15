haproxytech/kubernetes-ingress

commit 5e3fc89fbe53ef1b8ccdab9b42917a9d6f2c41d7

MINOR: Rely on APIResourceList for Ingress API Discovery
Instead of inferring the supported Ingress API Groups
(networking, extensions) and their versions from the major and minor
versions of kubernetes servers. These are now discoverd by directly
asking k8s server for a list of supported resources.

controller/monitor.go
@@ -85,24 +85,25 @@ func (c *HAProxyController) monitorChanges() {
 		informersSynced = append(informersSynced, nsi.HasSynced)
 		c.k8s.EventsNamespaces(nsChan, stop, nsi)
 
-		if c.k8s.IsNetworkingV1ApiSupported() {
-			// Enabling networking.k8s.io/v1 Ingress support only to >= 1.19
-			ii := factory.Networking().V1().Ingresses().Informer()
-			informersSynced = append(informersSynced, ii.HasSynced)
-			c.k8s.EventsIngresses(ingChan, stop, ii)
-		}
-		// Handling deprecated Ingress resources:
-		// https://github.com/kubernetes/enhancements/issues/1453
 		var ii cache.SharedIndexInformer
-		if c.k8s.IsNetworkingV1Beta1ApiSupported() {
-			//logger.Warningf("Running on Kubernetes < 1.22: using ingresses.networking.k8s.io/v1beta1 for the Ingress/v1beta1 resources")
-			ii = factory.Networking().V1beta1().Ingresses().Informer()
-		} else {
-			logger.Warningf("Running on Kubernetes < 1.14: using ingresses.extensions/v1beta1 for the Ingress shared informer, networking.k8s.io API group is not available")
-			ii = factory.Extensions().V1beta1().Ingresses().Informer()
+		for i, apiGroup := range []string{"networking.k8s.io/v1", "networking.k8s.io/v1beta1", "extensions/v1beta1"} {
+			resources, _ := c.k8s.API.ServerResourcesForGroupVersion(apiGroup)
+			for _, rs := range resources.APIResources {
+				if rs.Kind == "Ingress" {
+					switch i {
+					case 0:
+						ii = factory.Networking().V1().Ingresses().Informer()
+					case 1:
+						ii = factory.Networking().V1beta1().Ingresses().Informer()
+					case 2:
+						ii = factory.Extensions().V1beta1().Ingresses().Informer()
+					}
+					informersSynced = append(informersSynced, ii.HasSynced)
+					c.k8s.EventsIngresses(ingChan, stop, ii)
+					break
+				}
+			}
 		}
-		informersSynced = append(informersSynced, ii.HasSynced)
-		c.k8s.EventsIngresses(ingChan, stop, ii)
 
 		ci := factory.Core().V1().ConfigMaps().Informer()
 		informersSynced = append(informersSynced, ci.HasSynced)

commit 94fd76781cd14f19ed8ff5ed86a219270c87b56b

BUG/MEDIUM: avoid nil pointer dereference in k8s versions < 1.14
Regression introduced in 5e3fc89fbe when updating API Discovery for
Ingress resources.

controller/monitor.go
@@ -86,9 +86,12 @@ func (c *HAProxyController) monitorChanges() {
 		informersSynced = append(informersSynced, nsi.HasSynced)
 		c.k8s.EventsNamespaces(nsChan, stop, nsi)
 
+		var ii, ici cache.SharedIndexInformer
 		for i, apiGroup := range []string{"networking.k8s.io/v1", "networking.k8s.io/v1beta1", "extensions/v1beta1"} {
-			var ii, ici cache.SharedIndexInformer
-			resources, _ := c.k8s.API.ServerResourcesForGroupVersion(apiGroup)
+			resources, err := c.k8s.API.ServerResourcesForGroupVersion(apiGroup)
+			if err != nil {
+				continue
+			}
 			for _, rs := range resources.APIResources {
 				if rs.Name == "ingresses" {
 					switch i {
@@ -99,6 +102,7 @@ func (c *HAProxyController) monitorChanges() {
 					case 2:
 						ii = factory.Extensions().V1beta1().Ingresses().Informer()
 					}
+					logger.Debugf("watching ingress resources of apiGroup %s:", apiGroup)
 					informersSynced = append(informersSynced, ii.HasSynced)
 					c.k8s.EventsIngresses(ingChan, stop, ii)
 				}
@@ -113,6 +117,9 @@ func (c *HAProxyController) monitorChanges() {
 					c.k8s.EventsIngressClass(ingClassChan, stop, ici)
 				}
 			}
+		}
+		if ii == nil {
+			logger.Panic("ingress resources not supported in this cluster")
 		}
 
 		ci := factory.Core().V1().ConfigMaps().Informer()