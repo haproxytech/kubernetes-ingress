f6f1c55922adf029881c9514931d7e29053d2ae6
ivan matmati <imatmati@haproxy.com>
2021-03-01 01:08:55 +0100

    TEST: Reorg send proxy test

---
+++ deploy/tests/e2e/proxy-protocol/send_proxy_test.go
@@ -0,0 +1,38 @@
+// Copyright 2019 HAProxy Technologies LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build integration
+
+package proxyprotocol
+
+import (
+	"io/ioutil"
+
+	"github.com/haproxytech/kubernetes-ingress/deploy/tests/e2e"
+)
+
+func (suite *ProxyProtocolSuite) Test_Send_Proxy() {
+	suite.Eventually(func() bool {
+		res, cls, err := suite.client.Do()
+		if err != nil {
+			suite.FailNow(err.Error())
+		}
+		defer cls()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return false
+		}
+		return string(body) == "hello!"
+	}, e2e.WaitDuration, e2e.TickDuration)
+}
