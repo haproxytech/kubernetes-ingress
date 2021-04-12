haproxytech/kubernetes-ingress

commit 18c7192d051615cd59a2f3364e10e4982933761a

REFACTOR/MEDIUM: refactor secrets handling code and move it to haproxy package
The goal of this refactoring is to handle haproxy related secrets in a
central place (haproxy.Certificates)

controller/haproxy/certificates.go
@@ -1,0 +1,175 @@
+package haproxy
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
+)
+
+type Certificates struct {
+	//Index is secretPath
+	// Value is true if secret is used otherwise false
+	frontend map[string]bool
+}
+
+type SecretType int
+
+//nolint
+const (
+	NONE_CERT SecretType = iota
+	FT_CERT
+	FT_DEFAULT_CERT
+)
+
+type SecretCtx struct {
+	DefaultNS  string
+	SecretPath string
+	SecretType SecretType
+}
+
+var certDir string
+
+func NewCertificates(dir string) *Certificates {
+	certDir = dir
+	return &Certificates{
+		frontend: make(map[string]bool),
+	}
+}
+
+func (c *Certificates) HandleTLSSecret(k8s store.K8s, secretCtx SecretCtx) (reload bool) {
+	secret := fetchSecret(k8s, secretCtx.SecretPath, secretCtx.DefaultNS)
+	if secret == nil || secret.Status == store.DELETED {
+		return false
+	}
+	certName := ""
+	switch secretCtx.SecretType {
+	case FT_DEFAULT_CERT:
+		// starting filename with "0" makes it first cert to be picked by HAProxy when no SNI matches.
+		certName = path.Join(certDir, fmt.Sprintf("0_%s_%s.pem", secret.Namespace, secret.Name))
+	case FT_CERT:
+		certName = path.Join(certDir, fmt.Sprintf("%s_%s.pem", secret.Namespace, secret.Name))
+	default:
+		logger.Error("unspecified context")
+		return false
+	}
+	if _, ok := c.frontend[certName]; ok {
+		c.frontend[certName] = true
+		return false
+	}
+	err := writeSecret(secret, certName)
+	if err != nil {
+		logger.Error(err)
+		return false
+	}
+	c.frontend[certName] = true
+	return true
+}
+
+func (c *Certificates) Clean() {
+	for i := range c.frontend {
+		c.frontend[i] = false
+	}
+}
+
+func (c *Certificates) FrontendCertsEnabled() bool {
+	for _, used := range c.frontend {
+		if used {
+			return true
+		}
+	}
+	return false
+}
+
+// Refresh removes unused certs from HAProxyCertDir
+// Certificates will remain in HAProxy memory until next Reload
+func (c *Certificates) Refresh() {
+	files, err := ioutil.ReadDir(certDir)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		filename := f.Name()
+		used := c.frontend[filename]
+		if !used {
+			logger.Error(os.Remove(path.Join(certDir, filename)))
+			delete(c.frontend, filename)
+		}
+	}
+}
+
+func fetchSecret(k8s store.K8s, defaultNs, secretPath string) (secret *store.Secret) {
+	secretName := ""
+	secretNamespace := defaultNs
+	parts := strings.Split(secretPath, "/")
+	if len(parts) > 1 {
+		secretNamespace = parts[0]
+		secretName = parts[1]
+	} else {
+		secretName = parts[0] // only secretname is here
+	}
+	ns, namespaceOK := k8s.Namespaces[secretNamespace]
+	if !namespaceOK {
+		logger.Warningf("namespace [%s] does not exist, ignoring.", secretNamespace)
+		return nil
+	}
+	secret, secretOK := ns.Secret[secretName]
+	if !secretOK {
+		logger.Warningf("secret [%s/%s] does not exist, ignoring.", secretNamespace, secretName)
+		return nil
+	}
+	return secret
+}
+
+func writeSecret(secret *store.Secret, certName string) (err error) {
+	for _, k := range []string{"tls", "rsa", "ecdsa"} {
+		key := secret.Data[k+".key"]
+		crt := secret.Data[k+".crt"]
+		if len(key) != 0 && len(crt) != 0 {
+			return writeCert(certName, key, crt)
+		}
+	}
+	return nil
+}
+
+func writeCert(filename string, key, crt []byte) error {
+	var f *os.File
+	var err error
+	if f, err = os.Create(filename); err != nil {
+		logger.Error(err)
+		return err
+	}
+	defer f.Close()
+	if _, err = f.Write(key); err != nil {
+		logger.Error(err)
+		return err
+	}
+	//Force writing a newline so that parsing does not barf
+	if len(key) > 0 && key[len(key)-1] != byte('\n') {
+		logger.Warningf("secret key in %s does not end with \\n, appending it to avoid mangling key and certificate", filename)
+		if _, err = f.WriteString("\n"); err != nil {
+			logger.Error(err)
+			return err
+		}
+	}
+	if _, err = f.Write(crt); err != nil {
+		logger.Error(err)
+		return err
+	}
+	if err = f.Sync(); err != nil {
+		logger.Error(err)
+		return err
+	}
+	if err = f.Close(); err != nil {
+		logger.Error(err)
+		return err
+	}
+	return nil
+}

controller/configuration.go
@@ -28,25 +28,27 @@ import (
 type Configuration struct {
 	MapFiles       haproxy.Maps
 	HAProxyRules   haproxy.Rules
+	Certificates   *haproxy.Certificates
 	IngressRoutes  ingress.Routes
 	HTTPS          bool
 	SSLPassthrough bool
-	UsedCerts      map[string]struct{}
 }
 
 //Init initialize configuration
-func (c *Configuration) Init(mapDir string) {
+func (c *Configuration) Init() {
 
-	c.MapFiles = haproxy.NewMapFiles(mapDir)
+	c.MapFiles = haproxy.NewMapFiles(HAProxyMapDir)
 	c.MapFiles.SetPreserve(true, SNI, HOST, PATH_EXACT, PATH_PREFIX)
 	c.IngressRoutes = ingress.Routes{}
 	logger.Panic(c.HAProxyRulesInit())
+	c.Certificates = haproxy.NewCertificates(HAProxyCertDir)
 }
 
 //Clean cleans all the statuses of various data that was changed
 //deletes them completely or just resets them if needed
 func (c *Configuration) Clean() {
 	c.MapFiles.Clean()
+	c.Certificates.Clean()
 	c.IngressRoutes = ingress.Routes{}
 	logger.Panic(c.HAProxyRulesInit())
 	rateLimitTables = []string{}

controller/controller.go
@@ -29,6 +29,7 @@ import (
 	"github.com/haproxytech/models/v2"
 	"k8s.io/apimachinery/pkg/watch"
 
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	ingressRoute "github.com/haproxytech/kubernetes-ingress/controller/ingress"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
@@ -204,11 +205,8 @@ func (c *HAProxyController) updateHAProxy() error {
 	}()
 
 	restart, reload := c.handleGlobalAnnotations()
-
+	reload = c.handleDefaultCert() || reload
 	c.handleDefaultService()
-
-	usedCerts := map[string]struct{}{}
-	c.cfg.UsedCerts = usedCerts
 
 	for _, namespace := range c.Store.Namespaces {
 		if !namespace.Relevant {
@@ -226,12 +224,13 @@ func (c *HAProxyController) updateHAProxy() error {
 					Path:      ingress.DefaultBackend,
 				})
 			}
-			ingressSecrets := map[string]struct{}{}
+			// Ingress secrets
 			for _, tls := range ingress.TLS {
-				if _, ok := ingressSecrets[tls.SecretName.Value]; !ok {
-					ingressSecrets[tls.SecretName.Value] = struct{}{}
-					reload = c.handleTLSSecret(*ingress, *tls, usedCerts) || reload
-				}
+				reload = c.cfg.Certificates.HandleTLSSecret(c.Store, haproxy.SecretCtx{
+					DefaultNS:  ingress.Namespace,
+					SecretPath: tls.SecretName.Value,
+					SecretType: haproxy.FT_CERT,
+				}) || reload
 			}
 			// Ingress annotations
 			if len(ingress.Rules) == 0 {
@@ -385,7 +384,7 @@ func (c *HAProxyController) haproxyInitialize() {
 	logger.Error(err)
 	logger.Printf("Running on %s", hostname)
 
-	c.cfg.Init(HAProxyMapDir)
+	c.cfg.Init()
 }
 
 // handleBind configures Frontends bind lines
@@ -450,6 +449,9 @@ func (c *HAProxyController) handlePprof() (err error) {
 // handleDefaultService configures HAProy default backend provided via cli param "default-backend-service"
 func (c *HAProxyController) handleDefaultService() {
 	dsvcData, _ := c.Store.GetValueFromAnnotations("default-backend-service")
+	if dsvcData == nil {
+		return
+	}
 	dsvc := strings.Split(dsvcData.Value, "/")
 
 	if len(dsvc) != 2 {
@@ -484,6 +486,18 @@ func (c *HAProxyController) handleDefaultService() {
 		Namespace: namespace,
 		Ingress:   ingress,
 		Path:      path,
+	})
+}
+
+// handleDefaultCert configures default/fallback HAProxy certificate to use for client HTTPS requests.
+func (c *HAProxyController) handleDefaultCert() (reload bool) {
+	secretAnn, _ := c.Store.GetValueFromAnnotations("ssl-certificate", c.Store.ConfigMaps[Main].Annotations)
+	if secretAnn == nil {
+		return false
+	}
+	return c.cfg.Certificates.HandleTLSSecret(c.Store, haproxy.SecretCtx{
+		SecretPath: secretAnn.Value,
+		SecretType: haproxy.FT_CERT,
 	})
 }
 

controller/handler-default-certificate.go
@@ -1,49 +1,0 @@
-// Copyright 2019 HAProxy Technologies LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package controller
-
-import (
-	"strings"
-
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
-	"github.com/haproxytech/kubernetes-ingress/controller/store"
-)
-
-type DefaultCertificate struct{}
-
-func (d DefaultCertificate) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (reload bool, err error) {
-	secretAnn, defSecretErr := k.GetValueFromAnnotations("ssl-certificate", k.ConfigMaps[Main].Annotations)
-	writeSecret := false
-	if defSecretErr == nil {
-		if secretAnn.Status != DELETED && secretAnn.Status != EMPTY {
-			writeSecret = true
-		}
-		secretData := strings.Split(secretAnn.Value, "/")
-		namespace, namespaceOK := k.Namespaces[secretData[0]]
-		if len(secretData) == 2 && namespaceOK {
-			secret, ok := namespace.Secret[secretData[1]]
-			if ok {
-				if secret.Status != EMPTY && secret.Status != DELETED {
-					writeSecret = true
-				}
-				reload, err = HandleSecret(store.Ingress{
-					Name: "0",
-				}, *secret, writeSecret, cfg.UsedCerts, logger)
-				return reload, err
-			}
-		}
-	}
-	return false, nil
-}

controller/handler-https.go
@@ -16,9 +16,6 @@ package controller
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path"
 
 	"github.com/haproxytech/models/v2"
 
@@ -78,7 +75,7 @@ func (h HTTPS) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (r
 		return false, nil
 	}
 	// ssl-offload
-	if len(cfg.UsedCerts) > 0 {
+	if cfg.Certificates.FrontendCertsEnabled() {
 		if !cfg.HTTPS {
 			logger.Panic(api.FrontendEnableSSLOffload(FrontendHTTPS, h.certDir, true))
 			cfg.HTTPS = true
@@ -106,8 +103,6 @@ func (h HTTPS) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (r
 		cfg.SSLPassthrough = false
 		reload = true
 	}
-	//remove certs that are not needed
-	logger.Error(h.CleanCertDir(cfg.UsedCerts))
 
 	return reload, nil
 }
@@ -178,25 +173,6 @@ func (h HTTPS) toggleSSLPassthrough(passthrough, offload bool, api api.HAProxyCl
 	}
 	if offload {
 		logger.Panic(api.FrontendEnableSSLOffload(FrontendHTTPS, h.certDir, true))
-	}
-	return nil
-}
-
-func (h HTTPS) CleanCertDir(usedCerts map[string]struct{}) error {
-	files, err := ioutil.ReadDir(HAProxyCertDir)
-	if err != nil {
-		return err
-	}
-
-	for _, f := range files {
-		if f.IsDir() {
-			continue
-		}
-		filename := path.Join(HAProxyCertDir, f.Name())
-		_, isOK := usedCerts[filename]
-		if !isOK {
-			os.Remove(filename)
-		}
 	}
 	return nil
 }

controller/handler-refresh.go
@@ -26,6 +26,7 @@ func (h RefreshHandler) Update(k store.K8s, cfg *Configuration, api api.HAProxyC
 	h.clearBackends(api, cfg, activeBackends)
 	reload = cfg.HAProxyRules.Refresh(api) || reload
 	reload = cfg.MapFiles.Refresh(api) || reload
+	cfg.Certificates.Refresh()
 	return reload, nil
 }
 

controller/handler.go
@@ -27,7 +27,6 @@ func (c *HAProxyController) initHandlers() {
 	c.UpdateHandlers = []UpdateHandler{
 		SourceIPHeader{},
 		ProxyProtocol{},
-		DefaultCertificate{},
 		ErrorFile{},
 		HTTPS{
 			enabled:  !c.osArgs.DisableHTTPS,

controller/store/annotations.go
@@ -120,6 +120,9 @@ func (k K8s) GetValueFromAnnotations(annotationName string, annotations ...MapSt
 }
 
 func (k K8s) SetDefaultAnnotation(annotation, value string) {
+	if value == "" {
+		return
+	}
 	defaultAnnotationValues[annotation] = &StringW{
 		Value:  value,
 		Status: ADDED,

commit 50d5f6649e16ac1542483807bda8e13f497b5e61

BUG/MINOR: Fix regression when loading multiple cert formats
Regression introduced in 18c7192d0. Controller should be able to handle
a kubernetes secret containing multiple certificate formats (ecdsa,rsa)
In addition in this commit controller will also parse "dsa" key in
secret.

controller/haproxy/certificates.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
@@ -13,9 +14,14 @@ import (
 type Certificates struct {
 	// Index is secretPath
 	// Value is true if secret is used otherwise false
-	frontend map[string]bool
-	backend  map[string]bool
-	ca       map[string]bool
+	frontend map[string]*cert
+	backend  map[string]*cert
+	ca       map[string]*cert
+}
+
+type cert struct {
+	path  string
+	inUse bool
 }
 
 type SecretType int
@@ -45,9 +51,9 @@ func NewCertificates(caDir, ftDir, bdDir string) *Certificates {
 	backendCertDir = bdDir
 	caCertDir = caDir
 	return &Certificates{
-		frontend: make(map[string]bool),
-		backend:  make(map[string]bool),
-		ca:       make(map[string]bool),
+		frontend: make(map[string]*cert),
+		backend:  make(map[string]*cert),
+		ca:       make(map[string]*cert),
 	}
 }
 
@@ -60,58 +66,63 @@ func (c *Certificates) HandleTLSSecret(k8s store.K8s, secretCtx SecretCtx) (cert
 	if secret.Status == store.DELETED {
 		return "", true, nil
 	}
-	var certs map[string]bool
-	certName := ""
-	var privateKeyNull bool
+
+	var certs map[string]*cert
+	var crt *cert
+	var crtOk, privateKeyNull bool
+	var certName string
 	switch secretCtx.SecretType {
 	case FT_DEFAULT_CERT:
 		// starting filename with "0" makes it first cert to be picked by HAProxy when no SNI matches.
-		certName = fmt.Sprintf("0_%s_%s.pem", secret.Namespace, secret.Name)
+		certName = fmt.Sprintf("0_%s_%s", secret.Namespace, secret.Name)
 		certPath = path.Join(frontendCertDir, certName)
 		certs = c.frontend
 	case FT_CERT:
-		certName = fmt.Sprintf("%s_%s.pem", secret.Namespace, secret.Name)
+		certName = fmt.Sprintf("%s_%s", secret.Namespace, secret.Name)
 		certPath = path.Join(frontendCertDir, certName)
 		certs = c.frontend
 	case BD_CERT:
-		certName = fmt.Sprintf("%s_%s.pem", secret.Namespace, secret.Name)
+		certName = fmt.Sprintf("%s_%s", secret.Namespace, secret.Name)
 		certPath = path.Join(backendCertDir, certName)
 		certs = c.backend
 	case CA_CERT:
-		certName = fmt.Sprintf("%s_%s.pem", secret.Namespace, secret.Name)
+		certName = fmt.Sprintf("%s_%s", secret.Namespace, secret.Name)
 		certPath = path.Join(caCertDir, certName)
 		certs = c.ca
 		privateKeyNull = true
 	default:
 		return "", false, errors.New("unspecified context")
 	}
-	if _, ok := certs[certName]; ok && secret.Status == store.EMPTY {
-		certs[certName] = true
-		return certPath, false, nil
+	crt, crtOk = certs[certName]
+	if crtOk && secret.Status == store.EMPTY {
+		crt.inUse = true
+		return crt.path, false, nil
 	}
-	err = writeSecret(secret, certPath, privateKeyNull)
+	crt = &cert{path: certPath}
+	err = writeSecret(secret, crt, privateKeyNull)
 	if err != nil {
 		return "", false, err
 	}
-	certs[certName] = true
-	return certPath, true, nil
+	crt.inUse = true
+	certs[certName] = crt
+	return crt.path, true, nil
 }
 
 func (c *Certificates) Clean() {
 	for i := range c.frontend {
-		c.frontend[i] = false
+		c.frontend[i].inUse = false
 	}
 	for i := range c.backend {
-		c.backend[i] = false
+		c.backend[i].inUse = false
 	}
 	for i := range c.ca {
-		c.ca[i] = false
+		c.ca[i].inUse = false
 	}
 }
 
 func (c *Certificates) FrontendCertsEnabled() bool {
-	for _, used := range c.frontend {
-		if used {
+	for _, cert := range c.frontend {
+		if cert.inUse {
 			return true
 		}
 	}
@@ -119,7 +130,6 @@ func (c *Certificates) FrontendCertsEnabled() bool {
 }
 
 // Refresh removes unused certs from HAProxyCertDir
-// Certificates will remain in HAProxy memory until next Reload
 func (c *Certificates) Refresh() (reload bool) {
 	reload = refreshCerts(c.frontend, frontendCertDir)
 	reload = refreshCerts(c.backend, backendCertDir) || reload
@@ -127,7 +137,7 @@ func (c *Certificates) Refresh() (reload bool) {
 	return
 }
 
-func refreshCerts(certs map[string]bool, certDir string) (reload bool) {
+func refreshCerts(certs map[string]*cert, certDir string) (reload bool) {
 	files, err := ioutil.ReadDir(certDir)
 	if err != nil {
 		logger.Error(err)
@@ -138,10 +148,11 @@ func refreshCerts(certs map[string]bool, certDir string) (reload bool) {
 			continue
 		}
 		filename := f.Name()
-		used := certs[filename]
-		if !used {
+		// certificate file name should be already in the format: certName.pem
+		certName := strings.Split(filename, ".")[0]
+		if !certs[certName].inUse {
 			logger.Error(os.Remove(path.Join(certDir, filename)))
-			delete(certs, filename)
+			delete(certs, certName)
 			reload = true
 			logger.Debug("Unused certificates removed, reload required")
 		}
@@ -149,21 +160,38 @@ func refreshCerts(certs map[string]bool, certDir string) (reload bool) {
 	return
 }
 
-func writeSecret(secret *store.Secret, certPath string, privateKeyNull bool) (err error) {
-	for _, k := range []string{"tls", "rsa", "ecdsa"} {
-		key := secret.Data[k+".key"]
-		if key == nil {
-			if privateKeyNull {
-				key = []byte("")
-			} else {
-				return fmt.Errorf("private key missing in %s/%s", secret.Namespace, secret.Name)
+func writeSecret(secret *store.Secret, c *cert, privateKeyNull bool) (err error) {
+	var crtValue, keyValue []byte
+	var crtOk, keyOk, pemOk bool
+	var certPath string
+	if privateKeyNull {
+		crtValue, crtOk = secret.Data["tls.crt"]
+		if !crtOk {
+			return fmt.Errorf("certificate missing in %s/%s", secret.Namespace, secret.Name)
+		}
+		c.path = fmt.Sprintf("%s.pem", c.path)
+		return writeCert(c.path, []byte(""), crtValue)
+	}
+	for _, k := range []string{"tls", "rsa", "ecdsa", "dsa"} {
+		keyValue, keyOk = secret.Data[k+".key"]
+		crtValue, crtOk = secret.Data[k+".crt"]
+		if keyOk && crtOk {
+			pemOk = true
+			certPath = fmt.Sprintf("%s.pem", c.path)
+			if k != "tls" {
+				// HAProxy "cert bundle"
+				certPath = fmt.Sprintf("%s.%s", certPath, k)
+			}
+			err = writeCert(certPath, keyValue, crtValue)
+			if err != nil {
+				return err
 			}
 		}
-		crt := secret.Data[k+".crt"]
-		if len(crt) != 0 {
-			return writeCert(certPath, key, crt)
-		}
 	}
+	if !pemOk {
+		return fmt.Errorf("certificate or private key missing in %s/%s", secret.Namespace, secret.Name)
+	}
+	c.path = certPath
 	return nil
 }
 