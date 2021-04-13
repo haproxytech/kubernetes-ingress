haproxytech/kubernetes-ingress

commit bf9e353417ff246073bdb6c7a3d6799c201ed33d

REORG/MAJOR: handle ingress routes and services seperately
"controller/ingress" module used to handle everything related to an
ingress route: haproxy route (backend Switching), service (backend) and
endpoints (backend's servers).
In order to make this easier to maintain and easier to improve (like
adding additional routing options), code handling a service and its
endpoints was refactored in a seperate module "controller/service" and
code handling Ingress routes is refactored in "controller/route".

controller/configuration.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/rules"
-	"github.com/haproxytech/kubernetes-ingress/controller/ingress"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
@@ -29,7 +28,7 @@ type Configuration struct {
 	MapFiles       haproxy.Maps
 	HAProxyRules   *haproxy.Rules
 	Certificates   *haproxy.Certificates
-	IngressRoutes  ingress.Routes
+	ActiveBackends map[string]struct{}
 	HTTPS          bool
 	SSLPassthrough bool
 }
@@ -38,9 +37,9 @@ type Configuration struct {
 func (c *Configuration) Init() {
 	c.MapFiles = haproxy.NewMapFiles(MapDir)
 	c.MapFiles.SetPreserve(true, SNI, HOST, PATH_EXACT, PATH_PREFIX)
-	c.IngressRoutes = ingress.Routes{}
 	logger.Panic(c.HAProxyRulesInit())
 	c.Certificates = haproxy.NewCertificates(CaCertDir, FrontendCertDir, BackendCertDir)
+	c.ActiveBackends = make(map[string]struct{})
 }
 
 // Clean cleans all the statuses of various data that was changed
@@ -48,7 +47,6 @@ func (c *Configuration) Init() {
 func (c *Configuration) Clean() {
 	c.MapFiles.Clean()
 	c.Certificates.Clean()
-	c.IngressRoutes = ingress.Routes{}
 	logger.Panic(c.HAProxyRulesInit())
 	rateLimitTables = []string{}
 }

controller/controller.go
@@ -28,7 +28,7 @@ import (
 	"github.com/haproxytech/kubernetes-ingress/controller/annotations"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
-	ingressRoute "github.com/haproxytech/kubernetes-ingress/controller/ingress"
+	"github.com/haproxytech/kubernetes-ingress/controller/route"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
@@ -147,7 +147,7 @@ func (c *HAProxyController) updateHAProxy() {
 		c.Store.ConfigMaps.Main.Annotations,
 	)
 	reload = c.handleDefaultCert() || reload
-	c.handleDefaultService()
+	reload = c.handleDefaultService() || reload
 
 	for _, namespace := range c.Store.Namespaces {
 		if !namespace.Relevant {
@@ -164,13 +164,12 @@ func (c *HAProxyController) updateHAProxy() {
 			if c.PublishService != nil {
 				logger.Error(c.k8s.UpdateIngressStatus(ingress, c.PublishService))
 			}
-			// Default Backend
 			if ingress.DefaultBackend != nil {
-				logger.Error(c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
-					Namespace: namespace,
-					Ingress:   ingress,
-					Path:      ingress.DefaultBackend,
-				}))
+				if r, errSvc := c.setDefaultService(ingress, []string{FrontendHTTP, FrontendHTTPS}); errSvc != nil {
+					logger.Errorf("Ingress '%s/%s': default backend: %s", ingress.Namespace, ingress.Name, errSvc)
+				} else {
+					reload = reload || r
+				}
 			}
 			// Ingress secrets
 			logger.Tracef("ingress '%s/%s': processing secrets...", ingress.Namespace, ingress.Name)
@@ -199,14 +198,11 @@ func (c *HAProxyController) updateHAProxy() {
 			logger.Tracef("ingress '%s/%s': processing rules...", ingress.Namespace, ingress.Name)
 			for _, rule := range ingress.Rules {
 				for _, path := range rule.Paths {
-					logger.Error(c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
-						Namespace:      namespace,
-						Ingress:        ingress,
-						Host:           rule.Host,
-						Path:           path,
-						HAProxyRules:   c.cfg.HAProxyRules.GetIngressRuleIDs(ingress.Namespace + "-" + ingress.Name),
-						SSLPassthrough: c.sslPassthroughEnabled(ingress, path),
-					}))
+					if r, errIng := c.handleIngressPath(ingress, rule.Host, path); errIng != nil {
+						logger.Errorf("Ingress '%s/%s': %s", ingress.Namespace, ingress.Name, errIng)
+					} else {
+						reload = reload || r
+					}
 				}
 			}
 		}
@@ -436,19 +432,22 @@ func (c *HAProxyController) handlePprof() (err error) {
 		return err
 	}
 	logger.Debug("pprof backend created")
-	logger.Error(c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
+	err = route.AddHostPathRoute(route.Route{
+		BackendName: pprofBackend,
 		Path: &store.IngressPath{
 			Path:           "/debug/pprof",
 			ExactPathMatch: false,
 		},
-		BackendName:  pprofBackend,
-		LocalBackend: true,
-	}))
+	}, c.cfg.MapFiles)
+	if err != nil {
+		return err
+	}
+	c.cfg.ActiveBackends[pprofBackend] = struct{}{}
 	return nil
 }
 
 // handleDefaultService configures HAProy default backend provided via cli param "default-backend-service"
-func (c *HAProxyController) handleDefaultService() {
+func (c *HAProxyController) handleDefaultService() (reload bool) {
 	dsvcData, _ := c.Store.GetValueFromAnnotations("default-backend-service")
 	if dsvcData == nil {
 		return
@@ -456,7 +455,7 @@ func (c *HAProxyController) handleDefaultService() {
 	dsvc := strings.Split(dsvcData.Value, "/")
 
 	if len(dsvc) != 2 {
-		logger.Errorf("default service invalid data")
+		logger.Errorf("default service '%s': invalid format", dsvcData.Value)
 		return
 	}
 	if dsvc[0] == "" || dsvc[1] == "" {
@@ -464,30 +463,30 @@ func (c *HAProxyController) handleDefaultService() {
 	}
 	namespace, ok := c.Store.Namespaces[dsvc[0]]
 	if !ok {
-		logger.Errorf("default service invalid namespace " + dsvc[0])
+		logger.Errorf("default service '%s': namespace not found" + dsvc[0])
 		return
 	}
 	service, ok := namespace.Services[dsvc[1]]
 	if !ok {
-		logger.Errorf("service '" + dsvc[1] + "' does not exist")
+		logger.Errorf("default service '%s': service name not found" + dsvc[1])
 		return
 	}
 	ingress := &store.Ingress{
 		Namespace:   namespace.Name,
 		Name:        "DefaultService",
 		Annotations: store.MapStringW{},
-		Rules:       map[string]*store.IngressRule{},
+		DefaultBackend: &store.IngressPath{
+			SvcName:          service.Name,
+			SvcPortInt:       service.Ports[0].Port,
+			IsDefaultBackend: true,
+		},
 	}
-	path := &store.IngressPath{
-		SvcName:          service.Name,
-		SvcPortInt:       service.Ports[0].Port,
-		IsDefaultBackend: true,
+	reload, err := c.setDefaultService(ingress, []string{FrontendHTTP, FrontendHTTPS})
+	if err != nil {
+		logger.Errorf("default service '%s/%s': %s", namespace.Name, service.Name, err)
+		return
 	}
-	logger.Error(c.cfg.IngressRoutes.AddRoute(&ingressRoute.Route{
-		Namespace: namespace,
-		Ingress:   ingress,
-		Path:      path,
-	}))
+	return reload
 }
 
 // handleDefaultCert configures default/fallback HAProxy certificate to use for client HTTPS requests.
@@ -514,60 +513,4 @@ func (c *HAProxyController) clean(failedSync bool) {
 		return
 	}
 	c.Store.Clean()
-}
-
-func (c *HAProxyController) sslPassthroughEnabled(ingress *store.Ingress, path *store.IngressPath) bool {
-	var annSSLPassthrough *store.StringW
-	var service *store.Service
-	ok := false
-	if path != nil {
-		service, ok = c.Store.Namespaces[ingress.Namespace].Services[path.SvcName]
-	}
-	if ok {
-		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", service.Annotations, ingress.Annotations, c.Store.ConfigMaps.Main.Annotations)
-	} else {
-		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", ingress.Annotations, c.Store.ConfigMaps.Main.Annotations)
-	}
-	enabled, err := utils.GetBoolValue(annSSLPassthrough.Value, "ssl-passthrough")
-	if err != nil {
-		logger.Errorf("ssl-passthrough annotation: %s", err)
-		return false
-	}
-	if annSSLPassthrough.Status == DELETED {
-		return false
-	}
-	if enabled {
-		c.cfg.SSLPassthrough = true
-		return true
-	}
-	return false
-}
-
-// igClassIsSupported verifies if the IngressClass matches the ControllerClass
-// and in such case returns true otherwise false
-//
-// According to https://github.com/kubernetes/api/blob/master/networking/v1/types.go#L257
-// ingress.class annotation should have precedence over the IngressClass mechanism implemented
-// in "networking.k8s.io".
-func (c *HAProxyController) igClassIsSupported(ingress *store.Ingress) bool {
-	var igClassAnn string
-	var igClass *store.IngressClass
-	if ann, _ := c.Store.GetValueFromAnnotations("ingress.class", ingress.Annotations); ann != nil {
-		igClassAnn = ann.Value
-	}
-	if igClassAnn == "" || igClassAnn != c.IngressClass {
-		igClass = c.Store.IngressClasses[ingress.Class]
-		if igClass != nil && igClass.Status != DELETED && igClass.Controller == CONTROLLER_CLASS {
-			// Corresponding IngresClass was updated so Ingress resource should be re-processed
-			// This is particularly important if the Ingress was skipped due to mismatching ingrssClass
-			if igClass.Status != EMPTY {
-				ingress.Status = MODIFIED
-			}
-			return true
-		}
-	}
-	if igClassAnn == c.IngressClass {
-		return true
-	}
-	return false
 }

controller/ingress.go
@@ -1,0 +1,152 @@
+// Copyright 2019 HAProxy Technologies LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	"github.com/haproxytech/models/v2"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/route"
+	"github.com/haproxytech/kubernetes-ingress/controller/service"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
+)
+
+// igClassIsSupported verifies if the IngressClass matches the ControllerClass
+// and in such case returns true otherwise false
+//
+// According to https://github.com/kubernetes/api/blob/master/networking/v1/types.go#L257
+// ingress.class annotation should have precedence over the IngressClass mechanism implemented
+// in "networking.k8s.io".
+func (c *HAProxyController) igClassIsSupported(ingress *store.Ingress) bool {
+	var igClassAnn string
+	var igClass *store.IngressClass
+	if ann, _ := c.Store.GetValueFromAnnotations("ingress.class", ingress.Annotations); ann != nil {
+		igClassAnn = ann.Value
+	}
+	if igClassAnn == "" || igClassAnn != c.IngressClass {
+		igClass = c.Store.IngressClasses[ingress.Class]
+		if igClass != nil && igClass.Status != DELETED && igClass.Controller == CONTROLLER_CLASS {
+			// Corresponding IngresClass was updated so Ingress resource should be re-processed
+			// This is particularly important if the Ingress was skipped due to mismatching ingrssClass
+			if igClass.Status != EMPTY {
+				ingress.Status = MODIFIED
+			}
+			return true
+		}
+	}
+	if igClassAnn == c.IngressClass {
+		return true
+	}
+	return false
+}
+
+func (c *HAProxyController) handleIngressPath(ingress *store.Ingress, host string, path *store.IngressPath) (reload bool, err error) {
+	sslPassthrough := c.sslPassthroughEnabled(ingress, path)
+	svc, err := service.NewCtx(c.Store, ingress, path, sslPassthrough)
+	if err != nil {
+		return
+	}
+	if svc.GetStatus() == DELETED {
+		return
+	}
+	backendReload, backendName, err := svc.HandleBackend(c.Client, c.Store)
+	if err != nil {
+		return
+	}
+	err = route.AddHostPathRoute(route.Route{
+		Host:           host,
+		Path:           path,
+		HAProxyRules:   c.cfg.HAProxyRules.GetIngressRuleIDs(ingress.Namespace + "-" + ingress.Name),
+		BackendName:    backendName,
+		SSLPassthrough: sslPassthrough,
+	}, c.cfg.MapFiles)
+	if err != nil {
+		return
+	}
+	c.cfg.ActiveBackends[backendName] = struct{}{}
+	endpointsReload := svc.HandleEndpoints(c.Client, c.Store, c.cfg.Certificates)
+	reload = backendReload || endpointsReload
+	return
+}
+
+func (c *HAProxyController) setDefaultService(ingress *store.Ingress, frontends []string) (reload bool, err error) {
+	var frontend models.Frontend
+	var ftReload bool
+	frontend, err = c.Client.FrontendGet(frontends[0])
+	if err != nil {
+		return
+	}
+	tcpService := false
+	if frontend.Mode == "tcp" {
+		tcpService = true
+	}
+	svc, err := service.NewCtx(c.Store, ingress, ingress.DefaultBackend, tcpService)
+	if err != nil {
+		return
+	}
+	if svc.GetStatus() == DELETED {
+		return
+	}
+	bdReload, backendName, err := svc.HandleBackend(c.Client, c.Store)
+	if err != nil {
+		return
+	}
+	if frontend.DefaultBackend != backendName {
+		if frontend.Name == FrontendHTTP {
+			logger.Infof("Setting http default backend to '%s'", backendName)
+		}
+		for _, frontendName := range frontends {
+			frontend, _ := c.Client.FrontendGet(frontendName)
+			frontend.DefaultBackend = backendName
+			err = c.Client.FrontendEdit(frontend)
+			if err != nil {
+				return
+			}
+			ftReload = true
+			logger.Debugf("Setting '%s' default backend to '%s'", frontendName, backendName)
+			c.cfg.ActiveBackends[backendName] = struct{}{}
+		}
+	}
+	endpointsReload := svc.HandleEndpoints(c.Client, c.Store, c.cfg.Certificates)
+	reload = bdReload || ftReload || endpointsReload
+	return reload, err
+}
+
+func (c *HAProxyController) sslPassthroughEnabled(ingress *store.Ingress, path *store.IngressPath) bool {
+	var annSSLPassthrough *store.StringW
+	var service *store.Service
+	ok := false
+	if path != nil {
+		service, ok = c.Store.Namespaces[ingress.Namespace].Services[path.SvcName]
+	}
+	if ok {
+		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", service.Annotations, ingress.Annotations, c.Store.ConfigMaps.Main.Annotations)
+	} else {
+		annSSLPassthrough, _ = c.Store.GetValueFromAnnotations("ssl-passthrough", ingress.Annotations, c.Store.ConfigMaps.Main.Annotations)
+	}
+	enabled, err := utils.GetBoolValue(annSSLPassthrough.Value, "ssl-passthrough")
+	if err != nil {
+		logger.Errorf("ssl-passthrough annotation: %s", err)
+		return false
+	}
+	if annSSLPassthrough.Status == DELETED {
+		return false
+	}
+	if enabled {
+		c.cfg.SSLPassthrough = true
+		return true
+	}
+	return false
+}

controller/route/route.go
@@ -1,0 +1,83 @@
+// Copyright 2019 HAProxy Technologies LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package route
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
+)
+
+//nolint:golint,stylecheck
+const (
+	// MapFiles
+	SNI         = "sni"
+	HOST        = "host"
+	PATH_EXACT  = "path-exact"
+	PATH_PREFIX = "path-prefix"
+)
+
+type Route struct {
+	Host           string
+	Path           *store.IngressPath
+	HAProxyRules   []haproxy.RuleID
+	BackendName    string
+	SSLPassthrough bool
+}
+
+// AddHostPathRoute adds Host/Path ingress route to haproxy Map files used for backend switching.
+func AddHostPathRoute(route Route, mapFiles haproxy.Maps) error {
+	if route.BackendName == "" {
+		return fmt.Errorf("backendName missing")
+	}
+	// Wildcard host
+	if route.Host != "" && route.Host[0] == '*' {
+		route.Host = route.Host[1:]
+	}
+	value := route.BackendName
+	for _, id := range route.HAProxyRules {
+		value += "." + strconv.Itoa(int(id))
+	}
+	// SSLPassthrough
+	if route.SSLPassthrough {
+		if route.Host == "" {
+			return fmt.Errorf("empty SNI for backend %s,", route.BackendName)
+		}
+		mapFiles.AppendRow(SNI, route.Host+"\t\t\t"+value)
+		return nil
+	}
+	// HTTP
+	if route.Host != "" {
+		mapFiles.AppendRow(HOST, route.Host+"\t\t\t"+route.Host)
+	} else if route.Path.Path == "" {
+		return fmt.Errorf("neither Host nor Path are provided for backend %v,", route.BackendName)
+	}
+	// if PathTypeExact is not set, PathTypePrefix will be applied
+	path := route.Path.Path
+	switch {
+	case route.Path.ExactPathMatch:
+		mapFiles.AppendRow(PATH_EXACT, route.Host+path+"\t\t\t"+value)
+	case path == "" || path == "/":
+		mapFiles.AppendRow(PATH_PREFIX, route.Host+"/"+"\t\t\t"+value)
+	default:
+		path = strings.TrimSuffix(path, "/")
+		mapFiles.AppendRow(PATH_EXACT, route.Host+path+"\t\t\t"+value)
+		mapFiles.AppendRow(PATH_PREFIX, route.Host+path+"/"+"\t\t\t"+value)
+	}
+	return nil
+}

controller/ingress/route.go
@@ -1,174 +1,0 @@
-// Copyright 2019 HAProxy Technologies LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package ingress
-
-import (
-	"fmt"
-	"strconv"
-	"strings"
-
-	"github.com/haproxytech/models/v2"
-
-	"github.com/haproxytech/kubernetes-ingress/controller/annotations"
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
-	"github.com/haproxytech/kubernetes-ingress/controller/store"
-)
-
-type Route struct {
-	Namespace      *store.Namespace
-	Ingress        *store.Ingress
-	Path           *store.IngressPath
-	service        *store.Service
-	endpoints      *store.PortEndpoints
-	status         store.Status
-	HAProxyRules   []haproxy.RuleID
-	Host           string
-	BackendName    string
-	NewBackend     bool
-	LocalBackend   bool
-	SSLPassthrough bool
-	TCPService     bool
-	reload         bool
-}
-
-// addToMapFile adds ingress route to haproxy Map files used for backend switching.
-func (route *Route) addToMapFile(mapFiles haproxy.Maps) error {
-	// Wildcard host
-	if route.Host != "" && route.Host[0] == '*' {
-		route.Host = route.Host[1:]
-	}
-	value := route.BackendName
-	for _, id := range route.HAProxyRules {
-		value += "." + strconv.Itoa(int(id))
-	}
-	// SSLPassthrough
-	if route.SSLPassthrough {
-		if route.Host == "" {
-			return fmt.Errorf("empty SNI for backend %s, SKIP", route.BackendName)
-		}
-		mapFiles.AppendRow(SNI, route.Host+"\t\t\t"+value)
-		return nil
-	}
-	// HTTP
-	if route.Host != "" {
-		mapFiles.AppendRow(HOST, route.Host+"\t\t\t"+route.Host)
-	} else if route.Path.Path == "" {
-		return fmt.Errorf("neither Host nor Path are provided for backend %v, SKIP", route.BackendName)
-	}
-	// if PathTypeExact is not set, PathTypePrefix will be applied
-	path := route.Path.Path
-	switch {
-	case route.Path.ExactPathMatch:
-		mapFiles.AppendRow(PATH_EXACT, route.Host+path+"\t\t\t"+value)
-	case path == "" || path == "/":
-		mapFiles.AppendRow(PATH_PREFIX, route.Host+"/"+"\t\t\t"+value)
-	default:
-		path = strings.TrimSuffix(path, "/")
-		mapFiles.AppendRow(PATH_EXACT, route.Host+path+"\t\t\t"+value)
-		mapFiles.AppendRow(PATH_PREFIX, route.Host+path+"/"+"\t\t\t"+value)
-	}
-	return nil
-}
-
-// handleBackend processes an Ingress Route and makes corresponding backend configuration in HAProxy
-func (route *Route) handleBackend() (err error) {
-	// Get/Create Backend
-	var backend models.Backend
-	if backend, err = client.BackendGet(route.BackendName); err != nil {
-		mode := "http"
-		backend = models.Backend{
-			Name: route.BackendName,
-			Mode: mode,
-		}
-		if err = client.BackendCreate(backend); err != nil {
-			return err
-		}
-		route.NewBackend = true
-		route.reload = true
-		logger.Debugf("Ingress '%s/%s': new backend '%s', reload required", route.Namespace.Name, route.Ingress.Name, route.BackendName)
-	}
-	// Update Backend
-	var switchMode bool
-	if backend.Mode == "http" {
-		if route.SSLPassthrough || route.TCPService {
-			backend.Mode = "tcp"
-			switchMode = true
-		}
-	} else if !route.SSLPassthrough && !route.TCPService {
-		backend.Mode = "http"
-		switchMode = true
-	}
-	backendUpdated := annotations.HandleBackendAnnotations(
-		k8sStore,
-		client,
-		&backend,
-		route.NewBackend,
-		route.service.Annotations,
-		route.Ingress.Annotations,
-		k8sStore.ConfigMaps.Main.Annotations,
-	) || switchMode
-	if backendUpdated {
-		if err = client.BackendEdit(backend); err != nil {
-			return err
-		}
-		route.reload = true
-		logger.Debugf("Ingress '%s/%s': backend '%s' updated, reload required", route.Namespace.Name, route.Ingress.Name, route.BackendName)
-	}
-
-	return nil
-}
-
-// SetBackendName checks if Ingress ServiceName and ServicePort exists and construct corresponding backend name
-// NB: if sp.Name is available it should be used instead of sp.Port to avoid backends duplication
-func (route *Route) SetBackendName() (err error) {
-	route.service = route.Namespace.Services[route.Path.SvcName]
-	if route.service == nil {
-		return fmt.Errorf("ingress %s/%s: service '%s' not found", route.Namespace.Name, route.Ingress.Name, route.Path.SvcName)
-	}
-	var svcPort store.ServicePort
-	found := false
-	for _, sp := range route.service.Ports {
-		if (sp.Port == route.Path.SvcPortInt) ||
-			(sp.Name != "" && sp.Name == route.Path.SvcPortString) {
-			svcPort = sp
-			found = true
-			break
-		}
-	}
-	if !found {
-		if route.Path.SvcPortString != "" {
-			return fmt.Errorf("ingress %s/%s: service %s: no service port matching '%s'", route.Namespace.Name, route.Ingress.Name, route.service.Name, route.Path.SvcPortString)
-		}
-		return fmt.Errorf("ingress %s/%s: service %s: no service port matching '%d'", route.Namespace.Name, route.Ingress.Name, route.service.Name, route.Path.SvcPortInt)
-	}
-	route.Path.SvcPortResolved = &svcPort
-	if svcPort.Name != "" {
-		route.BackendName = fmt.Sprintf("%s-%s-%s", route.service.Namespace, route.service.Name, svcPort.Name)
-	} else {
-		route.BackendName = fmt.Sprintf("%s-%s-%s", route.service.Namespace, route.service.Name, strconv.Itoa(int(svcPort.Port)))
-	}
-	return nil
-}
-
-func (route *Route) setStatus() {
-	if route.Path.Status == DELETED || route.service.Status == DELETED {
-		route.status = DELETED
-		return
-	}
-	route.status = route.Path.Status
-	if route.status == EMPTY {
-		route.status = route.service.Status
-	}
-}

controller/handler-refresh.go
@@ -22,8 +22,7 @@ import (
 type RefreshHandler struct{}
 
 func (h RefreshHandler) Update(k store.K8s, cfg *Configuration, api api.HAProxyClient) (reload bool, err error) {
-	reload, activeBackends := cfg.IngressRoutes.Refresh(api, k, cfg.MapFiles, cfg.Certificates)
-	h.clearBackends(api, cfg, activeBackends)
+	h.clearBackends(api, cfg)
 	reload = cfg.HAProxyRules.Refresh(api) || reload
 	reload = cfg.MapFiles.Refresh(api) || reload
 	reload = cfg.Certificates.Refresh() || reload
@@ -31,21 +30,21 @@ func (h RefreshHandler) Update(k store.K8s, cfg *Configuration, api api.HAProxyC
 }
 
 // Remove unused backends
-func (h RefreshHandler) clearBackends(api api.HAProxyClient, cfg *Configuration, activeBackends map[string]struct{}) {
+func (h RefreshHandler) clearBackends(api api.HAProxyClient, cfg *Configuration) {
 	if cfg.SSLPassthrough {
 		// SSL default backend
-		activeBackends[SSLDefaultBaceknd] = struct{}{}
+		cfg.ActiveBackends[SSLDefaultBaceknd] = struct{}{}
 	}
 	// Ratelimting backends
 	for _, rateLimitTable := range rateLimitTables {
-		activeBackends[rateLimitTable] = struct{}{}
+		cfg.ActiveBackends[rateLimitTable] = struct{}{}
 	}
 	allBackends, err := api.BackendsGet()
 	if err != nil {
 		return
 	}
 	for _, backend := range allBackends {
-		if _, ok := activeBackends[backend.Name]; !ok {
+		if _, ok := cfg.ActiveBackends[backend.Name]; !ok {
 			logger.Debugf("Deleting backend '%s'", backend.Name)
 			if err := api.BackendDelete(backend.Name); err != nil {
 				logger.Panic(err)

commit 446846e6c2367589b6ae46d3c6183588fbe014df

BUG/MEDIUM: reset ActiveBackends when cleaning controller config
Bug introduced in bf9e3534, ActiveBackends list should be reset when
cleaning controller state. This also implied correctly resetting custom
routes.

controller/configuration.go
@@ -45,10 +45,11 @@ func (c *Configuration) Init() {
 // Clean cleans all the statuses of various data that was changed
 // deletes them completely or just resets them if needed
 func (c *Configuration) Clean() {
+	rateLimitTables = []string{}
+	c.ActiveBackends = make(map[string]struct{})
 	c.MapFiles.Clean()
 	c.Certificates.Clean()
 	logger.Panic(c.HAProxyRulesInit())
-	rateLimitTables = []string{}
 }
 
 func (c *Configuration) HAProxyRulesInit() error {

controller/controller.go
@@ -141,6 +141,11 @@ func (c *HAProxyController) updateHAProxy() {
 
 	reload, restart := c.handleGlobalConfig()
 
+	if route.CustomRoutes {
+		logger.Error(route.RoutesReset(c.Client))
+		route.CustomRoutes = false
+	}
+
 	for _, namespace := range c.Store.Namespaces {
 		if !namespace.Relevant {
 			continue
@@ -445,7 +450,6 @@ func (c *HAProxyController) clean(failedSync bool) {
 		c.PublishService.Status = EMPTY
 	}
 	c.cfg.SSLPassthrough = false
-	route.CustomRoutes = false
 	if !failedSync {
 		c.Store.Clean()
 	}

controller/ingress.go
@@ -131,9 +131,9 @@ func (c *HAProxyController) setDefaultService(ingress *store.Ingress, frontends 
 			}
 			ftReload = true
 			logger.Debugf("Setting '%s' default backend to '%s'", frontendName, backendName)
-			c.cfg.ActiveBackends[backendName] = struct{}{}
 		}
 	}
+	c.cfg.ActiveBackends[backendName] = struct{}{}
 	endpointsReload := svc.HandleEndpoints(c.Client, c.Store, c.cfg.Certificates)
 	reload = bdReload || ftReload || endpointsReload
 	return reload, err

controller/route/route.go
@@ -98,12 +98,7 @@ func AddCustomRoute(route Route, routeACLAnn store.StringW, api api.HAProxyClien
 		logger.Debugf("Custom Route to backend '%s' deleted, reload required", route.BackendName)
 		return
 	}
-	if !CustomRoutes {
-		if err = customRouteInit(api); err != nil {
-			return
-		}
-		CustomRoutes = true
-	}
+	CustomRoutes = true
 	var routeCond string
 	if route.Host != "" {
 		routeCond = fmt.Sprintf("{ var(txn.host) %s } ", route.Host)
@@ -135,7 +130,7 @@ func AddCustomRoute(route Route, routeACLAnn store.StringW, api api.HAProxyClien
 	return reload, err
 }
 
-func customRouteInit(api api.HAProxyClient) (err error) {
+func RoutesReset(api api.HAProxyClient) (err error) {
 	for _, frontend := range []string{FrontendHTTP, FrontendHTTPS} {
 		api.BackendSwitchingRuleDeleteAll(frontend)
 		err = api.BackendSwitchingRuleCreate(frontend, models.BackendSwitchingRule{