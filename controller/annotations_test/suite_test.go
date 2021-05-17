4262a20d333771e9130f54a1f4182aa60ed53e43
Zlatko Bratkovic <zbratkovic@haproxy.com>
2021-05-17 13:53:54 +0200

    BUILD: docker: use usr/local/etc/haproxy as base dir
    /etc/haproxy is just a symlink to /usr/local/etc/haproxy,
    copying whole folder deleted the symlink in some builds

--- controller/annotations_test/suite_test.go
+++ controller/annotations_test/suite_test.go
@@ -22,7 +22,7 @@ func (suite *AnnotationSuite) SetupSuite() {
 	if err != nil {
 		panic(err)
 	}
-	suite.client, err = api.Init(suite.transactionDir, "../../fs/etc/haproxy/haproxy.cfg", "haproxy", "")
+	suite.client, err = api.Init(suite.transactionDir, "../../fs/usr/local/etc/haproxy/haproxy.cfg", "haproxy", "")
 	suite.Nil(err)
 	suite.NoError(suite.client.APIStartTransaction())
 }
