4700537a0bfb039ca3acd30a22ac6801cab44017
ivan matmati <imatmati@haproxy.com>
2021-07-12 15:04:10 +0200

    BUG/MINOR: fix balance management

--- controller/annotations/backendLoadBalance.go
+++ controller/annotations/backendLoadBalance.go
@@ -1,7 +1,11 @@
 package annotations
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/haproxytech/client-native/v2/models"
 )
@@ -21,9 +25,11 @@ func (a *BackendLoadBalance) GetName() string {
 }
 
 func (a *BackendLoadBalance) Parse(input string) error {
-	params := &models.Balance{
-		Algorithm: &input,
+	params, err := getParamsFromInput(input)
+	if err != nil {
+		return fmt.Errorf("load-balance: %w", err)
 	}
+
 	if err := params.Validate(nil); err != nil {
 		return fmt.Errorf("load-balance: %w", err)
 	}
@@ -34,4 +40,89 @@ func (a *BackendLoadBalance) Parse(input string) error {
 func (a *BackendLoadBalance) Update() error {
 	a.backend.Balance = a.params
 	return nil
+}
+
+func getParamsFromInput(value string) (*models.Balance, error) {
+	balance := &models.Balance{}
+	tokens := strings.Split(value, " ")
+	if len(tokens) == 0 {
+		return nil, errors.New("missing algorithm name")
+	}
+
+	reg := regexp.MustCompile(`(\\(|\\))"`)
+	algorithmTokens := reg.Split(tokens[0], -1)
+	algorithm := algorithmTokens[0]
+	balance.Algorithm = &algorithm
+	if len(algorithmTokens) == 3 {
+		switch algorithm {
+		case "hdr":
+			balance.HdrName = algorithmTokens[1]
+		case "random":
+			if randomDraws, err := strconv.Atoi(algorithmTokens[1]); err == nil {
+				balance.RandomDraws = int64(randomDraws)
+			}
+		case "rdp-cookie":
+			balance.RdpCookieName = algorithmTokens[1]
+		}
+	}
+	i := 1
+	if algorithm == "url_param" {
+		balance.URLParam = tokens[i]
+		i++
+	}
+
+	for ; i < len(tokens); i++ {
+		token := tokens[i]
+		switch token {
+		case "len":
+			if i+1 >= len(tokens) {
+				logger.Errorf("Missing parameter for option '%s' in balance configuration", token)
+				continue
+			}
+			if length, err := strconv.Atoi(tokens[i+1]); err == nil {
+				balance.URILen = int64(length)
+			}
+			// We already got the next token
+			i++
+		case "depth":
+			if i+1 >= len(tokens) {
+				logger.Errorf("Missing parameter for option '%s' in balance configuration", token)
+				continue
+			}
+			if depth, err := strconv.Atoi(tokens[i+1]); err == nil {
+				balance.URIDepth = int64(depth)
+			}
+			// We already got the next token
+			i++
+		case "whole":
+			balance.URIWhole = true
+		case "max_wait":
+			if i+1 >= len(tokens) {
+				logger.Errorf("Missing parameter for option '%s' in balance configuration", token)
+				continue
+			}
+			if maxWait, err := strconv.Atoi(tokens[i+1]); err == nil {
+				balance.URLParamMaxWait = int64(maxWait)
+			}
+			// We already got the next token
+			i++
+		case "path-only":
+			balance.URIPathOnly = true
+		case "check_post":
+			if i+1 >= len(tokens) {
+				logger.Errorf("Missing parameter for option '%s' in balance configuration", token)
+				continue
+			}
+			if checkPost, err := strconv.Atoi(tokens[i+1]); err == nil {
+				balance.URLParamCheckPost = int64(checkPost)
+			}
+			// We already got the next token
+			i++
+		case "use_domain_only":
+			balance.HdrUseDomainOnly = true
+		default:
+			logger.Warningf("balance configuration '%s' is ignored", token)
+		}
+	}
+	return balance, nil
 }
--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.16
 require (
 	github.com/go-test/deep v1.0.7
 	github.com/google/renameio v0.1.1-0.20200217212219-353f81969824
-	github.com/haproxytech/client-native/v2 v2.5.1-0.20210716092634-921eb1292e53
-	github.com/haproxytech/config-parser/v4 v4.0.0-rc1.0.20210706120926-340f1b3664db
+	github.com/haproxytech/client-native/v2 v2.5.1-0.20210722153259-a0b616644239
+	github.com/haproxytech/config-parser/v4 v4.0.0-rc1.0.20210719074749-d9f3915990e4
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/stretchr/testify v1.6.1
 	k8s.io/api v0.21.3
