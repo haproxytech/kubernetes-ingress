haproxytech/kubernetes-ingress

commit cf377f96e8602309714d6cc722759be984fb192b

MAJOR: api: use client-native instead of config-parser for global config/annotations
global annotations dealing with haproxy configuration in "global" and
"defaults" section used to be implemented via config-parser module.
client-native is a higher level module and will be used instead because:
- it offloads configuration logic from Ingress Controller.
- it is already used in the rest of IC codebase.
- client-native data models makes it easier to adopt CRDs in Ingress
Controller.

controller/annotations/defaultLogFormat.go
@@ -4,20 +4,19 @@ import (
 	"errors"
 	"strings"
 
-	"github.com/haproxytech/config-parser/v3/types"
+	"github.com/haproxytech/client-native/v2/models"
 
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type DefaultLogFormat struct {
-	name   string
-	data   *types.StringC
-	client api.HAProxyClient
+	name     string
+	defaults *models.Defaults
+	data     string
 }
 
-func NewDefaultLogFormat(n string, c api.HAProxyClient) *DefaultLogFormat {
-	return &DefaultLogFormat{name: n, client: c}
+func NewDefaultLogFormat(n string, d *models.Defaults) *DefaultLogFormat {
+	return &DefaultLogFormat{name: n, defaults: d}
 }
 
 func (a *DefaultLogFormat) GetName() string {
@@ -31,19 +30,19 @@ func (a *DefaultLogFormat) Parse(input store.StringW, forceParse bool) error {
 	if input.Status == store.DELETED {
 		return nil
 	}
-	v := strings.TrimSpace(input.Value)
-	if v == "" {
+	a.data = strings.TrimSpace(input.Value)
+	if a.data == "" {
 		return errors.New("unable to parse log-format: empty input")
 	}
-	a.data = &types.StringC{Value: "'" + v + "'"}
 	return nil
 }
 
 func (a *DefaultLogFormat) Update() error {
-	if a.data == nil {
+	if a.data == "" {
 		logger.Infof("Removing default log-format")
-		return a.client.DefaultLogFormat(nil)
+	} else {
+		logger.Infof("Setting default log-format to: %s", a.data)
 	}
-	logger.Infof("Setting default log-format to: %s", a.data.Value)
-	return a.client.DefaultLogFormat(a.data)
+	a.defaults.LogFormat = a.data
+	return nil
 }

controller/annotations/defaultOption.go
@@ -1,23 +1,24 @@
 package annotations
 
 import (
-	"github.com/haproxytech/config-parser/v3/types"
+	"errors"
 
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/client-native/v2/models"
+
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 type DefaultOption struct {
-	name   string
-	data   *types.SimpleOption
-	client api.HAProxyClient
+	name     string
+	defaults *models.Defaults
+	enabled  *bool
 }
 
-func NewDefaultOption(n string, c api.HAProxyClient) *DefaultOption {
+func NewDefaultOption(n string, d *models.Defaults) *DefaultOption {
 	return &DefaultOption{
-		name:   n,
-		client: c,
+		name:     n,
+		defaults: d,
 	}
 }
 
@@ -36,19 +37,50 @@ func (a *DefaultOption) Parse(input store.StringW, forceParse bool) error {
 	if err != nil {
 		return err
 	}
-	a.data = &types.SimpleOption{NoOption: !enabled}
+	a.enabled = &enabled
 	return nil
 }
 
 func (a *DefaultOption) Update() error {
-	if a.data == nil {
+	if a.enabled == nil {
 		logger.Infof("Removing option %s", a.name)
-		return a.client.DefaultOption(a.name, nil)
+		switch a.name {
+		case "http-server-close", "http-keep-alive":
+			a.defaults.HTTPConnectionMode = ""
+		case "dontlognull":
+			a.defaults.Dontlognull = ""
+		case "logasap":
+			a.defaults.Logasap = ""
+		default:
+			return errors.New("unknown param")
+		}
 	}
-	if a.data.NoOption {
-		logger.Infof("disabling option %s", a.name)
-	} else {
+	if *a.enabled {
 		logger.Infof("enabling option %s", a.name)
+		switch a.name {
+		case "http-server-close":
+			a.defaults.HTTPConnectionMode = "http-server-close"
+		case "http-keep-alive":
+			a.defaults.HTTPConnectionMode = "http-keep-alive"
+		case "dontlognull":
+			a.defaults.Dontlognull = "enabled"
+		case "logasap":
+			a.defaults.Logasap = "enabled"
+		default:
+			return errors.New("unknown param")
+		}
+	} else {
+		logger.Infof("disabling option %s", a.name)
+		switch a.name {
+		case "http-server-close", "http-keep-alive":
+			a.defaults.HTTPConnectionMode = ""
+		case "dontlognull":
+			a.defaults.Dontlognull = "disabled"
+		case "logasap":
+			a.defaults.Logasap = "disabled"
+		default:
+			return errors.New("unknown param")
+		}
 	}
-	return a.client.DefaultOption(a.name, a.data)
+	return nil
 }

controller/annotations/defaultTimeout.go
@@ -1,23 +1,23 @@
 package annotations
 
 import (
+	"errors"
 	"strings"
-	"time"
 
-	"github.com/haproxytech/config-parser/v3/types"
+	"github.com/haproxytech/client-native/v2/models"
 
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 type DefaultTimeout struct {
-	name   string
-	data   *types.SimpleTimeout
-	client api.HAProxyClient
+	name     string
+	defaults *models.Defaults
+	timeout  *int64
 }
 
-func NewDefaultTimeout(n string, c api.HAProxyClient) *DefaultTimeout {
-	return &DefaultTimeout{name: n, client: c}
+func NewDefaultTimeout(n string, d *models.Defaults) *DefaultTimeout {
+	return &DefaultTimeout{name: n, defaults: d}
 }
 
 func (a *DefaultTimeout) GetName() string {
@@ -31,27 +31,39 @@ func (a *DefaultTimeout) Parse(input store.StringW, forceParse bool) error {
 	if input.Status == store.DELETED {
 		return nil
 	}
-	timeout, err := time.ParseDuration(input.Value)
-	if err != nil {
-		return err
-	}
-	s := timeout.String()
-	if strings.HasSuffix(s, "m0s") {
-		s = s[:len(s)-2]
-	}
-	if strings.HasSuffix(s, "h0m") {
-		s = s[:len(s)-2]
-	}
-	a.data = &types.SimpleTimeout{Value: s}
-	return nil
+	var err error
+	a.timeout, err = utils.ParseTime(input.Value)
+	return err
 }
 
 func (a *DefaultTimeout) Update() error {
 	timeout := strings.TrimPrefix(a.name, "timeout-")
-	if a.data == nil {
-		logger.Infof("Removing default timeout-%s ", timeout)
-		return a.client.DefaultTimeout(timeout, nil)
+	if a.timeout == nil {
+		logger.Infof("Removing default timeout-%s", timeout)
+	} else {
+		logger.Infof("Setting default timeout-%s to %ds", timeout, *a.timeout)
 	}
-	logger.Infof("Setting default timeout-%s to %s", timeout, a.data.Value)
-	return a.client.DefaultTimeout(timeout, a.data)
+	switch a.name {
+	case "timeout-client":
+		a.defaults.ClientTimeout = a.timeout
+	case "timeout-client-fin":
+		a.defaults.ClientFinTimeout = a.timeout
+	case "timeout-connect":
+		a.defaults.ConnectTimeout = a.timeout
+	case "timeout-http-keep-alive":
+		a.defaults.HTTPKeepAliveTimeout = a.timeout
+	case "timeout-http-request":
+		a.defaults.HTTPRequestTimeout = a.timeout
+	case "timeout-queue":
+		a.defaults.QueueTimeout = a.timeout
+	case "timeout-server":
+		a.defaults.ServerTimeout = a.timeout
+	case "timeout-server-fin":
+		a.defaults.ServerFinTimeout = a.timeout
+	case "timeout-tunnel":
+		a.defaults.TunnelTimeout = a.timeout
+	default:
+		return errors.New("unknown param")
+	}
+	return nil
 }

controller/annotations/global.go
@@ -1,12 +1,14 @@
 package annotations
 
 import (
+	"github.com/haproxytech/client-native/v2/models"
+
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
-func HandleGlobalAnnotations(k8sStore store.K8s, client api.HAProxyClient, forcePase bool, annotations store.MapStringW) (restart bool, reload bool) {
-	annList := GetGlobalAnnotations(client)
+func HandleGlobalAnnotations(k8sStore store.K8s, client api.HAProxyClient, global *models.Global, defaults *models.Defaults, forcePase bool, annotations store.MapStringW) (restart bool, reload bool) {
+	annList := GetGlobalAnnotations(client, global, defaults)
 	for _, a := range annList {
 		annValue, _ := k8sStore.GetValueFromAnnotations(a.GetName(), annotations)
 		if annValue == nil {
@@ -21,28 +23,28 @@ func HandleGlobalAnnotations(k8sStore store.K8s, client api.HAProxyClient, force
 	return restart, reload
 }
 
-func GetGlobalAnnotations(client api.HAProxyClient) []Annotation {
+func GetGlobalAnnotations(client api.HAProxyClient, global *models.Global, defaults *models.Defaults) []Annotation {
 	return []Annotation{
 		NewFrontendCfgSnippet("frontend-config-snippet", client, []string{"http", "https"}),
 		NewFrontendCfgSnippet("stats-config-snippet", client, []string{"stats"}),
 		NewGlobalCfgSnippet("global-config-snippet", client),
-		NewGlobalSyslogServers("syslog-server", client),
-		NewGlobalNbthread("nbthread", client),
-		NewGlobalMaxconn("maxconn", client),
-		NewGlobalHardStopAfter("hard-stop-after", client),
-		NewDefaultOption("http-server-close", client),
-		NewDefaultOption("http-keep-alive", client),
-		NewDefaultOption("dontlognull", client),
-		NewDefaultOption("logasap", client),
-		NewDefaultTimeout("timeout-http-request", client),
-		NewDefaultTimeout("timeout-connect", client),
-		NewDefaultTimeout("timeout-client", client),
-		NewDefaultTimeout("timeout-client-fin", client),
-		NewDefaultTimeout("timeout-queue", client),
-		NewDefaultTimeout("timeout-server", client),
-		NewDefaultTimeout("timeout-server-fin", client),
-		NewDefaultTimeout("timeout-tunnel", client),
-		NewDefaultTimeout("timeout-http-keep-alive", client),
-		NewDefaultLogFormat("log-format", client),
+		NewGlobalSyslogServers("syslog-server", client, global),
+		NewGlobalNbthread("nbthread", global),
+		NewGlobalMaxconn("maxconn", global),
+		NewGlobalHardStopAfter("hard-stop-after", global),
+		NewDefaultOption("http-server-close", defaults),
+		NewDefaultOption("http-keep-alive", defaults),
+		NewDefaultOption("dontlognull", defaults),
+		NewDefaultOption("logasap", defaults),
+		NewDefaultTimeout("timeout-http-request", defaults),
+		NewDefaultTimeout("timeout-connect", defaults),
+		NewDefaultTimeout("timeout-client", defaults),
+		NewDefaultTimeout("timeout-client-fin", defaults),
+		NewDefaultTimeout("timeout-queue", defaults),
+		NewDefaultTimeout("timeout-server", defaults),
+		NewDefaultTimeout("timeout-server-fin", defaults),
+		NewDefaultTimeout("timeout-tunnel", defaults),
+		NewDefaultTimeout("timeout-http-keep-alive", defaults),
+		NewDefaultLogFormat("log-format", defaults),
 	}
 }

controller/annotations/globalHardStopAfter.go
@@ -1,23 +1,20 @@
 package annotations
 
 import (
-	"strings"
-	"time"
+	"github.com/haproxytech/client-native/v2/models"
 
-	"github.com/haproxytech/config-parser/v3/types"
-
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 type GlobalHardStopAfter struct {
 	name   string
-	data   *types.StringC
-	client api.HAProxyClient
+	data   *int64
+	global *models.Global
 }
 
-func NewGlobalHardStopAfter(n string, c api.HAProxyClient) *GlobalHardStopAfter {
-	return &GlobalHardStopAfter{name: n, client: c}
+func NewGlobalHardStopAfter(n string, g *models.Global) *GlobalHardStopAfter {
+	return &GlobalHardStopAfter{name: n, global: g}
 }
 
 func (a *GlobalHardStopAfter) GetName() string {
@@ -25,41 +22,26 @@ func (a *GlobalHardStopAfter) GetName() string {
 }
 
 func (a *GlobalHardStopAfter) Parse(input store.StringW, forceParse bool) error {
+	var err error
 	if input.Status == store.EMPTY && !forceParse {
 		return ErrEmptyStatus
 	}
 	if input.Status == store.DELETED {
 		return nil
 	}
-	after, err := time.ParseDuration(input.Value)
+	a.data, err = utils.ParseTime(input.Value)
 	if err != nil {
 		return err
 	}
-	duration := after.String()
-	if strings.HasSuffix(duration, "m0s") {
-		duration = duration[:len(duration)-2]
-	}
-	if strings.HasSuffix(duration, "h0m") {
-		duration = duration[:len(duration)-2]
-	}
-
-	if err != nil {
-		return err
-	}
-
-	a.data = &types.StringC{Value: duration}
 	return nil
-}
-
-func (a *GlobalHardStopAfter) Delete() error {
-	return a.client.GlobalHardStopAfter(nil)
 }
 
 func (a *GlobalHardStopAfter) Update() error {
 	if a.data == nil {
 		logger.Infof("Removing hard-stop-after timeout")
-		return a.client.GlobalHardStopAfter(nil)
+	} else {
+		logger.Infof("Setting hard-stop-after to %ds", *a.data)
 	}
-	logger.Infof("Setting hard-stop-after to %s", a.data.Value)
-	return a.client.GlobalHardStopAfter(a.data)
+	a.global.HardStopAfter = a.data
+	return nil
 }

controller/annotations/globalMaxconn.go
@@ -3,20 +3,19 @@ package annotations
 import (
 	"strconv"
 
-	"github.com/haproxytech/config-parser/v3/types"
+	"github.com/haproxytech/client-native/v2/models"
 
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type GlobalMaxconn struct {
 	name   string
-	data   *types.Int64C
-	client api.HAProxyClient
+	data   int64
+	global *models.Global
 }
 
-func NewGlobalMaxconn(n string, c api.HAProxyClient) *GlobalMaxconn {
-	return &GlobalMaxconn{name: n, client: c}
+func NewGlobalMaxconn(n string, g *models.Global) *GlobalMaxconn {
+	return &GlobalMaxconn{name: n, global: g}
 }
 
 func (a *GlobalMaxconn) GetName() string {
@@ -34,15 +33,16 @@ func (a *GlobalMaxconn) Parse(input store.StringW, forceParse bool) error {
 	if err != nil {
 		return err
 	}
-	a.data = &types.Int64C{Value: int64(v)}
+	a.data = int64(v)
 	return nil
 }
 
 func (a *GlobalMaxconn) Update() error {
-	if a.data == nil {
-		logger.Infof("Removing default maxconn")
-		return a.client.GlobalMaxconn(nil)
+	if a.data == 0 {
+		logger.Infof("Removing global maxconn")
+	} else {
+		logger.Infof("Setting global maxconn to %d", a.data)
 	}
-	logger.Infof("Setting default maxconn to %d", a.data.Value)
-	return a.client.GlobalMaxconn(a.data)
+	a.global.Maxconn = a.data
+	return nil
 }

controller/annotations/globalNbthread.go
@@ -4,20 +4,19 @@ import (
 	"runtime"
 	"strconv"
 
-	"github.com/haproxytech/config-parser/v3/types"
+	"github.com/haproxytech/client-native/v2/models"
 
-	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type GlobalNbthread struct {
 	name   string
-	data   *types.Int64C
-	client api.HAProxyClient
+	data   int64
+	global *models.Global
 }
 
-func NewGlobalNbthread(n string, c api.HAProxyClient) *GlobalNbthread {
-	return &GlobalNbthread{name: n, client: c}
+func NewGlobalNbthread(n string, g *models.Global) *GlobalNbthread {
+	return &GlobalNbthread{name: n, global: g}
 }
 
 func (a *GlobalNbthread) GetName() string {
@@ -39,15 +38,16 @@ func (a *GlobalNbthread) Parse(input store.StringW, forceParse bool) error {
 	if v > maxProcs {
 		v = maxProcs
 	}
-	a.data = &types.Int64C{Value: int64(v)}
+	a.data = int64(v)
 	return nil
 }
 
 func (a *GlobalNbthread) Update() error {
-	if a.data == nil {
+	if a.data == 0 {
 		logger.Infof("Removing nbThread option")
-		return a.client.Nbthread(nil)
+	} else {
+		logger.Infof("Setting nbThread to %d", a.data)
 	}
-	logger.Infof("Setting nbThread to: %d", a.data.Value)
-	return a.client.Nbthread(a.data)
+	a.global.Nbthread = a.data
+	return nil
 }

controller/annotations/globalSyslogServer.go
@@ -5,22 +5,24 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/haproxytech/config-parser/v3/types"
+	"github.com/haproxytech/client-native/v2/models"
 
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 type GlobalSyslogServers struct {
-	name    string
-	data    []*types.Log
-	client  api.HAProxyClient
-	stdout  bool
-	restart bool
+	name       string
+	global     *models.Global
+	logTargets models.LogTargets
+	client     api.HAProxyClient
+	stdout     bool
+	restart    bool
 }
 
-func NewGlobalSyslogServers(n string, c api.HAProxyClient) *GlobalSyslogServers {
-	return &GlobalSyslogServers{name: n, client: c}
+func NewGlobalSyslogServers(n string, c api.HAProxyClient, g *models.Global) *GlobalSyslogServers {
+	return &GlobalSyslogServers{name: n, client: c, global: g}
 }
 
 func (a *GlobalSyslogServers) GetName() string {
@@ -40,7 +42,6 @@ func (a *GlobalSyslogServers) Parse(input store.StringW, forceParse bool) error 
 	if input.Status == store.DELETED {
 		return nil
 	}
-	a.data = nil
 	a.stdout = false
 	for _, syslogLine := range strings.Split(input.Value, "\n") {
 		if syslogLine == "" {
@@ -64,12 +65,12 @@ func (a *GlobalSyslogServers) Parse(input store.StringW, forceParse bool) error 
 			}
 		}
 		// populate annotation data
-		logData := new(types.Log)
+		logTarget := models.LogTarget{Index: utils.PtrInt64(0)}
 		if address, ok := logParams["address"]; !ok {
 			logger.Errorf("incorrect syslog Line: no address param in '%s'", syslogLine)
 			continue
 		} else {
-			logData.Address = address
+			logTarget.Address = address
 		}
 		for k, v := range logParams {
 			switch strings.ToLower(k) {
@@ -79,65 +80,64 @@ func (a *GlobalSyslogServers) Parse(input store.StringW, forceParse bool) error 
 				}
 			case "port":
 				if logParams["address"] != "stdout" {
-					logData.Address += ":" + v
+					logTarget.Address += ":" + v
 				}
 			case "length":
 				if length, errConv := strconv.Atoi(v); errConv == nil {
-					logData.Length = int64(length)
+					logTarget.Length = int64(length)
 				}
 			case "format":
-				logData.Format = v
+				logTarget.Format = v
 			case "facility":
-				logData.Facility = v
+				logTarget.Facility = v
 			case "level":
-				logData.Level = v
+				logTarget.Level = v
 			case "minlevel":
-				logData.Level = v
+				logTarget.Minlevel = v
 			default:
 				logger.Errorf("unknown syslog param: '%s' in '%s' ", k, syslogLine)
 				continue
 			}
 		}
-		a.data = append(a.data, logData)
+		a.logTargets = append(a.logTargets, &logTarget)
 	}
-	if len(a.data) == 0 {
+	if len(a.logTargets) == 0 {
 		return errors.New("could not parse syslog-server annotation")
 	}
 	return nil
 }
 
 func (a *GlobalSyslogServers) Update() error {
-	err := a.client.LogTarget(nil, -1)
-	if err != nil {
-		return err
-	}
-	if len(a.data) == 0 {
+	a.client.GlobalDeleteLogTargets()
+	if len(a.logTargets) == 0 {
 		logger.Infof("log targets removed")
+		return nil
 	}
-	for i, syslog := range a.data {
-		logger.Infof("adding syslog server: 'address: %s, facility: %s'", syslog.Address, syslog.Facility)
-		if err = a.client.LogTarget(syslog, i); err != nil {
+	var err error
+	for _, logTarget := range a.logTargets {
+		logger.Infof("adding syslog server: 'address: %s, facility: %s'", logTarget.Address, logTarget.Facility)
+		err = a.client.GlobalCreateLogTarget(logTarget)
+		if err != nil {
 			return err
 		}
 	}
 	// stdout logging won't work with daemon mode
-	daemonMode, err := a.client.GlobalConfigEnabled("global", "daemon")
+	var daemonMode bool
+	if a.global.Daemon == "enabled" {
+		daemonMode = true
+	}
 	if err != nil {
 		return err
 	}
 	if a.stdout {
 		if daemonMode {
 			logger.Info("Disabling Daemon mode")
-			if err = a.client.DaemonMode(nil); err != nil {
-				return err
-			}
+			a.global.Daemon = "disabled"
 			a.restart = true
 		}
 	} else if !daemonMode {
 		logger.Info("Enabling Daemon mode")
-		if err = a.client.DaemonMode(&types.Enabled{}); err != nil {
-			return err
-		}
+		a.global.Daemon = "enabled"
 		a.restart = true
 	}
 	return nil

commit cbf5aeac1b6e928f2d8e61f3d66a46b15210aa7e

BUG/MINOR: logFormat: put value between quotes to escape spaces
Regression introduced in cf377f96e8

controller/annotations/defaultLogFormat.go
@@ -23,6 +23,7 @@ func (a *DefaultLogFormat) GetName() string {
 
 func (a *DefaultLogFormat) Parse(input string) error {
 	a.data = strings.TrimSpace(input)
+	a.data = "'" + strings.TrimSpace(input) + "'"
 	if a.data == "" {
 		return errors.New("unable to parse log-format: empty input")
 	}