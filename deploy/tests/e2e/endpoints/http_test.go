b9ba191f45511bb45c993083a9e87c629692e3d4
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2021-08-27 17:20:58 +0200

    TEST/MINOR: check for unnecessary reload
    This commit updates endpoints e2e test to check for unnecessary HAProxy
    reloads by monitoring HAProxy PID.
    In such case, reloads are only required when scaling up, otherwise they
    should be avoided.

--- deploy/tests/e2e/endpoints/http_test.go
+++ deploy/tests/e2e/endpoints/http_test.go
@@ -27,6 +27,7 @@ import (
 // For each test we send two times mores requests than replicas available.
 // In the end the counter should be 2 for each returned pod-name in request answer.
 func (suite *EndpointsSuite) Test_HTTP_Reach() {
+	pid := suite.getPID()
 	for _, replicas := range []int{4, 8, 2, 0, 3} {
 		suite.Run(fmt.Sprintf("%d-replicas", replicas), func() {
 			suite.tmplData.Replicas = replicas
@@ -48,8 +49,20 @@ func (suite *EndpointsSuite) Test_HTTP_Reach() {
 						}
 					}()
 				}
-				if replicas == 0 && len(counter) > 0 {
-					return false
+				switch replicas {
+				case 8:
+					// HAProxy reloaded due to scale up
+					pid = suite.getPID()
+				case 0:
+					if len(counter) > 0 {
+						return false
+					}
+					fallthrough
+				default:
+					if pid != suite.getPID() {
+						suite.Error(fmt.Errorf("Uncessary reload of HAproxy"))
+						return false
+					}
 				}
 				for _, v := range counter {
 					if v != 2 {
@@ -60,4 +73,14 @@ func (suite *EndpointsSuite) Test_HTTP_Reach() {
 			}, e2e.WaitDuration, e2e.TickDuration)
 		})
 	}
+}
+
+func (suite *EndpointsSuite) getPID() (pid string) {
+	r, err := e2e.GetGlobalHAProxyInfo()
+	if err != nil {
+		suite.T().Log(err)
+		return
+	}
+	pid = r.Pid
+	return
 }
