6f75288dce213c2873cafa435cbfbaa10d861ba1
Ivan Matmati <imatmati@haproxy.com>
2021-11-24 11:27:32 +0100

    MINOR: sync k8s resources names with Helm project

--- deploy/tests/create.sh
+++ deploy/tests/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-
+clustername=${1:-dev}
 command -v kind >/dev/null 2>&1 || { echo >&2 "Kind not installed.  Aborting."; exit 1; }
 command -v kubectl >/dev/null 2>&1 || { echo >&2 "Kubectl not installed.  Aborting."; exit 1; }
 DIR=$(dirname "$0")
@@ -12,23 +12,23 @@ if [ -n "${CI_ENV}" ]; then
   docker build --build-arg TARGETPLATFORM="linux/amd64" -t haproxytech/kubernetes-ingress -f build/Dockerfile .
 
   echo "loading image of ingress controller in kind"
-  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=dev
+  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=$clustername
 elif [ -n "${K8S_IC_STABLE}" ]; then
-  kind delete cluster --name dev
-  kind create cluster --name dev --config $DIR/kind-config.yaml
+  kind delete cluster --name $clustername
+  kind create cluster --name $clustername --config $DIR/kind-config.yaml
 
   echo "using last stable image for ingress controller"
   docker pull haproxytech/kubernetes-ingress:latest
-  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=dev
+  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=$clustername
 else
-  kind delete cluster --name dev
-  kind create cluster --name dev --config $DIR/kind-config.yaml
+  kind delete cluster --name $clustername
+  kind create cluster --name $clustername --config $DIR/kind-config.yaml
 
   echo "building image for ingress controller"
   docker build --build-arg TARGETPLATFORM="linux/amd64" -t haproxytech/kubernetes-ingress -f build/Dockerfile .
 
   echo "loading image of ingress controller in kind"
-  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=dev
+  kind load docker-image haproxytech/kubernetes-ingress:latest  --name=$clustername
 fi
 
 echo "deploying Ingress Controller ..."
@@ -42,8 +42,8 @@ echo "wait --for=condition=ready ..."
 COUNTER=0
 while [  $COUNTER -lt 150 ]; do
     sleep 2
-    kubectl get pods -n haproxy-controller | grep haproxy-ingress | awk '{print "haproxy-controller/haproxy-ingress " $3 " " $5}'
-    result=$(kubectl get pods -n haproxy-controller | grep haproxy-ingress | awk '{print $3}')
+    kubectl get pods -n default --no-headers --selector=run=haproxy-ingress | awk '{print "default/haproxy-kubernetes-ingress " $3 " " $5}'
+    result=$(kubectl get pods -n default  --no-headers --selector=run=haproxy-ingress | awk '{print $3}')
     if [ "$result" = "Running" ]; then
       COUNTER=151
     else
@@ -51,4 +51,4 @@ while [  $COUNTER -lt 150 ]; do
     fi
 done
 
-kubectl wait --for=condition=ready --timeout=10s pod -l run=haproxy-ingress -n haproxy-controller
+kubectl wait --for=condition=ready --timeout=10s pod -l run=haproxy-ingress -n default
