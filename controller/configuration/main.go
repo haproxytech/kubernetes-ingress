haproxytech/kubernetes-ingress

commit 708775fb3edefcf13ed1de7102cd6b6aa4bdec86

OPTIM/MEDIUM: Rewrite ErrorFiles and PatternFiles handlers
Rewrite handlers in order to ignore "Status" in ErrorFiles and
PatternFiles handlers.
Use Client-Native models instead of Config-Parser library when setting
ErrorFiles in HAProxy

controller/handler/errorfile.go
@@ -15,77 +15,97 @@
 package handler
 
 import (
-	"os"
+	"fmt"
 	"path/filepath"
+	"strconv"
 
-	"github.com/google/renameio"
-	"github.com/haproxytech/config-parser/v4/types"
+	"github.com/haproxytech/client-native/v2/models"
+
 	config "github.com/haproxytech/kubernetes-ingress/controller/configuration"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type ErrorFile struct {
-	httpErrorCodes []string
-	modified       bool
+	files     files
+	updateAPI bool
 }
 
-func (e ErrorFile) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
+func (h ErrorFile) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
+	h.files.dir = cfg.Env.ErrFileDir
 	if k.ConfigMaps.Errorfiles == nil {
 		return false, nil
 	}
 
-	codes := [15]string{"200", "400", "401", "403", "404", "405", "407", "408", "410", "425", "429", "500", "502", "503", "504"}
-
-	for code, value := range k.ConfigMaps.Errorfiles.Annotations {
-		filePath := filepath.Join(cfg.Env.ErrFileDir, code)
-		switch value.Status {
-		case store.EMPTY:
-			e.httpErrorCodes = append(e.httpErrorCodes, code)
+	for code, v := range k.ConfigMaps.Errorfiles.Annotations {
+		_, ok := h.files.data[code]
+		if ok {
+			err = h.files.updateFile(code, v.Value)
+			if err != nil {
+				logger.Errorf("failed updating errorfile for code '%s': %s", code, err)
+			}
 			continue
-		case store.DELETED:
-			logger.Debugf("deleting errorfile associated to '%s' error code ", code)
-			if err = os.Remove(filePath); err != nil {
-				logger.Errorf("failed deleting '%s': %s", filePath, err)
-			}
-			e.modified = true
-		case store.ADDED, store.MODIFIED:
-			var c string
-			for _, c = range codes {
-				if code == c {
-					break
-				}
-			}
-			if c != code {
-				logger.Errorf("HTTP error code '%s' not supported", code)
-				continue
-			}
-			e.httpErrorCodes = append(e.httpErrorCodes, code)
-			logger.Debugf("Setting errorfile associated to '%s' error code", code)
-			if err = renameio.WriteFile(filePath, []byte(value.Value), os.ModePerm); err != nil {
-				logger.Errorf("failed writing errorfile '%s': %s", filePath, err)
-				continue
-			}
-			e.modified = true
 		}
+		err = checkCode(code)
+		if err != nil {
+			logger.Errorf("failed creating errorfile for code '%s': %s", code, err)
+		}
+		err = h.files.newFile(code, v.Value)
+		if err != nil {
+			logger.Errorf("failed creating errorfile for code '%s': %s", code, err)
+		}
+		h.updateAPI = true
 	}
-	if e.modified {
-		return e.updateAPI(api, cfg.Env.ErrFileDir), nil
+
+	var apiInput = []*models.Errorfile{}
+	for code, f := range h.files.data {
+		if !f.inUse {
+			h.updateAPI = true
+			err = h.files.deleteFile(code)
+			if err != nil {
+				logger.Errorf("failed deleting errorfile for code '%s': %s", code, err)
+			}
+			continue
+		}
+		if f.updated {
+			logger.Debugf("updating errorfile for code '%s': reload required", code)
+			reload = true
+		}
+		c, _ := strconv.Atoi(code) // code already checked in newCode
+		apiInput = append(apiInput, &models.Errorfile{
+			Code: int64(c),
+			File: filepath.Join(h.files.dir, code),
+		})
+		f.inUse = false
+		f.updated = false
 	}
-	return false, nil
+	// HAProxy config update
+	if h.updateAPI {
+		defaults, err := api.DefaultsGetConfiguration()
+		if err != nil {
+			logger.Error(err)
+			return reload, err
+		}
+		defaults.ErrorFiles = apiInput
+		if err = api.DefaultsPushConfiguration(defaults); err != nil {
+			logger.Error(err)
+			return reload, err
+		}
+		h.updateAPI = false
+	}
+	return reload, nil
 }
 
-func (e ErrorFile) updateAPI(api api.HAProxyClient, errFileDir string) (reload bool) {
-	logger.Error(api.DefaultsErrorFile(nil, -1))
-	for index, code := range e.httpErrorCodes {
-		err := api.DefaultsErrorFile(&types.ErrorFile{Code: code, File: filepath.Join(errFileDir, code)}, index)
-
-		if err == nil {
-			reload = true
-			logger.Debug("Errorfile updated, reload required")
-		} else {
-			logger.Error(err)
+func checkCode(code string) error {
+	var codes = [15]string{"200", "400", "401", "403", "404", "405", "407", "408", "410", "425", "429", "500", "502", "503", "504"}
+	var c string
+	for _, c = range codes {
+		if code == c {
+			break
 		}
 	}
-	return reload
+	if c != code {
+		return fmt.Errorf("HTTP error code '%s' not supported", code)
+	}
+	return nil
 }

controller/handler/pattern-files.go
@@ -18,6 +18,8 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/google/renameio"
+
 	config "github.com/haproxytech/kubernetes-ingress/controller/configuration"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
@@ -25,35 +27,84 @@ import (
 )
 
 type PatternFiles struct {
+	files files
 }
 
-func (t PatternFiles) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
-	var f *os.File
-	var errors utils.Errors
+type files struct {
+	dir  string
+	data map[string]file
+}
+type file struct {
+	hash    string
+	inUse   bool
+	updated bool
+}
+
+func (h PatternFiles) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
 	if k.ConfigMaps.PatternFiles == nil {
 		return false, nil
 	}
-	for name, content := range k.ConfigMaps.PatternFiles.Annotations {
-		filename := filepath.Join(cfg.Env.PatternDir, name)
-		switch content.Status {
-		case store.EMPTY:
-			continue
-		case store.DELETED:
-			logger.Error(os.Remove(filename))
-		default:
-			f, err = os.Create(filename)
+	for name, v := range k.ConfigMaps.PatternFiles.Annotations {
+		_, ok := h.files.data[name]
+		if ok {
+			err = h.files.updateFile(name, v.Value)
 			if err != nil {
-				errors.Add(err)
-				continue
+				logger.Errorf("failed updating patternFile '%s': %s", name, err)
 			}
-			defer f.Close()
-			_, err = f.WriteString(content.Value)
+		} else {
+			err = h.files.newFile(name, v.Value)
 			if err != nil {
-				errors.Add(err)
-				continue
+				logger.Errorf("failed creating patternFile '%s': %s", name, err)
 			}
-			reload = true
 		}
 	}
-	return reload, errors.Result()
+
+	for name, f := range h.files.data {
+		if !f.inUse {
+			err = h.files.deleteFile(name)
+			if err != nil {
+				logger.Errorf("failed deleting PatternFile '%s': %s", name, err)
+			}
+			continue
+		}
+		if f.updated {
+			logger.Debugf("updating PatternFile '%s': reload required", name)
+			reload = true
+		}
+		f.inUse = false
+		f.updated = false
+	}
+	return reload, nil
+}
+
+func (f files) deleteFile(code string) error {
+	delete(f.data, code)
+	err := os.Remove(filepath.Join(f.dir, code))
+	return err
+}
+func (f files) newFile(code, value string) error {
+	if err := renameio.WriteFile(filepath.Join(f.dir, code), []byte(value), os.ModePerm); err != nil {
+		return err
+	}
+	f.data[code] = file{
+		hash:    utils.Hash([]byte(value)),
+		inUse:   true,
+		updated: true,
+	}
+	return nil
+}
+func (f files) updateFile(name, value string) error {
+	newHash := utils.Hash([]byte(value))
+	file := f.data[name]
+	if file.hash != newHash {
+		err := renameio.WriteFile(filepath.Join(f.dir, name), []byte(value), os.ModePerm)
+		if err != nil {
+			return err
+		}
+		file.hash = newHash
+		file.updated = true
+	}
+	file.inUse = true
+	f.data[name] = file
+	return nil
 }

controller/haproxy/api/api.go
@@ -28,7 +28,6 @@ type HAProxyClient interface {
 	BackendServerDelete(backendName string, serverName string) error
 	BackendSwitchingRuleCreate(frontend string, rule models.BackendSwitchingRule) error
 	BackendSwitchingRuleDeleteAll(frontend string)
-	DefaultsErrorFile(value *types.ErrorFile, index int) error
 	DefaultsGetConfiguration() (*models.Defaults, error)
 	DefaultsPushConfiguration(*models.Defaults) error
 	ExecuteRaw(command string) (result []string, err error)

controller/haproxy/api/global.go
@@ -6,15 +6,6 @@ import (
 	"github.com/haproxytech/config-parser/v4/types"
 )
 
-func (c *clientNative) DefaultsErrorFile(value *types.ErrorFile, index int) error {
-	config, err := c.nativeAPI.Configuration.GetParser(c.activeTransaction)
-	if err != nil {
-		return err
-	}
-	err = config.Set(parser.Defaults, parser.DefaultSectionName, "errorfile", value)
-	return err
-}
-
 func (c *clientNative) DefaultsGetConfiguration() (defaults *models.Defaults, err error) {
 	_, defaults, err = c.nativeAPI.Configuration.GetDefaultsConfiguration(c.activeTransaction)
 	return

commit 9a2af57324f03ce14a28cdda14cab4afa97739b0

BUG/MEDIUM: Fix inconsistent processing of haproxy files
- Fix uninitialized map access
- handling files (errorfiles and patternfiles) by reference to make
  changes persistent.

Regression introduced in 708775fb3

controller/configuration/main.go
@@ -173,7 +173,7 @@ func (c *ControllerCfg) envInit() (err error) {
 		c.Env.PatternDir = filepath.Join(c.Env.CfgDir, "patterns")
 	}
 	if c.Env.ErrFileDir == "" {
-		c.Env.ErrFileDir = filepath.Join(c.Env.CfgDir, "errors")
+		c.Env.ErrFileDir = filepath.Join(c.Env.CfgDir, "errorfiles")
 	}
 
 	for _, d := range []string{

controller/handler.go
@@ -41,7 +41,7 @@ func (c *HAProxyController) initHandlers() {
 			Port:     c.OSArgs.HTTPSBindPort,
 		},
 		handler.ProxyProtocol{},
-		handler.ErrorFile{},
+		&handler.ErrorFile{},
 		handler.TCPServices{
 			CertDir:  c.Cfg.Env.FrontendCertDir,
 			IPv4:     !c.OSArgs.DisableIPV4,
@@ -49,7 +49,7 @@ func (c *HAProxyController) initHandlers() {
 			IPv6:     !c.OSArgs.DisableIPV6,
 			AddrIPv6: c.OSArgs.IPV6BindAddr,
 		},
-		handler.PatternFiles{},
+		&handler.PatternFiles{},
 		handler.Refresh{},
 	}
 	if c.OSArgs.PprofEnabled {

controller/handler/errorfile.go
@@ -31,7 +31,7 @@ type ErrorFile struct {
 	updateAPI bool
 }
 
-func (h ErrorFile) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
+func (h *ErrorFile) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
 	h.files.dir = cfg.Env.ErrFileDir
 	if k.ConfigMaps.Errorfiles == nil {
 		return false, nil

controller/handler/pattern-files.go
@@ -30,17 +30,8 @@ type PatternFiles struct {
 	files files
 }
 
-type files struct {
-	dir  string
-	data map[string]file
-}
-type file struct {
-	hash    string
-	inUse   bool
-	updated bool
-}
-
-func (h PatternFiles) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
+func (h *PatternFiles) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
+	h.files.dir = cfg.Env.PatternDir
 	if k.ConfigMaps.PatternFiles == nil {
 		return false, nil
 	}
@@ -77,23 +68,39 @@ func (h PatternFiles) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAP
 	return reload, nil
 }
 
-func (f files) deleteFile(code string) error {
+type files struct {
+	dir  string
+	data map[string]*file
+}
+
+type file struct {
+	hash    string
+	inUse   bool
+	updated bool
+}
+
+func (f *files) deleteFile(code string) error {
 	delete(f.data, code)
 	err := os.Remove(filepath.Join(f.dir, code))
 	return err
 }
-func (f files) newFile(code, value string) error {
+
+func (f *files) newFile(code, value string) error {
 	if err := renameio.WriteFile(filepath.Join(f.dir, code), []byte(value), os.ModePerm); err != nil {
 		return err
 	}
-	f.data[code] = file{
+	if f.data == nil {
+		f.data = map[string]*file{}
+	}
+	f.data[code] = &file{
 		hash:    utils.Hash([]byte(value)),
 		inUse:   true,
 		updated: true,
 	}
 	return nil
 }
-func (f files) updateFile(name, value string) error {
+
+func (f *files) updateFile(name, value string) error {
 	newHash := utils.Hash([]byte(value))
 	file := f.data[name]
 	if file.hash != newHash {