haproxytech/kubernetes-ingress

commit 6333c379d2c39b447f1f84b36c98de886b5412d1

MEDIUM: handle annotations parsing in annotations package

controller/handler/https.go
@@ -74,41 +74,48 @@ func (h HTTPS) bindList(passhthrough bool) (binds []models.Bind) {
 }
 
 func (h HTTPS) handleClientTLSAuth(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
-	annTLSAuth := annotations.GetValue("client-ca", k.ConfigMaps.Main.Annotations)
-	annTLSVerify := annotations.GetValue("client-crt-optional", k.ConfigMaps.Main.Annotations)
-	if annTLSAuth == "" {
-		return false, nil
-	}
-	binds, err := api.FrontendBindsGet(cfg.FrontHTTPS)
-	if err != nil {
-		return
-	}
-	// Parsing annotations
+	// Parsing
 	var caFile string
-	caFile, err = cfg.Certificates.HandleTLSSecret(k, haproxy.SecretCtx{
-		DefaultNS:  "",
-		SecretPath: annTLSAuth,
-		SecretType: haproxy.CA_CERT,
-	})
-	if err != nil {
-		if errors.Is(err, haproxy.ErrCertNotFound) {
-			logger.Warningf("unable to configure TLS authentication secret '%s' not found", annTLSAuth)
-			err = nil
+	var notFound store.ErrNotFound
+	secret, annErr := annotations.Secret("client-ca", "", k, k.ConfigMaps.Main.Annotations)
+	if annErr != nil {
+		if errors.Is(annErr, notFound) {
+			logger.Warningf("client TLS Auth: %s", annErr)
+		} else {
+			err = fmt.Errorf("client TLS Auth: %w", annErr)
+			return
 		}
 	}
+	if secret == nil {
+		return
+	}
+	caFile, err = cfg.Certificates.HandleTLSSecret(secret, haproxy.CA_CERT)
+	if err != nil {
+		err = fmt.Errorf("client TLS Auth: %w", err)
+		return
+	}
+
+	binds, bindsErr := api.FrontendBindsGet(cfg.FrontHTTPS)
+	if bindsErr != nil {
+		err = fmt.Errorf("client TLS Auth: %w", bindsErr)
+		return
+	}
+
+	var enabled bool
 	verify := "required"
-	enabled, annErr := utils.GetBoolValue(annTLSVerify, "client-crt-optional")
+	enabled, annErr = annotations.Bool("client-crt-optional", k.ConfigMaps.Main.Annotations)
 	logger.Error(annErr)
 	if enabled {
 		verify = "optional"
 	}
+
 	// No changes
 	if binds[0].SslCafile == caFile && binds[0].Verify == verify {
 		return
 	}
 	// Removing config
 	if caFile == "" {
-		logger.Infof("removing client TLS authentication")
+		logger.Info("removing client TLS authentication")
 		for i := range binds {
 			binds[i].SslCafile = ""
 			binds[i].Verify = ""
@@ -120,7 +127,7 @@ func (h HTTPS) handleClientTLSAuth(k store.K8s, cfg *config.ControllerCfg, api a
 		return
 	}
 	// Updating config
-	logger.Infof("configuring client TLS authentication")
+	logger.Info("configuring client TLS authentication")
 	for i := range binds {
 		binds[i].SslCafile = caFile
 		binds[i].Verify = verify
@@ -134,12 +141,12 @@ func (h HTTPS) handleClientTLSAuth(k store.K8s, cfg *config.ControllerCfg, api a
 
 func (h HTTPS) Update(k store.K8s, cfg *config.ControllerCfg, api api.HAProxyClient) (reload bool, err error) {
 	if !h.Enabled {
-		logger.Debugf("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
+		logger.Debug("Cannot proceed with SSL Passthrough update, HTTPS is disabled")
 		return false, nil
 	}
 
 	// Fetch tls-alpn value for when SSL offloading is enabled
-	h.Alpn = annotations.GetValue("tls-alpn", k.ConfigMaps.Main.Annotations)
+	h.Alpn = annotations.String("tls-alpn", k.ConfigMaps.Main.Annotations)
 
 	// ssl-offload
 	if cfg.Certificates.FrontendCertsEnabled() {
@@ -251,14 +258,10 @@ func (h HTTPS) toggleSSLPassthrough(passthrough bool, cfg *config.ControllerCfg,
 }
 
 func (h HTTPS) sslPassthroughRules(k store.K8s, cfg *config.ControllerCfg) error {
-	inspectTimeout := utils.PtrInt64(5000)
-	annTimeout := annotations.GetValue("timeout-client", k.ConfigMaps.Main.Annotations)
-	if annTimeout != "" {
-		if value, errParse := utils.ParseTime(annTimeout); errParse == nil {
-			inspectTimeout = value
-		} else {
-			logger.Error(errParse)
-		}
+	inspectTimeout, err := annotations.Timeout("timeout-client", k.ConfigMaps.Main.Annotations)
+	if err != nil {
+		logger.Errorf("SSL Passthrough: %s", err)
+		inspectTimeout = utils.PtrInt64(5000)
 	}
 	errors := utils.Errors{}
 	errors.Add(cfg.HAProxyRules.AddRule(rules.ReqAcceptContent{}, false, cfg.FrontSSL),

controller/annotations/annotations.go
@@ -1,18 +1,23 @@
 package annotations
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/haproxytech/client-native/v2/models"
 
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
 	"github.com/haproxytech/kubernetes-ingress/controller/annotations/global"
 	"github.com/haproxytech/kubernetes-ingress/controller/annotations/ingress"
 	"github.com/haproxytech/kubernetes-ingress/controller/annotations/service"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy"
 	"github.com/haproxytech/kubernetes-ingress/controller/store"
+	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
 type Annotation interface {
 	GetName() string
-	Process(value string) error
+	Process(k store.K8s, annotations ...map[string]string) error
 }
 
 func Global(g *models.Global, l *models.LogTargets) []Annotation {
@@ -47,11 +52,11 @@ func Defaults(d *models.Defaults) []Annotation {
 	}
 }
 
-func Frontend(i store.Ingress, r *haproxy.Rules, m haproxy.Maps, k store.K8s) []Annotation {
+func Frontend(i store.Ingress, r *haproxy.Rules, m haproxy.Maps) []Annotation {
 	reqRateLimit := ingress.NewReqRateLimit(r)
 	httpsRedirect := ingress.NewHTTPSRedirect(r, i)
 	hostRedirect := ingress.NewHostRedirect(r)
-	reqAuth := ingress.NewReqAuth(r, i, k)
+	reqAuth := ingress.NewReqAuth(r, i)
 	reqCapture := ingress.NewReqCapture(r)
 	resSetCORS := ingress.NewResSetCORS(r)
 	return []Annotation{
@@ -101,59 +106,106 @@ func Backend(b *models.Backend) []Annotation {
 	return annotations
 }
 
-func Server(s *models.Server, k8sStore store.K8s, certs *haproxy.Certificates) []Annotation {
+func Server(s *models.Server, certs *haproxy.Certificates) []Annotation {
 	return []Annotation{
 		service.NewCheck("check", s),
 		service.NewCheckInter("check-interval", s),
 		service.NewCookie("cookie-persistence", nil, s),
-		service.NewMaxconn("pod-maxconn", s, k8sStore.NbrHAProxyInst),
+		service.NewMaxconn("pod-maxconn", s),
 		service.NewSendProxy("send-proxy-protocol", s),
 		// Order is important for ssl annotations so they don't conflict
 		service.NewSSL("server-ssl", s),
-		service.NewCrt("server-crt", k8sStore, certs, s),
-		service.NewCA("server-ca", k8sStore, certs, s),
+		service.NewCrt("server-crt", certs, s),
+		service.NewCA("server-ca", certs, s),
 		service.NewProto("server-proto", s),
 	}
 }
 
-// GetValue returns value by checking in multiple annotations.
-func GetValue(annotationName string, annotations ...map[string]string) string {
-	for _, a := range annotations {
-		val, ok := a[annotationName]
-		if ok {
-			return val
-		}
-	}
-	return defaultValues[annotationName]
-}
-
 func SetDefaultValue(annotation, value string) {
-	defaultValues[annotation] = value
+	common.DefaultValues[annotation] = value
 }
 
-var defaultValues = map[string]string{
-	"auth-realm":             "Protected Content",
-	"check":                  "true",
-	"cors-allow-origin":      "*",
-	"cors-allow-methods":     "*",
-	"cors-allow-headers":     "*",
-	"cors-max-age":           "5s",
-	"cookie-indirect":        "true",
-	"cookie-nocache":         "true",
-	"cookie-type":            "insert",
-	"forwarded-for":          "true",
-	"load-balance":           "roundrobin",
-	"rate-limit-size":        "100k",
-	"rate-limit-period":      "1s",
-	"rate-limit-status-code": "403",
-	"request-capture-len":    "128",
-	"ssl-redirect-code":      "302",
-	"request-redirect-code":  "302",
-	"ssl-redirect-port":      "443",
-	"ssl-passthrough":        "false",
-	"server-ssl":             "false",
-	"scale-server-slots":     "42",
-	"syslog-server":          "address:127.0.0.1, facility: local0, level: notice",
-	"client-crt-optional":    "false",
-	"tls-alpn":               "h2,http/1.1",
+func Bool(name string, annotations ...map[string]string) (out bool, err error) {
+	input := common.GetValue(name, annotations...)
+	if input == "" {
+		return
+	}
+	out, err = utils.GetBoolValue(input, name)
+	if err != nil {
+		err = fmt.Errorf("%s annotation: %w", name, err)
+		return
+	}
+	return
+}
+
+func Int(name string, annotations ...map[string]string) (out int, err error) {
+	input := common.GetValue(name, annotations...)
+	if input == "" {
+		return
+	}
+	out, err = strconv.Atoi(input)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	return
+}
+
+func Secret(name, defaultNs string, k store.K8s, annotations ...map[string]string) (secret *store.Secret, err error) {
+	var secNs, secName string
+	secNs, secName, err = common.GetK8sPath(name, annotations...)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	if secName == "" {
+		return
+	}
+	if secNs == "" {
+		secNs = defaultNs
+	}
+	secret, err = k.GetSecret(secNs, secName)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	return
+}
+
+func Service(name, defaultNs string, k store.K8s, annotations ...map[string]string) (service *store.Service, err error) {
+	var svcNs, svcName string
+	svcNs, svcName, err = common.GetK8sPath(name, annotations...)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	if svcName == "" {
+		return
+	}
+	if svcNs == "" {
+		svcNs = defaultNs
+	}
+	service, err = k.GetService(svcNs, svcName)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	return
+}
+
+func String(name string, annotations ...map[string]string) string {
+	return common.GetValue(name, annotations...)
+}
+
+func Timeout(name string, annotations ...map[string]string) (out *int64, err error) {
+	input := common.GetValue(name, annotations...)
+	if input == "" {
+		return
+	}
+	out, err = utils.ParseTime(input)
+	if err != nil {
+		err = fmt.Errorf("annotation '%s': %w", name, err)
+		return
+	}
+	return
 }

controller/annotations/cfgSnippet.go
@@ -4,7 +4,9 @@ import (
 	"reflect"
 	"strings"
 
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
 	"github.com/haproxytech/kubernetes-ingress/controller/haproxy/api"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type CfgSnippet struct {
@@ -47,7 +49,8 @@ func (a *CfgSnippet) GetName() string {
 	return a.name
 }
 
-func (a *CfgSnippet) Process(input string) error {
+func (a *CfgSnippet) Process(k store.K8s, annotations ...map[string]string) error {
+	input := common.GetValue(a.GetName(), annotations...)
 	data := []string{}
 	if input != "" {
 		data = strings.Split(strings.Trim(input, "\n"), "\n")

controller/annotations/common/main.go
@@ -1,0 +1,65 @@
+package common
+
+import (
+	"errors"
+	"strings"
+)
+
+// GetValue returns value by checking in multiple annotations.
+func GetValue(annotationName string, annotations ...map[string]string) string {
+	for _, a := range annotations {
+		val, ok := a[annotationName]
+		if ok {
+			return val
+		}
+	}
+	return DefaultValues[annotationName]
+}
+
+func GetK8sPath(annotationName string, annotations ...map[string]string) (ns, name string, err error) {
+	a := GetValue(annotationName, annotations...)
+	if a == "" {
+		return
+	}
+	parts := strings.Split(a, "/")
+	switch len(parts) {
+	case 1:
+		name = parts[0]
+	case 2:
+		ns = parts[0]
+		name = parts[1]
+		if ns == "" {
+			err = errors.New("invalid format")
+		}
+	}
+	if name == "" {
+		err = errors.New("invalid format")
+	}
+	return
+}
+
+var DefaultValues = map[string]string{
+	"auth-realm":             "Protected Content",
+	"check":                  "true",
+	"cors-allow-origin":      "*",
+	"cors-allow-methods":     "*",
+	"cors-allow-headers":     "*",
+	"cors-max-age":           "5s",
+	"cookie-indirect":        "true",
+	"cookie-nocache":         "true",
+	"cookie-type":            "insert",
+	"forwarded-for":          "true",
+	"load-balance":           "roundrobin",
+	"rate-limit-size":        "100k",
+	"rate-limit-period":      "1s",
+	"rate-limit-status-code": "403",
+	"request-capture-len":    "128",
+	"ssl-redirect-code":      "302",
+	"request-redirect-code":  "302",
+	"ssl-redirect-port":      "443",
+	"ssl-passthrough":        "false",
+	"server-ssl":             "false",
+	"scale-server-slots":     "42",
+	"client-crt-optional":    "false",
+	"tls-alpn":               "h2,http/1.1",
+}

controller/annotations/global/hardStopAfter.go
@@ -3,6 +3,8 @@ package global
 import (
 	"github.com/haproxytech/client-native/v2/models"
 
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
 	"github.com/haproxytech/kubernetes-ingress/controller/utils"
 )
 
@@ -19,7 +21,8 @@ func (a *HardStopAfter) GetName() string {
 	return a.name
 }
 
-func (a *HardStopAfter) Process(input string) error {
+func (a *HardStopAfter) Process(k store.K8s, annotations ...map[string]string) error {
+	input := common.GetValue(a.GetName(), annotations...)
 	if input == "" {
 		a.global.HardStopAfter = nil
 		return nil

controller/annotations/global/logFormat.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 
 	"github.com/haproxytech/client-native/v2/models"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type LogFormat struct {
@@ -19,7 +22,8 @@ func (a *LogFormat) GetName() string {
 	return a.name
 }
 
-func (a *LogFormat) Process(input string) error {
+func (a *LogFormat) Process(k store.K8s, annotations ...map[string]string) error {
+	input := common.GetValue(a.GetName(), annotations...)
 	if input != "" {
 		input = "'" + strings.TrimSpace(input) + "'"
 	}

controller/annotations/global/maxconn.go
@@ -4,6 +4,9 @@ import (
 	"strconv"
 
 	"github.com/haproxytech/client-native/v2/models"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type Maxconn struct {
@@ -19,7 +22,8 @@ func (a *Maxconn) GetName() string {
 	return a.name
 }
 
-func (a *Maxconn) Process(input string) error {
+func (a *Maxconn) Process(k store.K8s, annotations ...map[string]string) error {
+	input := common.GetValue(a.GetName(), annotations...)
 	if input == "" {
 		a.global.Maxconn = 0
 		return nil

controller/annotations/global/nbthread.go
@@ -5,6 +5,9 @@ import (
 	"strconv"
 
 	"github.com/haproxytech/client-native/v2/models"
+
+	"github.com/haproxytech/kubernetes-ingress/controller/annotations/common"
+	"github.com/haproxytech/kubernetes-ingress/controller/store"
 )
 
 type Nbthread struct {
@@ -20,7 +23,8 @@ func (a *Nbthread) GetName() string {
 	return a.name
 }
 
-func (a *Nbthread) Process(input string) error {
+func (a *Nbthread) Process(k store.K8s, annotations ...map[string]string) error {
+	input := common.GetValue(a.GetName(), annotations...)
 	if input == "" {
 		a.global.Nbthread = 0
 		return nil

commit 1966e37d7315be071fe15c4321fce2f9aaf5578e

BUG/MEDIUM: ssl-passthrough: Fix default delay value
When no client-timeout is provided inspect delay rule should fallback to
a default value.
Reported in https://github.com/haproxytech/kubernetes-ingress/issues/414
Regression introduced in 6333c379d2
Backport to 1.7

controller/handler/https.go
@@ -260,8 +260,10 @@ func (h HTTPS) toggleSSLPassthrough(passthrough bool, cfg *config.ControllerCfg,
 
 func (h HTTPS) sslPassthroughRules(k store.K8s, cfg *config.ControllerCfg) error {
 	inspectTimeout, err := annotations.Timeout("timeout-client", k.ConfigMaps.Main.Annotations)
-	if err != nil {
-		logger.Errorf("SSL Passthrough: %s", err)
+	if inspectTimeout == nil {
+		if err != nil {
+			logger.Errorf("SSL Passthrough: %s", err)
+		}
 		inspectTimeout = utils.PtrInt64(5000)
 	}
 	errors := utils.Errors{}