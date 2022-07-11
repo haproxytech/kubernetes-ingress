50bae0ce94b67a7ace40578f8a0a28400217d405
Moemen MHEDHBI <mmhedhbi@haproxy.com>
2022-07-11 17:01:44 +0200

    MINOR: cr: Update helper script for CR spec

--- crs/get-crd-schema.sh
+++ crs/get-crd-schema.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o pipefail
 
+command -v swagger >/dev/null 2>&1 || { echo >&2 "swagger not installed.  Aborting."; exit 1; }
 command -v yq >/dev/null 2>&1 || { echo >&2 "yq not installed.  Aborting."; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo >&2 "jq not installed.  Aborting."; exit 1; }
 
@@ -11,30 +12,22 @@ CN_COMMIT=$(go list -m github.com/haproxytech/client-native/v3 | sed 's/^.*-//')
 if [ -z "$1" ]; then echo >&2 "No model name supplied.  Aborting."; exit 1; fi
 if [ -z "$CN_COMMIT" ]; then echo >&2 "Unable to get git commit for CN module.  Aborting."; exit 1; fi
 
-curl -sk https://raw.githubusercontent.com/haproxytech/client-native/$CN_COMMIT/specification/models/configuration.yaml |
+swagger expand https://raw.githubusercontent.com/haproxytech/client-native/$CN_COMMIT/specification/build/haproxy_spec.yaml |
 	yq |
-  jq --arg MODEL $1 '.|
-  	reduce paths as $p(.;
-    if $p[0] == $MODEL and $p[-1] == "$ref" then
-  		setpath($p[0:-1];getpath(getpath($p) | split("/")[-1]| split(" ")))
-  	else
-  		.
-  	end
-  	) |
-		.[$MODEL] |
-		walk(
-				if type == "object" then with_entries(
-					if .key == "x-nullable" then
-						if .value == false then
-							empty
-						else
-							.key = "nullable"
-						end
-					elif (.key | contains("x-")) then
-						empty
-					else
-						.
-					end
-				) else . end
-		)' |
+  jq --arg MODEL $1 '.["definitions"][$MODEL]| 
+	  walk(
+        if type == "object" then with_entries(
+          if .key == "x-nullable" then
+            if .value == false then
+              empty
+            else
+              .key = "nullable"
+            end
+          elif (.key | contains("x-")) then
+            empty
+          else
+            .
+          end
+        ) else . end
+    )' |
   yq -y
