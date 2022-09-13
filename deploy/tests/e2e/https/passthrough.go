8bddf5eca27d506f6bee74f8c290113291100118
Zlatko Bratkovic <zbratkovic@haproxy.com>
2022-09-13 10:13:13 +0200

    TEST: ci: extract https tests to separate ci job on github

--- deploy/tests/e2e/https/passthrough.go
+++ deploy/tests/e2e/https/passthrough.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build e2e_parallel
+//go:build e2e_https
 
 package https
 
@@ -76,6 +76,6 @@ func (suite *HTTPSSuite) Test_HTTPS_Passthrough() {
 			}
 			// should not be blocked by whitelist rule since sni does not match
 			return res != nil
-		}, e2e.WaitDuration, e2e.TickDuration)
+		}, e2e.WaitDuration, e2e.TickDuration, "reason is :: ")
 	})
 }
--- deploy/tests/e2e/https/suite_test.go
+++ deploy/tests/e2e/https/suite_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build e2e_parallel
+//go:build e2e_https
 
 package https
 
