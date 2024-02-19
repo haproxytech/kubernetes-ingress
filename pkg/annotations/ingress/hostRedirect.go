9338c489c0ec59b1a34c12f5a56b2fe5851b1e94
Ivan Matmati <imatmati@haproxy.com>
2024-02-19 15:02:01 +0100

    MINOR: add scheme support in HTTP(S) redirects

--- pkg/annotations/ingress/hostRedirect.go
+++ pkg/annotations/ingress/hostRedirect.go
@@ -3,10 +3,17 @@ package ingress
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/haproxytech/kubernetes-ingress/pkg/annotations/common"
 	"github.com/haproxytech/kubernetes-ingress/pkg/haproxy/rules"
 	"github.com/haproxytech/kubernetes-ingress/pkg/store"
+)
+
+//nolint:golint,stylecheck
+const (
+	HTTP_PREFIX  = "http://"
+	HTTPS_PREFIX = "https://"
 )
 
 type HostRedirect struct {
@@ -43,6 +50,14 @@ func (a HostRedirectAnn) Process(k store.K8s, annotations ...map[string]string) 
 	switch a.name {
 	case "request-redirect":
 		a.parent.redirect = &rules.RequestRedirect{Host: input}
+		switch {
+		case strings.HasPrefix(input, HTTPS_PREFIX):
+			a.parent.redirect.SSLRequest = true
+			a.parent.redirect.Host = a.parent.redirect.Host[len(HTTPS_PREFIX):]
+		case strings.HasPrefix(input, HTTP_PREFIX):
+			a.parent.redirect.SSLRequest = false
+			a.parent.redirect.Host = a.parent.redirect.Host[len(HTTP_PREFIX):]
+		}
 		a.parent.rules.Add(a.parent.redirect)
 		return
 	case "request-redirect-code":
