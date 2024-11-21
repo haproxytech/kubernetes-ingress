c28d620aee948a8e912c9a52b4be4ad4d8cd6c82
Ivan Matmati <imatmati@haproxy.com>
2024-11-21 14:40:47 +0100

    MINOR: avoid extra space in custom route

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -108,9 +108,17 @@ func AddCustomRoute(route Route, routeACLAnn string, api api.HAProxyClient) (err
 	}
 	if route.Path.Path != "" {
 		if route.Path.PathTypeMatch == store.PATH_TYPE_EXACT {
-			routeCond = fmt.Sprintf("%s { path %s } ", routeCond, route.Path.Path)
+			if routeCond == "" {
+				routeCond = fmt.Sprintf("{ path %s }", route.Path.Path)
+			} else {
+				routeCond = fmt.Sprintf("%s { path %s }", routeCond, route.Path.Path)
+			}
 		} else {
-			routeCond = fmt.Sprintf("%s { path -m beg %s } ", routeCond, route.Path.Path)
+			if routeCond == "" {
+				routeCond = fmt.Sprintf("{ path -m beg %s }", route.Path.Path)
+			} else {
+				routeCond = fmt.Sprintf("%s { path -m beg %s }", routeCond, route.Path.Path)
+			}
 		}
 	}
 	routeCond = fmt.Sprintf("%s { %s } ", routeCond, routeACLAnn)
