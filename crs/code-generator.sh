a8f5b276ad3da11219255624fdf1c78b0c9ffd63
Zlatko Bratkovic <zbratkovic@haproxy.com>
2025-02-25 13:34:58 +0100

    BUILD/MINOR: remove controller-gen annotation from CRDs

--- crs/code-generator.sh
+++ crs/code-generator.sh
@@ -100,3 +100,5 @@ go install sigs.k8s.io/controller-tools/cmd/controller-gen@${CONTROLLER_GEN_VERS
 # # Controller-gen version
 echo "Controller-gen: " ${CONTROLLER_GEN_VERSION}
 controller-gen crd paths=./crs/api/ingress/v3/...  output:crd:dir=./crs/definition
+# remove code-gen annotation (dependabot fails)
+find ${CR_DIR}/definition -type f -name '*.yaml' -exec sed -i '/controller-gen.kubebuilder.io\/version/d' {} +
