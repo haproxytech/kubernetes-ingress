b702b7aaf3dbffecd5b89f7aa4bcdb1964ec777b
Zlatko Bratkovic <zbratkovic@haproxy.com>
2025-09-15 09:55:56 +0200

    BUILD/MINOR: update go-openapi/swag package

--- crs/api/ingress/v1/tcp.go
+++ crs/api/ingress/v1/tcp.go
@@ -16,7 +16,7 @@
 package v1
 
 import (
-	"github.com/go-openapi/swag"
+	"github.com/go-openapi/swag/jsonutils"
 	"github.com/haproxytech/client-native/v5/models"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -161,13 +161,13 @@ func (s *TCPService) MarshalBinary() ([]byte, error) {
 	if s == nil {
 		return nil, nil
 	}
-	return swag.WriteJSON(s)
+	return jsonutils.WriteJSON(s)
 }
 
 // UnmarshalBinary interface implementation
 func (s *TCPService) UnmarshalBinary(b []byte) error {
 	var res TCPService
-	if err := swag.ReadJSON(b, &res); err != nil {
+	if err := jsonutils.ReadJSON(b, &res); err != nil {
 		return err
 	}
 	*s = res
