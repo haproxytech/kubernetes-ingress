8e5c702910e1ed32735e1c6828fc74236d734ac1
Helene Durand <hdurand@haproxy.com>
2026-01-13 15:21:31 +0100

    BUG/MEDIUM: fix reloads Prometheus metrics for success and failure
    The wrong error was used to distinguish between success and failure for the reload metrics.
    This was tracking the success or failure of the haproxy configuration file commit, not the reload status.

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -202,6 +202,7 @@ func (c *HAProxyController) updateHAProxy() {
 		var msg string
 		if msg, err = c.haproxy.Service("reload"); err != nil {
 			logger.Error(err)
+			c.prometheusMetricsManager.UpdateReloadMetrics(err)
 			errLines := strings.Split(msg, "\n")
 			msg := ""
 			// Extract only lines with [ALERT] prefix to reuse functions
@@ -227,8 +228,8 @@ func (c *HAProxyController) updateHAProxy() {
 			logger.Error(c.haproxy.PopPreviousBackends())
 		} else {
 			logger.Info("HAProxy reloaded")
+			c.prometheusMetricsManager.UpdateReloadMetrics(err)
 		}
-		c.prometheusMetricsManager.UpdateReloadMetrics(err)
 	} else if c.osArgs.DisableDelayedWritingOnlyIfReload {
 		// If the osArgs flag is set, then write the files to disk even if there is no reload of haproxy
 		fs.RunDelayedFuncs()
