ef11dd9a64d7cec5193042220457f4efec3d8a77
Helene Durand <hdurand@haproxy.com>
2026-02-02 09:09:00 +0100

    BUG/MEDIUM: fix error on rate-limit-requests
    With this commit, we allow to enable or not rate limit based on the present of rate-limit-request annotation.
    If rate-limit-request is set, then other annotations related to rate-limit have default values:
    - "rate-limit-size":        "100k",
    - "rate-limit-period":      "1s",
    - "rate-limit-status-code": "403",
    but can be set as annotation too.
    Commit 'MINOR: rate-limit: add rate-limit-whitelist annotation to exclude IPs' did reveal that we were  missing a default value for rate-limit-requests.
    This was generating error logs during each cycle.

--- pkg/haproxy/rules/reqRatelimit.go
+++ pkg/haproxy/rules/reqRatelimit.go
@@ -20,6 +20,10 @@ type ReqRateLimit struct {
 	WhitelistMaps  []maps.Path // Pattern file references
 }
 
+const (
+	defaultRateLimitStatueCode = "403"
+)
+
 func (r ReqRateLimit) GetType() Type {
 	return REQ_RATELIMIT
 }
@@ -28,7 +32,17 @@ func (r ReqRateLimit) Create(client api.HAProxyClient, frontend *models.Frontend
 	if frontend.Mode == "tcp" {
 		return errors.New("request Track cannot be configured in TCP mode")
 	}
+
+	// ReqsLimit == 0 means rate-limit disabled
+	if r.ReqsLimit == 0 {
+		return nil
+	}
 	condTest := fmt.Sprintf("{ sc0_http_req_rate(%s) gt %d }", r.TableName, r.ReqsLimit)
+
+	err := r.applyDefaults()
+	if err != nil {
+		return err
+	}
 
 	// Build whitelist conditions if configured
 	// If whitelist is set, only apply rate limiting if source IP is NOT in the whitelist
@@ -57,4 +71,15 @@ func (r ReqRateLimit) Create(client api.HAProxyClient, frontend *models.Frontend
 		CondTest:   condTest,
 	}
 	return client.FrontendHTTPRequestRuleCreate(0, frontend.Name, httpRule, ingressACL)
+}
+
+func (r *ReqRateLimit) applyDefaults() error {
+	if r.DenyStatusCode == 0 {
+		code, err := utils.ParseInt(defaultRateLimitStatueCode)
+		if err != nil {
+			return err
+		}
+		r.DenyStatusCode = code
+	}
+	return nil
 }
