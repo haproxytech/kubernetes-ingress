52a314d4a63555312786bee2a7bfc34ea8da339c
Zlatko Bratkovic <zbratkovic@haproxy.com>
2026-02-10 09:52:32 +0100

    CLEANUP/MINOR: ci: use temporary directory for additional tools
    tmp is shared across projects, so we can easily reuse same binary

--- bin/check-commit.sh
+++ bin/check-commit.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-V=$(./check-commit tag)
+BIN_DIR="/tmp/check-commit/v$CHECK_COMMIT"
 
-if echo "$V" | grep -q "v$CHECK_COMMIT"; then
-    echo "$V"
-else
-    echo "go install github.com/haproxytech/check-commit/v5@v$CHECK_COMMIT"
-    GOBIN=$(pwd) go install github.com/haproxytech/check-commit/v5@v$CHECK_COMMIT
+if [ -x "$BIN_DIR/check-commit" ]; then
+    V=$("$BIN_DIR/check-commit" tag)
+    if echo "$V" | grep -q "v$CHECK_COMMIT"; then
+        echo "$V"
+        exit 0
+    fi
 fi
+
+mkdir -p "$BIN_DIR"
+echo "go install github.com/haproxytech/check-commit/v5@v$CHECK_COMMIT"
+GOBIN="$BIN_DIR" go install github.com/haproxytech/check-commit/v5@v$CHECK_COMMIT
