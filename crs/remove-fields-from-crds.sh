0d40143f6189b2d6a21de927ffe96535266ef7d2
Zlatko Bratkovic <zbratkovic@haproxy.com>
2026-02-16 10:04:56 +0100

    BUILD/MINOR: crd: do not require name for backend crd
    this name is not used since ingress controller is generating backends with predefined names

--- crs/remove-fields-from-crds.sh
+++ crs/remove-fields-from-crds.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# servers from the backend CRD v3
 yq -i 'del(.spec.versions[0].schema.openAPIV3Schema.properties.spec.properties.servers)' crs/definition/ingress.v3.haproxy.org_backends.yaml
+# name (required) from the backend CRD v3
+yq -i 'del(.spec.versions[0].schema.openAPIV3Schema.properties.spec.required[] | select(. == "name"))' crs/definition/ingress.v3.haproxy.org_backends.yaml
