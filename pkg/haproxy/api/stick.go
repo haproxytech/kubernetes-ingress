2488f0ca829ae7d84c3e522547048506d58a3596
Zlatko Bratkovic <zbratkovic@haproxy.com>
2026-05-05 16:47:11 +0200

    BUG/MINOR: add support for stick rules configuration in Backend CRD
    Introduce a new unit test suite in `deploy/tests/ut/stick/` to verify the behavior of stick rules in HAProxy.
    
    These tests ensure that:
    - Stick rules defined via Backend Custom Resources (CRs) are properly parsed and injected into the HAProxy configuration.
    - Modifying only the `StickRuleList` in a Backend CR successfully triggers a configuration update, ensuring old rules are removed and replaced with the updated directives.

---
+++ pkg/haproxy/api/stick.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/haproxytech/client-native/v6/models"
+)
+
+func (c *clientNative) StickRulesGet(backendName string) (models.StickRules, error) {
+	configuration, err := c.nativeAPI.Configuration()
+	if err != nil {
+		return nil, err
+	}
+	if backend, exists := c.backends[backendName]; exists {
+		return backend.StickRuleList, nil
+	}
+	_, rules, err := configuration.GetStickRules(backendName, c.activeTransaction)
+	if err != nil {
+		return nil, err
+	}
+	return rules, nil
+}
+
+func (c *clientNative) StickRuleDeleteAll(backendName string) error {
+	configuration, err := c.nativeAPI.Configuration()
+	if err != nil {
+		return err
+	}
+	if backend, exists := c.backends[backendName]; exists {
+		backend.StickRuleList = nil
+		c.backends[backendName] = backend
+		return nil
+	}
+	_, rules, errGet := configuration.GetStickRules(backendName, c.activeTransaction)
+	if errGet != nil {
+		return errGet
+	}
+	for range rules {
+		errDelete := configuration.DeleteStickRule(0, backendName, c.activeTransaction, 0)
+		if errDelete != nil {
+			return errDelete
+		}
+	}
+	return nil
+}
+
+func (c *clientNative) StickRuleCreate(id int64, backendName string, data *models.StickRule) error {
+	configuration, err := c.nativeAPI.Configuration()
+	if err != nil {
+		return err
+	}
+	if backend, exists := c.backends[backendName]; exists {
+		backend.StickRuleList = append(backend.StickRuleList, data)
+		c.backends[backendName] = backend
+		return nil
+	}
+	return configuration.CreateStickRule(id, backendName, data, c.activeTransaction, 0)
+}
+
+func (c *clientNative) StickRulesReplace(backendName string, rules models.StickRules) error {
+	configuration, err := c.nativeAPI.Configuration()
+	if err != nil {
+		return err
+	}
+	if backend, exists := c.backends[backendName]; exists {
+		backend.StickRuleList = rules
+		c.backends[backendName] = backend
+		return nil
+	}
+	if backendName == "" {
+		return fmt.Errorf("can't replace stick rules: backend %s : %w", backendName, ErrNotFound)
+	}
+	return configuration.ReplaceStickRules(backendName, rules, c.activeTransaction, 0)
+}
